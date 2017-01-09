ff24c8f08ad57bb7dec0444e169b01eb81fa664a
user3301 <stan_gai@Hotmail.com>
2017-01-09 22:36:19 +1100

    add common property listnode

---
+++ leetcodesln/common/ListNode.cs
@@ -0,0 +1,13 @@
+namespace leetcodesln
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
