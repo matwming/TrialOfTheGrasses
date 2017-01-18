3bc543411be781567b2658cc682ce348f2f4476a
zhelun <stan_gai@Hotmail.com>
2017-01-19 09:48:19 +1100

    wip

---
+++ leetcodesln/DesignHashSet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class DesignHashSet
+    {
+        /** Initialize your data structure here. */
+        public DesignHashSet()
+        {
+
+        }
+
+        public void Add(int key)
+        {
+
+        }
+
+        public void Remove(int key)
+        {
+
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */
