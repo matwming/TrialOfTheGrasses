a2c2fe25118dc3fcad2c50a03ba3b304f408ced3
zhelun <stan_gai@Hotmail.com>
2017-01-29 09:40:16 +1100

    longest continuous increasing subsequence (memory limit exceeded)

---
+++ leetcodesln/LongestContinuousIncreasingSubsequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class LongestContinuousIncreasingSubsequence
+    {
+        public int FindLengthOfLCIS(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            var max = int.MinValue;
+            var current = 0;
+            while(current < nums.Length)
+            {
+                var remain = new int[nums.Length - current];
+                for (int i = current, j =0; i < nums.Length; i++, j++)
+                {
+                    remain[j] = nums[i];
+                }
+
+                var partialLongest = FindPartialLongest(remain);
+                current += partialLongest;
+                max = max < partialLongest? partialLongest : max;
+            }
+            return max;
+        }
+
+        private int FindPartialLongest(int[] part)
+        {
+            int index = 0;
+            while (index + 1 < part.Length && part[index] < part[index + 1])
+            {
+                index++;
+            }
+            return index+1;
+        }
+    }
+}
---
+++ leetcodesln_test/LongestContinuousIncreasingSubsequenceUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LongestContinuousIncreasingSubsequenceUnitTest
+    {
+        [TestMethod]
+        public void LongestContinuousIncreasingSubsequenceTestMethod()
+        {
+            var longestContinuousIncreasingSubsequence = new LongestContinuousIncreasingSubsequence();
+            var input1 = new int[] { 1, 3, 5, 4, 7 };
+            var expected1 = 3;
+            var actual1 = longestContinuousIncreasingSubsequence.FindLengthOfLCIS(input1);
+            Assert.AreEqual(expected1, actual1);
+
+            var input2 = new int[] { 2,2,2,2,2 };
+            var expected2 = 1;
+            var actual2 = longestContinuousIncreasingSubsequence.FindLengthOfLCIS(input2);
+            Assert.AreEqual(expected2, actual2);
+        }
+    }
+}
