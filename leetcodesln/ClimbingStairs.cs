f25135fcf205bcbab353488445372fb890731538
zhelun <stan_gai@Hotmail.com>
2017-02-04 09:45:07 +1100

    climbing stairs (time limit exceeded)

---
+++ leetcodesln/ClimbingStairs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ClimbingStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            if (n == 0) return 0;
+
+            return ClimbStairsSln(n - 1) + ClimbStairsSln(n - 2);
+        }
+    }
+}
---
+++ leetcodesln_test/ClimbingStairsUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ClimbingStairsUnitTest
+    {
+        [TestMethod]
+        public void ClimbingStairsTestMethod()
+        {
+            var climbingStairs = new ClimbingStairs();
+            var input1 = 2;
+            var expected1 = 2;
+            var actual1 = climbingStairs.ClimbStairs(input1);
+            Assert.AreEqual(expected1, actual1);
+
+            var input2 = 3;
+            var expected2 = 3;
+            var actual2 = climbingStairs.ClimbStairs(input2);
+            Assert.AreEqual(expected2, actual2);
+
+            var input3 = 4;
+            var expected3 = 5;
+            var actual3 = climbingStairs.ClimbStairs(input3);
+            Assert.AreEqual(expected3, actual3);
+        }
+    }
+}
