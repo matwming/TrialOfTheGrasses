ce77f4d48aa5387d205c2ab6a10fdab85431804f
zhelun <stan_gai@Hotmail.com>
2017-02-06 11:40:08 +1100

    longest harmonious subsequence (time limit exceeded)

---
+++ leetcodesln/LongestHarmoniousSubsequence.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class LongestHarmoniousSubsequence
+    {
+        public int FindLHS(int[] nums)
+        {
+            var excemption = new List<int>();
+            var containTable = new HashSet<int>();
+            var maxlength = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (containTable.Contains(nums[i])) continue;
+                if (nums.Contains(nums[i] - 1))
+                {
+                    var min = nums[i] - 1;
+                    var max = nums[i];
+                    var localmax = 0;
+                    for (int j = i; j < nums.Length; j++)
+                    {
+                        if (min <= nums[j] && nums[j] <= max)
+                        {
+                            localmax++;
+                            excemption.Add(nums[j]);
+                        }
+                        if (excemption.Contains(nums[j] - 1) && excemption.Contains(nums[j] + 1)) containTable.Add(nums[j]);
+                    }
+                    maxlength = maxlength > localmax ? maxlength : localmax;
+                }
+                if (nums.Contains(nums[i] + 1))
+                {
+                    var min = nums[i];
+                    var max = nums[i]+1;
+                    var localmax = 0;
+                    for (int j = i; j < nums.Length; j++)
+                    {
+                        if (min <= nums[j] && nums[j] <= max)
+                        {
+                            localmax++;
+                            excemption.Add(nums[j]);
+                        }
+                        if (excemption.Contains(nums[j] - 1) && excemption.Contains(nums[j] + 1)) containTable.Add(nums[j]);
+                    }
+                    maxlength = maxlength > localmax ? maxlength : localmax;
+                }
+            }
+            return maxlength;
+        }
+    }
+}
---
+++ leetcodesln_test/LongestHarmoniousSubsequenceUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LongestHarmoniousSubsequenceUnitTest
+    {
+        [TestMethod]
+        public void LongestHarmoniousSubsequenceTestMethod()
+        {
+            var longestHarmoniousSubsequence = new LongestHarmoniousSubsequence();
+            var input1 = new int[] { 1, 3, 2, 2, 5, 2, 3, 7 };
+            var expected1 = 5;
+            var actual1 = longestHarmoniousSubsequence.FindLHS(input1);
+            Assert.AreEqual(expected1, actual1);
+
+            var input2 = new int[] { 1, 1, 1, 1 };
+            var expected2 = 0;
+            var actual2 = longestHarmoniousSubsequence.FindLHS(input2);
+            Assert.AreEqual(expected2, actual2);
+
+            var input3 = new int[] {2, 2, 2, 2, 2, 2, 2, 3, 1, 0, 0, 0, 3, 1, -1, 0, 1, 1, 0, 0, 1, 1, 2, 2, 2, 0, 1, 2, 2, 3, 2 };
+            var expected3 = 20;
+            var actual3 = longestHarmoniousSubsequence.FindLHS(input3);
+            Assert.AreEqual(expected3, actual3);
+
+            var input4 = new int[] { 1, 2, 1, 3, 0, 0, 2, 2, 1, 3, 3 };
+            var expected4 = 6;
+            var actual4 = longestHarmoniousSubsequence.FindLHS(input4);
+            Assert.AreEqual(expected4, actual4);
+        }
+    }
+}
