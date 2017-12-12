74b684f9242ee3c20ade58dc237dd86b6d7e9175
user3301 <stan_gai@Hotmail.com>
2017-12-12 14:00:52 +1100

    delete unit test project

--- leetcodetest/ArrayPartitionsTest.cs
+++ leetcodetest/ArrayPartitionsTest.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace leetcodetest
+namespace leetcodetest.ArrayPartitionsTest
 {
     [TestClass]
     public class ArrayPartitionsTest
