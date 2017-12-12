66ed9f4efa50e8797151355ec3c69c6d0a9c7ab7
user3301 <stan_gai@Hotmail.com>
2017-12-12 13:59:46 +1100

    new test

---
+++ leetcodetest/NewTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodetest
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class MyTestClass
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
