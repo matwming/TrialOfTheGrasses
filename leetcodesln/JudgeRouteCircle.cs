16b9d80bc751bd579bdd9340c5b9c6c7fdd937de
user3301 <stan_gai@Hotmail.com>
2017-12-13 14:57:23 +1100

    judege route circle with test case

---
+++ leetcodesln/JudgeRouteCircle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class JudgeRouteCircle
+    {
+        public bool JudgeRouteCircleSolution(string moves)
+        {
+            if(moves.Length==0 || moves==null)
+            {
+                return false;
+            }
+            double extraMoves = 0;
+            for (int i = 0; i < moves.Length; i++)
+            {
+                switch (moves[i])
+                {
+                    case 'U':
+                        extraMoves+=1.0;
+                        break;
+                    case 'D':
+                        extraMoves-=1.0;
+                        break;
+                    case 'R':
+                        extraMoves += 1.5;
+                        break;
+                    case 'L':
+                        extraMoves -= 1.5;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return extraMoves == 0;
+        }
+    }
+}
---
+++ leetcodesln_test/JudgeRouteCircleUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class JudgeRouteCircleUnitTest
+    {
+        [TestMethod]
+        public void JudgeRouteCircleTestMethod()
+        {
+            JudgeRouteCircle judgeRouteCircle = new JudgeRouteCircle();
+            Assert.AreEqual(true, judgeRouteCircle.JudgeRouteCircleSolution("UUDDRRLL"));
+            Assert.AreEqual(false, judgeRouteCircle.JudgeRouteCircleSolution(""));
+            Assert.AreEqual(false, judgeRouteCircle.JudgeRouteCircleSolution("UDDUURLRLLRRUDUDLLRLURLRLRLUUDLULRULRLDDDUDDDDLRRDDRDRLRLURRLLRUDURULULRDRDLURLUDRRLRLDDLUUULUDUUUUL"));
+        }
+    }
+}
