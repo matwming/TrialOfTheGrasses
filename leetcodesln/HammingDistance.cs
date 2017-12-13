7c10d1442969be0cd3e11fd5d46cad3e4f3cb0e2
user3301 <stan_gai@Hotmail.com>
2017-12-13 12:38:46 +1100

    hamming distance with test case

---
+++ leetcodesln/HammingDistance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class HammingDistance
+    {
+        public int HammingDistanceSolution(int x, int y)
+        {
+            var xorRes = x ^ y;
+            var xorResToBinary = Convert.ToString(xorRes, 2);
+            int count = 0;
+            foreach (var c in xorResToBinary)
+            {
+                if('1'.Equals(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ leetcodesln_test/HammingDistanceUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class HammingDistanceUnitTest
+    {
+        [TestMethod]
+        public void HammingDistanceTestMethod()
+        {
+            HammingDistance hammingDistance = new HammingDistance();
+            Assert.AreEqual(2, hammingDistance.HammingDistanceSolution(1, 4));
+        }
+    }
+}
