dc970c7d8adf0822b3f75ff4966d0d770cff5ca6
user3301 <stan_gai@Hotmail.com>
2017-12-14 11:11:37 +1100

    self dividing numbers with test case

---
+++ leetcodesln/SelfDividingNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class SelfDividingNumbers
+    {
+        public IList<int> SelfDividingNumbersSolution(int left, int right)
+        {
+            var result = new List<int>();
+            for (int i = left; i <= right; i++)
+            {
+                if(i.ToString().Contains("0"))
+                {
+                    continue;
+                }
+                var temp = new List<int>();
+                Array.ForEach(i.ToString().ToCharArray(), x=> temp.Add(int.Parse(x.ToString())));
+                bool flag = true;
+                foreach (var item in temp)
+                {
+                    if (i % item !=0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag)
+                {
+                    result.Add(i);
+                }
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/SelfDividingNumbersUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class SelfDividingNumbersUnitTest
+    {
+        [TestMethod]
+        public void SelfDividingNumbersTestMethod()
+        {
+            SelfDividingNumbers selfDividingNumbers = new SelfDividingNumbers();
+            IList<int> expected = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22 };
+            //AreEqual method only check equality of the reference, not the contents
+            Assert.AreEqual(expected.Count, selfDividingNumbers.SelfDividingNumbersSolution(1,22).Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], selfDividingNumbers.SelfDividingNumbersSolution(1, 22)[i]);
+            }
+        }
+    }
+}
