a1df54a1225289ed0fecdd5d9f04359d193a2206
user3301 <stan_gai@Hotmail.com>
2017-12-22 10:45:51 +1100

    baseball game with test case

---
+++ leetcodesln/BaseballGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class BaseballGame
+    {
+        public int CalPoints(string[] ops)
+        {
+            Stack<int> pointStack = new Stack<int>();
+            foreach (var item in ops)
+            {
+                int n;
+                if (int.TryParse(item, out n))
+                {
+                    pointStack.Push(n);
+                }
+                else if ("+".Equals(item))
+                {
+                    int lastElementPushedInStack = Convert.ToInt32(pointStack.Pop());
+                    int previousElementPushedInStack = Convert.ToInt32(pointStack.Pop());
+                    int newElementInStack = lastElementPushedInStack + previousElementPushedInStack;
+                    pointStack.Push(previousElementPushedInStack);
+                    pointStack.Push(lastElementPushedInStack);
+                    pointStack.Push(newElementInStack);
+                }
+                else if ("D".Equals(item))
+                {
+                    pointStack.Push(pointStack.Peek()*2);
+                }
+                else if ("C".Equals(item))
+                {
+                    pointStack.Pop();
+                }
+                else
+                {
+                    throw new InvalidOperationException("the operation or number is invalid");
+                }
+            }
+            int result = 0; ;
+            while (pointStack.Count!=0)
+            {
+                result += pointStack.Pop();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/BaseballGameUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class BaseballGameUnitTest
+    {
+        [TestMethod]
+        public void BaseballGameTestMethod()
+        {
+            BaseballGame baseballGame = new BaseballGame();
+            Assert.AreEqual(30,baseballGame.CalPoints(new string[] { "5", "2", "C", "D", "+" }));
+            Assert.AreEqual(5,baseballGame.CalPoints(new string[] { "5"}));
+            Assert.AreEqual(7,baseballGame.CalPoints(new string[] { "5","2"}));
+        }
+    }
+}
