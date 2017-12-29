c171c1e459bc5011760aab7b83c42a031ce999df
Zhelun Gai <stan_gai@Hotmail.com>
2017-12-29 21:15:27 +1100

    degree of an array with test case

---
+++ leetcodesln/DegreeOfAnArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class DegreeOfAnArray
+    {
+        public int FindShortestSubArray(int[] nums)
+        {
+            if (nums.Length==0 || nums==null)
+            {
+                return 0;
+            }
+
+            Dictionary<int, int[]> dictionary = new Dictionary<int, int[]>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dictionary.ContainsKey(nums[i]))
+                {
+                    // int[]: first->count of element, second->index of first time apparence, third-> index of last time apparence
+                    dictionary.Add(nums[i], new int[] {1,i,i});
+                }
+                else
+                {
+                    dictionary[nums[i]][0]++;
+                    dictionary[nums[i]][2] = i;
+                }
+            }
+
+            int degree = int.MinValue;
+            int sub = int.MaxValue;
+            foreach (var value in dictionary.Values)
+            {
+                if (value[0]>degree)
+                {
+                    degree = value[0];
+                    sub = value[2] - value[1] + 1;
+                }
+                else if (value[0]==degree)
+                {
+                    sub = Math.Min(sub, value[2]-value[1]+1);
+                }
+            }
+            return sub;
+        }
+    }
+}
---
+++ leetcodesln_test/FindShortestSubArrayUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindShortestSubArrayUnitTest
+    {
+        [TestMethod]
+        public void FindShortestSubArrayTestMethod()
+        {
+            DegreeOfAnArray degreeOfAnArray = new DegreeOfAnArray();
+            Assert.AreEqual(2, degreeOfAnArray.FindShortestSubArray(new int[] { 1, 2, 2, 3, 1 }));
+            Assert.AreEqual(6, degreeOfAnArray.FindShortestSubArray(new int[] { 1,2,2,3,1,4,2}));
+        }
+    }
+}
