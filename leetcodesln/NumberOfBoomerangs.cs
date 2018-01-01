227f9a48e87844c2967c2362568ab9987f2fdf64
Zhelun Gai <stan_gai@Hotmail.com>
2018-01-01 18:42:02 +1100

    number of boomerangs

--- leetcodesln/NumberOfBoomerangs.cs
+++ leetcodesln/NumberOfBoomerangs.cs
@@ -6,8 +6,39 @@ namespace leetcodesln
     {
         public int NumberOfBoomerangsSln(int[,] points)
         {
-            Dictionary<int[],int> dictionary = new Dictionary<int[], int>();
-            return -1;
+            int dim1 = points.GetLength(0); //get the length of the first dimension
+            int count = 0;
+
+            for (int i = 0; i < dim1; i++)
+            {
+                Dictionary<int, int> dictionary = new Dictionary<int, int>();
+                for (int j = 0; j < dim1; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    int distant = (points[i, 0] - points[j, 0]) * (points[i, 0] - points[j, 0]) + (points[i, 1] - points[j, 1]) * (points[i, 1] - points[j, 1]);
+
+                    if (!dictionary.ContainsKey(distant))
+                    {
+                        dictionary.Add(distant,1);
+                    }
+                    else
+                    {
+                        dictionary[distant]++;
+                    }
+                }
+
+                foreach (var item in dictionary.Values)
+                {
+                    if (item >=2)
+                    {
+                        count += item * (item - 1);
+                    }
+                }
+            }
+            return count;
         }
     }
 }
---
+++ leetcodesln_test/NumberOfBoomerangsUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class NumberOfBoomerangsUnitTest
+    {
+        [TestMethod]
+        public void NumberOfBoomerangsTestMethod()
+        {
+            NumberOfBoomerangs numberOfBoomerangs = new NumberOfBoomerangs();
+            Assert.AreEqual(2, numberOfBoomerangs.NumberOfBoomerangsSln(new int[,] { {0,0},{1,0},{2,0} }));
+        }
+    }
+}
