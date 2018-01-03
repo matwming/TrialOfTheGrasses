bc8fdf61a0be4b3b325097d439b0e5a30c5304c1
user3301 <stan_gai@Hotmail.com>
2018-01-03 14:34:11 +1100

    count binary substring (NB)

---
+++ leetcodesln/CountBinarySubstrings.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcodesln
+{
+    public class CountBinarySubstrings
+    {
+        public int CountBinarySubstringsSln(string s)
+        {
+            int previous = 0;
+            int current = 1;
+            int count = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i]==s[i-1])
+                {
+                    current++;
+                }
+                else
+                {
+                    previous = current;
+                    current = 1;
+                }
+                if (previous>=current)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
