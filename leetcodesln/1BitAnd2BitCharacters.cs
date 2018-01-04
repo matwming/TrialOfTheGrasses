147555f0971ab8556f404173f9e0e219b660e626
user3301 <stan_gai@Hotmail.com>
2018-01-04 12:57:47 +1100

    1 bit and 2 bit characters

---
+++ leetcodesln/1BitAnd2BitCharacters.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcodesln
+{
+    public class _1BitAnd2BitCharacters
+    {
+        public bool IsOneBitCharacter(int[] bits)
+        {
+            int i = 0;
+            while (i<bits.Length-2)
+            {
+                if (bits[i]==0)
+                {
+                    i++;
+                }
+                else
+                {
+                    i++;
+                    i++;
+                }
+            }
+            if (bits[i]==1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/1BitAnd2BitCharactersUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class _1BitAnd2BitCharactersUnitTest
+    {
+        [TestMethod]
+        public void _1BitAnd2BitCharactersTestMethod()
+        {
+            _1BitAnd2BitCharacters _1BitAnd2BitCharacters = new _1BitAnd2BitCharacters();
+            Assert.AreEqual(true, _1BitAnd2BitCharacters.IsOneBitCharacter(new int[] { 1, 0, 0 }));
+            Assert.AreEqual(false,_1BitAnd2BitCharacters.IsOneBitCharacter(new int[] { 1,1,1,0}));
+            Assert.AreEqual(true, _1BitAnd2BitCharacters.IsOneBitCharacter(new int[] { 0 }));
+            Assert.AreEqual(true, _1BitAnd2BitCharacters.IsOneBitCharacter(new int[] { 0, 0 }));
+        }
+    }
+}
