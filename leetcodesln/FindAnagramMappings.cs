748068e0857d2cfc34d0c9198be3a0f5e10d4ccf
user3301 <stan_gai@Hotmail.com>
2018-01-10 09:10:54 +1100

    find anagram mappings (dictionary(hashtable))

---
+++ leetcodesln/FindAnagramMappings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcodesln
+{
+    public class FindAnagramMappings
+    {
+        public int[] AnagramMappings(int[] A, int[] B)
+        {
+            Dictionary<int, int> HashMappingB = new Dictionary<int, int>();
+            for (int i = 0; i < B.Length; i++)
+            { 
+                HashMappingB.Add(i,B[i]);
+            }
+            int[] indexMapping = new int[A.Length];
+            for (int i = 0; i < indexMapping.Length; i++)
+            {
+                foreach(KeyValuePair<int,int> pair in HashMappingB)
+                {
+                    if (pair.Value == A[i])
+                    {
+                        indexMapping[i] = pair.Key;
+                        break;
+                    }
+                }
+            }
+            return indexMapping;
+        }
+    }
+}
---
+++ leetcodesln_test/FindAnagramMappingsUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindAnagramMappingsUnitTest
+    {
+        [TestMethod]
+        public void FindAnagramMappingsTestMethod()
+        {
+            FindAnagramMappings findAnagramMappings = new FindAnagramMappings();
+            int[] expected = { 1, 4, 3, 2, 0 };
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], findAnagramMappings.AnagramMappings(new int[] { 12, 28, 46, 32, 50 }, new int[] { 50, 12, 32, 46, 28 })[i]);
+            }
+
+            int[] expected2 = {0,0};
+            for (int i = 0; i < expected2.Length; i++)
+            {
+                Assert.AreEqual(expected2[i], findAnagramMappings.AnagramMappings(new int[] { 40,40},new int[] { 40,40})[i]);
+            }
+        }
+
+    }
+}
