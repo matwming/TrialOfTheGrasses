bf417fa34003bc2a9348a021dddfd6236ef22d92
user3301 <stan_gai@Hotmail.com>
2018-01-09 15:15:37 +1100

    flood fill (dfs)

---
+++ leetcodesln/FloodFill.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcodesln
+{
+    public class FloodFill
+    {
+        public int[,] FloodFillSln(int[,] image, int sr, int sc, int newColor)
+        {
+            if (image[sr,sc] == newColor)
+            {
+                return image;
+            }
+            int startingPixel = image[sr, sc];
+            image[sr, sc] = newColor;
+            int[,] floodFilledMatrix = PerformFloodFill(image, sr, sc, newColor, startingPixel);
+            return floodFilledMatrix;
+        }
+
+        private int[,] PerformFloodFill(int[,] image, int sr, int sc, int newColor,int startingPixel)
+        {
+            if (sr-1 >=0 && image[sr-1,sc]==startingPixel)
+            {
+                image[sr - 1, sc] = newColor;
+                PerformFloodFill(image, sr - 1, sc, newColor,startingPixel);
+            }
+            if (sr+1<image.GetLength(0) && image[sr+1,sc] == startingPixel)
+            {
+                image[sr + 1, sc] = newColor;
+                PerformFloodFill(image, sr + 1, sc, newColor,startingPixel);
+            }
+            if(sc-1>=0 && image[sr,sc-1] == startingPixel)
+            {
+                image[sr, sc - 1] = newColor;
+                PerformFloodFill(image, sr, sc - 1, newColor,startingPixel);
+            }
+            if (sc + 1 < image.GetLength(1) && image[sr, sc + 1] == startingPixel)
+            {
+                image[sr, sc + 1] = newColor;
+                PerformFloodFill(image, sr, sc + 1, newColor,startingPixel);
+            }
+            return image;
+        }
+    }
+}
---
+++ leetcodesln_test/FloodFillUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FloodFillUnitTest
+    {
+        [TestMethod]
+        public void FloodFillTestMethod()
+        {
+            FloodFill floodFill = new FloodFill();
+            int[,] result = new int[,] { { 2, 2, 2 }, { 2, 2, 0 }, { 2, 0, 1 } };
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Assert.AreEqual(result[i, j], floodFill.FloodFillSln
+                        (new int[,] { { 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 } }, 1, 1, 2)[i, j]);
+                }
+            }
+
+            int[,] result2 = new int[,] { { 0,0,0}, { 0,1,1} };
+            for (int i = 0; i < result2.GetLength(0); i++)
+            {
+                for (int j = 0; j < result2.GetLength(1); j++)
+                {
+                    Assert.AreEqual(result2[i, j], floodFill.FloodFillSln
+                        (new int[,] { { 0,0,0}, { 0, 1, 1 } }, 1, 1, 1)[i, j]);
+                }
+            }
+        }
+    }
+}
