27e6cace30bfa46234d0e8248538c259e4451515
user3301 <stan_gai@Hotmail.com>
2018-01-10 15:14:59 +1100

    largest number at least twice of others (easy)

---
+++ leetcodesln/LargestNumberAtLeastTwiceOfOthers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class LargestNumberAtLeastTwiceOfOthers
+    {
+        public int DominantIndex(int[] nums)
+        {
+            int[] numsCopy = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numsCopy[i] = nums[i];
+            }
+            Array.Sort(numsCopy);
+            return numsCopy[numsCopy.Length - 1] - numsCopy[numsCopy.Length - 2] >= numsCopy[numsCopy.Length-2] ? Array.IndexOf(nums, numsCopy[numsCopy.Length - 1]) : -1;
+        }
+    }
+}
---
+++ leetcodesln_test/LargestNumberAtLeastTwiceOfOthersUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LargestNumberAtLeastTwiceOfOthersUnitTest
+    {
+        [TestMethod]
+        public void LargestNumberAtLeastTwiceOfOthersTestMethod()
+        {
+            LargestNumberAtLeastTwiceOfOthers largestNumberAtLeastTwiceOfOthers = new LargestNumberAtLeastTwiceOfOthers();
+            //Assert.AreEqual(1,largestNumberAtLeastTwiceOfOthers.DominantIndex(new int[] { 3,6,1,0}));
+            //Assert.AreEqual(-1,largestNumberAtLeastTwiceOfOthers.DominantIndex(new int[] { 1,2,3,4}));
+            Assert.AreEqual(3,largestNumberAtLeastTwiceOfOthers.DominantIndex(new int[] { 0,0,0,1}));
+        }
+    }
+}
