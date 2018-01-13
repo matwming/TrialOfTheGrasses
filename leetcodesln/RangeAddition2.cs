12f47974d490fe53a9ddfb02735230f436cbd4cf
Zhelun Gai <stan_gai@Hotmail.com>
2018-01-13 20:21:22 +1100

    range addition 2

---
+++ leetcodesln/RangeAddition2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class RangeAddition2
+    {
+        public int MaxCount(int m, int n, int[,] ops)
+        {
+            int maxRowCovered = m;
+            int maxColCovered = n;
+
+            for (int i = 0; i < ops.GetLength(0); i++)
+            {
+                maxRowCovered = Math.Min(maxRowCovered, ops[i, 0]);
+                maxColCovered = Math.Min(maxColCovered,ops[i,1]);
+            }
+            return maxColCovered * maxRowCovered;
+        }
+    }
+}
---
+++ leetcodesln_test/RangeAddition2UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RangeAddition2UnitTest
+    {
+        [TestMethod]
+        public void RangeAddition2TestMethod()
+        {
+            RangeAddition2 rangeAddition2 = new RangeAddition2();
+            Assert.AreEqual(4, rangeAddition2.MaxCount(3,3,new int[,] { { 2,2},{2,3 } }));
+        }
+    }
+}
