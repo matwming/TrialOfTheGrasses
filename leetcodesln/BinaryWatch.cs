c35bbfe5a5f1e2ad1958ca4f592de7fa9375af43
Zhelun Gai <stan_gai@Hotmail.com>
2018-01-19 12:10:01 +1100

    binary watch (back tracking, test case compare two list)

---
+++ leetcodesln/BinaryWatch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class BinaryWatch
+    {
+        public IList<string> ReadBinaryWatch(int num)
+        {
+           
+            IList<string> result = new List<string>();
+
+            int[] hourArray = new int[] { 8, 4, 2, 1 };
+            int[] minArray = new int[] { 32, 16, 8, 4, 2, 1 };
+            for (int i = 0; i <= num; i++)
+            {
+                List<int> hourList = NumsPicker(hourArray, i);
+                List<int> minList = NumsPicker(minArray, num - i);
+                foreach (var hour in hourList)
+                {
+                    if (hour >= 12)
+                    {
+                        continue;
+                    }
+                    foreach (var min in minList)
+                    {
+                        if (min>=60)
+                        {
+                            continue;
+                        }
+                        string time = hour.ToString();
+                        time += ":";
+                        time += min < 10 ? "0" + min.ToString() : min.ToString();
+                        result.Add(time);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private List<int> NumsPicker(int[] digitArray, int count)
+        {
+            List<int> res = new List<int>();
+            NumsPickerHelper(digitArray,count,0,0,res);
+            return res;
+        }
+
+        private void NumsPickerHelper(int[] digitArray, int count, int position, int sum, List<int> res)
+        {
+            if (count == 0)
+            {
+                res.Add(sum);
+                return;
+            }
+            for (int i = position; i < digitArray.Length; i++)
+            {
+                NumsPickerHelper(digitArray, count - 1, i + 1, sum + digitArray[i], res);
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/BinaryWatchUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class BinaryWatchUnitTest
+    {
+        [TestMethod]
+        public void BinaryWatchTestMethod()
+        {
+            BinaryWatch binaryWatch = new BinaryWatch();
+            string[] sample = { "1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32" };
+            IList<string> expected = new List<string>();
+            foreach (var item in sample)
+            {
+                expected.Add(item);
+            }
+            IList<string> actual = new List<string>();
+            actual = binaryWatch.ReadBinaryWatch(1);
+            Assert.AreEqual(true, Enumerable.SequenceEqual(expected.OrderBy(t=>t),actual.OrderBy(t=>t)));
+        }
+    }
+}
