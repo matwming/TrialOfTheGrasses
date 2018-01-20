4d578a9706e4e072e41ead50753740cc53900b83
Zhelun Gai <stan_gai@Hotmail.com>
2018-01-20 11:35:43 +1100

    find smallest letter greater than target (brutal force but accepted answer)

---
+++ leetcodesln/FindSmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcodesln
+{
+    public class FindSmallestLetterGreaterThanTarget
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            foreach (var letter in letters)
+            {
+                if (letter-target>0)
+                {
+                    return letter;
+                }
+            }
+            return letters[0];
+        }
+    }
+}
---
+++ leetcodesln_test/FindSmallestLetterGreaterThanTargetUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindSmallestLetterGreaterThanTargetUnitTest
+    {
+        [TestMethod]
+        public void FindSmallestLetterGreaterThanTargetTestMethod()
+        {
+            FindSmallestLetterGreaterThanTarget findSmallestLetterGreaterThanTarget = new FindSmallestLetterGreaterThanTarget();
+            Assert.AreEqual('c',findSmallestLetterGreaterThanTarget.NextGreatestLetter(new char[]{ 'c','f','j'},'a'));
+            Assert.AreEqual('f',findSmallestLetterGreaterThanTarget.NextGreatestLetter(new char[]{ 'c','f','j'},'c'));
+            Assert.AreEqual('f',findSmallestLetterGreaterThanTarget.NextGreatestLetter(new char[]{ 'c','f','j'},'d'));
+            Assert.AreEqual('j',findSmallestLetterGreaterThanTarget.NextGreatestLetter(new char[]{ 'c','f','j'},'g'));
+            Assert.AreEqual('c',findSmallestLetterGreaterThanTarget.NextGreatestLetter(new char[]{ 'c','f','j'},'j'));
+            Assert.AreEqual('c',findSmallestLetterGreaterThanTarget.NextGreatestLetter(new char[]{ 'c','f','j'},'k'));
+        }
+    }
+}
