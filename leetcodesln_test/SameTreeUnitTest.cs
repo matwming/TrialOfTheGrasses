604531bc43dc38d4d5e94cd009f3d75390d19616
Zhelun Gai <stan_gai@Hotmail.com>
2018-01-26 15:02:54 +1100

    same tree (key:find base case)

---
+++ leetcodesln_test/SameTreeUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SameTreeUnitTest
+    {
+        [TestMethod]
+        public void SameTreeTestMethod()
+        {
+            SameTree sameTree = new SameTree();
+            TreeNode root = new TreeNode(1)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(3)
+            };
+         
+
+            TreeNode root1 = new TreeNode(1)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(3)
+            };
+
+            Assert.AreEqual(true, sameTree.IsSameTree(root, root1));
+            
+        }
+
+    }
+}
