a9678284e092d448de7534c13f77d7618d73f09a
Zhelun Gai <stan_gai@Hotmail.com>
2018-03-07 23:06:47 +1100

    :heavy_plus_sign:jewels and stones

---
+++ leetcodesln/JewelsAndStones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class JewelsAndStones
+    {
+        public int NumJewelsInStones(string J , string S)
+        {
+            int num = 0;
+            for (int i = 0; i < J.Length; i++)
+            {
+                for (int j = 0; j < S.Length; j++)
+                {
+                    if (J[i] == S[j])
+                    {
+                        num++;
+                    }
+                }
+            }
+            return num;
+        }
+    }
+}
---
+++ leetcodesln_test/JewelsAndStonesUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class JewelsAndStonesUnitTest
+    {
+        [TestMethod]
+        public void NumJewelsInStonesMethodTest()
+        {
+            JewelsAndStones jewelsAndStones = new JewelsAndStones();
+            Assert.AreEqual(3, jewelsAndStones.NumJewelsInStones("aA", "aAAbbbb"));
+            Assert.AreEqual(0, jewelsAndStones.NumJewelsInStones("z", "ZZ"));
+        }
+    }
+}
