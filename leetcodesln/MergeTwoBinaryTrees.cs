ab0923fd41a2e20c82fc6bc30ce898c6993e1d5f
CJ\zhelun <stan_gai@Hotmail.com>
2018-03-08 12:53:36 +1100

    :heavy_plus_sign:merge two binary trees without test case

---
+++ leetcodesln/MergeTwoBinaryTrees.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class MergeTwoBinaryTrees
+    {
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null) return null;
+
+            TreeNode newNode = new TreeNode((t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val))
+            {
+                left = MergeTrees(t1?.left, t2?.left),
+                right = MergeTrees(t1?.right, t2?.right)
+            };
+
+            return newNode;
+        }
+    }
+}
---
+++ leetcodesln_test/MergeTwoBinaryTreeUnitTest.cs
@@ -0,0 +1,78 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MergeTwoBinaryTreeUnitTest
+    {
+        [TestMethod]
+        public void MergesTreesMethodTest()
+        {
+            MergeTwoBinaryTrees mergeTwoBinaryTrees = new MergeTwoBinaryTrees();
+            TreeNode t1 = new TreeNode(1)
+            {
+                left = new TreeNode(3)
+                {
+                    left = new TreeNode(5),
+                    right = null
+                },
+                right = new TreeNode(2)
+                {
+                    left = null,
+                    right = null
+                },
+            };
+            TreeNode t2 = new TreeNode(2)
+            {
+                left = new TreeNode(1)
+                {
+                    left = null,
+                    right = new TreeNode(4)
+                    {
+                        left = null,
+                        right = null
+                    }
+                },
+                right = new TreeNode(3)
+                {
+                    left = null,
+                    right = new TreeNode(7)
+                    {
+                        left = null,
+                        right = null
+                    }
+                }
+            };
+            TreeNode mergedTree = new TreeNode(3)
+            {
+                left = new TreeNode(4)
+                {
+                    left = new TreeNode(5)
+                    {
+                        left = null,
+                        right = null
+                    },
+                    right = new TreeNode(4)
+                    {
+                        left = null,
+                        right = null
+                    }
+                },
+                right = new TreeNode(5)
+                {
+                    left = null,
+                    right = new TreeNode(7)
+                    {
+                        left = null,
+                        right = null
+                    }
+                }
+            };
+           
+        }
+    }
+}
