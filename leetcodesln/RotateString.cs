d7675108e68257786e68a3bff6792a05862c0a68
CJ\zhelun <stan_gai@Hotmail.com>
2018-03-13 15:34:45 +1100

    :heavy_plus_sign:rotate string

---
+++ leetcodesln/RotateString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class RotateString
+    {
+        public bool RotateStringSln(string A, string B)
+        {
+            if (A.Length != B.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                string newString = A.Substring(i) + A.Substring(0,i);
+                if (newString == B)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/RotateStringUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RotateStringUnitTest
+    {
+        [TestMethod]
+        public void RotateStringTestMethod()
+        {
+            RotateString rotateString = new RotateString();
+
+            Assert.AreEqual(true, rotateString.RotateStringSln("abcde", "cdeab"));
+            Assert.AreEqual(false, rotateString.RotateStringSln("abcde", "abced"));
+        }
+    }
+}
