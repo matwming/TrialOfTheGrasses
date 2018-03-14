14475285a77a43c6e28239664d8044fef62a3531
CJ\zhelun <stan_gai@Hotmail.com>
2018-03-14 11:19:25 +1100

    :heavy_plus_sign:toeplitz matrix

---
+++ leetcodesln/ToeplitzMatrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ToeplitzMatrix
+    {
+        public bool IsToeplitzMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i + 1 < matrix.GetLength(0) && j + 1 < matrix.GetLength(1))
+                    {
+                        if (matrix[i, j] != matrix[i + 1, j + 1])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/ToeplitzMatrixUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ToeplitzMatrixUnitTest
+    {
+        [TestMethod]
+        public void ToeplitzMatrixTestMethod()
+        {
+            ToeplitzMatrix toeplitzMatrix = new ToeplitzMatrix();
+            int[,] matrix1 = { { 1,2,3,4 }, { 5,1,2,3 }, { 9,5,1,2 } };
+            int[,] matrix2 = { { 1, 2 }, { 2, 2 } };
+            Assert.AreEqual(true, toeplitzMatrix.IsToeplitzMatrix(matrix1));
+            Assert.AreEqual(false, toeplitzMatrix.IsToeplitzMatrix(matrix2));
+        }
+    }
+}
