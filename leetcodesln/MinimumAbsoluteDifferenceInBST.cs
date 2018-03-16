912223b301a98b626c9715e040601e5431af8fea
user3301 <stan_gai@Hotmail.com>
2018-03-16 23:20:16 +1100

    :construction: wip

---
+++ leetcodesln/MinimumAbsoluteDifferenceInBST.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class MinimumAbsoluteDifferenceInBST
+    {
+        public int GetMinimumDifference(TreeNode root)
+        {
+
+            if (root.left == null && root.right == null)
+            {
+                return 0;
+            }
+
+        }
+
+        private int getDifference(TreeNode root)
+        {
+            int smallest = Int32.MaxValue;
+            if (root.left == null && root.right == null)
+            {
+                return smallest;
+            }
+            if (true)
+            {
+
+            }
+        }
+    }
+}
