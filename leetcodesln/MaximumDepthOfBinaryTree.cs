fb5dee099bd90197450960540efc9e8aa2825e3f
CJ\zhelun <stan_gai@Hotmail.com>
2018-03-19 16:17:49 +1100

    :construction:wip

---
+++ leetcodesln/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class MaximumDepthOfBinaryTree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            List<int> list = new List<int>();
+            int currentDpeth = 0;
+            if (root == null)
+            {
+                return 0;
+            }
+            GetNodeDepth(root, currentDpeth, ref list);
+            if (root.left != null)
+            {
+                GetNodeDepth(root.left, currentDpeth, ref list);
+            }
+            if (root.right != null)
+            {
+                GetNodeDepth(root.right, currentDpeth, ref list);
+            }
+        }
+
+        private void GetNodeDepth(TreeNode node, int preDepth, ref List<int> list)
+        {
+            if (node != null)
+            {
+                GetNodeDepth(node.left, preDepth, ref list);
+            }
+        }
+    }
+}
