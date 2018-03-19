b27703b29a8b4e3f5ce41aea2da7af82b19c781e
CJ\zhelun <stan_gai@Hotmail.com>
2018-03-19 13:40:46 +1100

    :heavy_plus_sign:prime number of sets in binary representation(prime integer checker incl)

---
+++ leetcodesln/PrimeNumberOfSetBitsInBinaryRepresentation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class PrimeNumberOfSetBitsInBinaryRepresentation
+    {
+        public int CountPrimeSetBits(int L, int R)
+        {
+            int count = 0;
+            for (int i = L; i <= R; i++)
+            {
+                char[] charArr =  Convert.ToString(i, 2).ToCharArray();
+                int primeCount = 0;
+                foreach (var character in charArr)
+                {
+                    if (character == '1')
+                    {
+                        primeCount++;
+                    }
+                }
+                if (IsPrime(primeCount))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(candidate); i++)
+            {
+                if (candidate % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/PrimeNumberOfSetsInBinaryRepresentationUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PrimeNumberOfSetsInBinaryRepresentationUnitTest
+    {
+        [TestMethod]
+        public void PrimeNumberOfSetsInBinaryRepresentationTestMethod()
+        {
+            PrimeNumberOfSetBitsInBinaryRepresentation pndsbibr = new PrimeNumberOfSetBitsInBinaryRepresentation();
+
+            Assert.AreEqual(4, pndsbibr.CountPrimeSetBits(6, 10));
+            Assert.AreEqual(5, pndsbibr.CountPrimeSetBits(10, 15));
+            Assert.AreEqual(21, pndsbibr.CountPrimeSetBits(567, 607));
+        }
+    }
+}
