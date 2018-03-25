92f48356d26c7ba590165157d531066d089a38bf
user3301 <stan_gai@Hotmail.com>
2018-03-25 23:36:30 +1100

    wip

---
+++ leetcodesln_test/AddBinaryUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class AddBinaryUnitTest
+    {
+        [TestMethod]
+        public void AddBinaryTestMethod()
+        {
+            AddBinary ab = new AddBinary();
+
+            //Assert.AreEqual("100", ab.AddBinarysln("11", "1"));
+            Assert.AreEqual("11110", ab.AddBinarysln("1111", "1111"));
+        }
+    }
+}
