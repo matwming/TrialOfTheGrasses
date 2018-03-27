63de587de5e9d0155d9423b7e5f8da2eaef57a68
CJ\zhelun <stan_gai@Hotmail.com>
2018-03-27 16:00:53 +1100

    wip

---
+++ leetcodesln/TwoSumIV.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class TwoSumIV
+    {
+        public bool FindTarget(TreeNode root, int k)
+        {
+            if (root == null) 
+            {
+                return false;
+            }
+
+            if (Math.Abs(root.val -k) >= root.val)
+            {
+                return FindRecursively(root.right, Math.Abs(root.val - k));
+            }
+            return FindRecursively(root.left, Math.Abs(root.val - k));
+        }
+
+        private bool FindRecursively(TreeNode node, int target)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.val == target)
+            {
+                return true;
+            }
+            if (node.val > target)
+            {
+                return FindRecursively(node.left, target);
+            }
+            if (node.val < target)
+            {
+                return FindRecursively(node.right, target);
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/FindTargetUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindTargetUnitTest
+    {
+        [TestMethod]
+        public void FindTargetTestMethod()
+        {
+            TwoSumIV tsiv = new TwoSumIV();
+
+            TreeNode tree1 = new TreeNode(5)
+            {
+                left = new TreeNode(3)
+                {
+                    left = new TreeNode(2)
+                    {
+                        left = null,
+                        right = null
+                    },
+                    right = new TreeNode(4)
+                    {
+                        left = null,
+                        right = null
+                    }
+
+                },
+                right = new TreeNode(6)
+                {
+                    left = null,
+                    right = new TreeNode(7)
+                    {
+                        left = null,
+                        right = null
+                    }
+
+                }
+            };
+
+            Assert.AreEqual(true, tsiv.FindTarget(tree1, 9));
+            Assert.AreEqual(false, tsiv.FindTarget(tree1, 28));
+        }
+    }
+}
