2fedd60f224da7736323d9b577058bcd8398d450
CJ\zhelun <stan_gai@Hotmail.com>
2018-04-04 09:18:59 +1000

    :100: unique morse code words

---
+++ leetcodesln/UniqueMorseCodeWords.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class UniqueMorseCodeWords
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] morseCodeTable = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < words.Length; i++)
+            {
+                foreach (var character in words[i])
+                {
+                    sb.Append(morseCodeTable[character - 97]);
+                }
+                words[i] = sb.ToString();
+                sb = new StringBuilder();
+            }
+            HashSet<string> set = new HashSet<string>();
+            foreach (var word in words)
+            {
+                set.Add(word);
+            }
+            return set.Count;
+        }
+    }
+}
---
+++ leetcodesln_test/UniqueMorseCodeWordsUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class UniqueMorseCodeWordsUnitTest
+    {
+        [TestMethod]
+        public void UniqueMorseCodeWordsTestMethod()
+        {
+            UniqueMorseCodeWords umcw = new UniqueMorseCodeWords();
+
+            Assert.AreEqual(2, umcw.UniqueMorseRepresentations(new string[] { "gin", "zen", "gig", "msg" }));
+        }
+    }
+}
