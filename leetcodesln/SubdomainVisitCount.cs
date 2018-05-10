9798cd143f470b8331175fea3a17395c785a8cab
CJ\zhelun <stan_gai@Hotmail.com>
2018-05-10 11:16:17 +1000

    subdomain visit count

---
+++ leetcodesln/SubdomainVisitCount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class SubdomainVisitCount
+    {
+        public List<string> SubdomainVisits(string[] cpdomains)
+        {
+            List<string> result = new List<string>();
+            Dictionary<string, int> resultMap = new Dictionary<string, int>();
+            foreach (var domainVisit in cpdomains)
+            {
+                string[] subdomains = ParseSubdomains(domainVisit, out int count);
+                foreach (var subdomain in subdomains)
+                {
+                    if (resultMap.ContainsKey(subdomain))
+                    {
+                        resultMap[subdomain] += count;
+                    }
+                    else
+                    {
+                        resultMap.Add(subdomain, count);
+                    }
+                }
+            }
+            foreach (KeyValuePair<string, int> item in resultMap)
+            {
+                result.Add(item.Value + " " + item.Key);
+            }
+
+            return result;
+            
+        }
+
+        private string[] ParseSubdomains(string input, out int count)
+        {
+            List<string> resultList = new List<string>();
+            int spacePosition = input.IndexOf(' ');
+            count = Int32.Parse(input.Substring(0, spacePosition));
+            var subdomain = input.Substring(spacePosition+1, input.Length - spacePosition-1);
+            while (subdomain.IndexOf('.') != -1)
+            {
+                resultList.Add(subdomain);
+                subdomain = subdomain.Substring(subdomain.IndexOf('.') +1, subdomain.Length - subdomain.IndexOf('.') -1);
+            }
+            resultList.Add(subdomain);
+            string[] result = new string[resultList.Count];
+            resultList.CopyTo(result);
+
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/SubdomainVisitCountUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SubdomainVisitCountUnitTest
+    {
+        [TestMethod]
+        public void SubdomainVisitCountTestMethod()
+        {
+            SubdomainVisitCount sdbc = new SubdomainVisitCount();
+            Assert.AreEqual(null, sdbc.SubdomainVisits(new string[]{ "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"}));
+        }
+    }
+}
