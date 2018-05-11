b42e85165a19b5a773e9a49d9cefe777f99d1278
CJ\zhelun <stan_gai@Hotmail.com>
2018-05-11 17:08:02 +1000

    add strings (two loop indexes)

---
+++ leetcodesln/AddStrings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class AddStrings
+    {
+        public string AddStringsSln(string num1, string num2)
+        {
+            int carry = 0;
+            StringBuilder sb = new StringBuilder(1024);
+            for (int s1 = num1.Length -1, s2 = num2.Length -1; s1 >=0 || s2 >=0 ; s1--, s2--)
+            {
+                int sum = 0;
+                if (s1 >= 0) { sum += num1[s1] - '0'; }
+                if (s2 >= 0) { sum += num2[s2] - '0'; }
+                if (sum + carry >= 10)
+                {
+                    sb.Append(sum - 10 + carry);
+                    carry = 1;
+                }
+                else
+                {
+                    sb.Append(sum + carry);
+                    carry = 0;
+                }
+            }
+            if (carry != 0)
+            {
+                sb.Append(carry);
+            }
+            char[] arr = sb.ToString().ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
---
+++ leetcodesln_test/AddStringsUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class AddStringsUnitTest
+    {
+        [TestMethod]
+        public void AddStringsTestMethod()
+        {
+            AddStrings addStrings = new AddStrings();
+            Assert.AreEqual("23", addStrings.AddStringsSln("11", "12"));
+            Assert.AreEqual("108", addStrings.AddStringsSln("9", "99"));
+        }
+    }
+}
