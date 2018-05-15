25b739e2ce5f8dbcda4c77e1dbf8b9a67d367685
CJ\zhelun <stan_gai@Hotmail.com>
2018-05-15 10:35:47 +1000

    shortest distance to a character

---
+++ leetcodesln/ShortestDistanceToACharacter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ShortestDistanceToACharacter
+    {
+        public int[] ShrtestToChar(String S, char C)
+        {
+            int[] result = new int[S.Length];
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i]==C)
+                {
+                    result[i] = 0;
+                    continue;
+                }
+                int count = 0;
+                for (int left = i, right = i; left >= 0 || right <= S.Length; left=i-count, right = i+count)
+                {
+                    if (left >=0 && S[left] == C )
+                    {
+                        result[i] = count;
+                        break;
+                    }
+                    if (right < S.Length && S[right] == C)
+                    {
+                        result[i] = count;
+                        break;
+                    }
+                    count++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/ShortestDistanceToACharacterUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ShortestDistanceToACharacterUnitTest
+    {
+        [TestMethod]
+        public void ShortestDistanceToACharacterTestMethod()
+        {
+            ShortestDistanceToACharacter shortestDistanceToACharacter = new ShortestDistanceToACharacter();
+            //int[] expected = new int[] { 3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0 };
+            //int[] actual = shortestDistanceToACharacter.ShrtestToChar("loveleetcode", 'e');
+            //for (int i = 0; i < expected.Length; i++)
+            //{
+            //    Assert.AreEqual(expected[i], actual[i]);
+            //}
+
+            int[] expected2 = new int[] { 1, 0, 1, 2 };
+            int[] actual2 = shortestDistanceToACharacter.ShrtestToChar("abaa", 'b');
+            for (int i = 0; i < expected2.Length; i++)
+            {
+                Assert.AreEqual(expected2[i], actual2[i]);
+            }
+        }
+    }
+}
