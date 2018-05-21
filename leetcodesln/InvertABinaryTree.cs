c3f80b2f9f86695b111ef7456c7f92aef5b3a991
CJ\zhelun <stan_gai@Hotmail.com>
2018-05-21 16:01:00 +1000

    invert a binary tree

---
+++ leetcodesln/InvertABinaryTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class InvertABinaryTree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            var temp = root.left;
+            root.left = InvertTree(root.right);
+            root.right = InvertTree(temp);
+            return root;
+        }
+    }
+}
---
+++ leetcodesln_test/InvertABinaryTreeUnitTest.cs
@@ -0,0 +1,79 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class InvertABinaryTreeUnitTest
+    {
+        //todo: test case for verify two binary tree are the same
+        [TestMethod]
+        public void InvertABinaryTreeTestMethod()
+        {
+            InvertABinaryTree invertABinaryTree = new InvertABinaryTree();
+            var input = new TreeNode(4)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(1)
+                    {
+                        left = null,
+                        right = null
+                    },
+                    right = new TreeNode(3)
+                    {
+                        left = null,
+                        right = null
+                    }
+                },
+                right = new TreeNode(7)
+                {
+                    left = new TreeNode(6)
+                    {
+                        left = null,
+                        right = null
+                    },
+                    right = new TreeNode(9)
+                    {
+                        left = null,
+                        right = null
+                    }
+                }
+            };
+
+            var expected = new TreeNode(4)
+            {
+                left = new TreeNode(7)
+                {
+                    left = new TreeNode(9)
+                    {
+                        left = null,
+                        right = null
+                    },
+                    right = new TreeNode(6)
+                    {
+                        left = null,
+                        right = null
+                    }
+                },
+                right = new TreeNode(2)
+                {
+                    left = new TreeNode(3)
+                    {
+                        left = null,
+                        right = null
+                    },
+                    right = new TreeNode(1)
+                    {
+                        left = null,
+                        right = null
+                    }
+                }
+            };
+
+        }
+    }
+}
