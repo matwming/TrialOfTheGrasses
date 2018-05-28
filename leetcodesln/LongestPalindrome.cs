c3be9b6c31c2ad3f8320b55ea107cb6e9165c1e1
CJ\zhelun <stan_gai@Hotmail.com>
2018-05-29 09:43:13 +1000

    longest palindrome wip (case 2 failed)

---
+++ leetcodesln/LongestPalindrome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class LongestPalindrome
+    {
+        public int FindLongestPalindrome(string s)
+        {
+            int longestPalindrome = 0;
+            var dictionary = ToDictionary(s);
+            foreach (var item in dictionary)
+            {
+                if (item.Value % 2==0)
+                {
+                    longestPalindrome += item.Value;
+                }
+            }
+            if (longestPalindrome < s.Length)
+            {
+                longestPalindrome++;
+            }
+            return longestPalindrome;
+        }
+
+        private ConcurrentDictionary<char,int> ToDictionary(string s)
+        {
+            ConcurrentDictionary<char, int> result = new ConcurrentDictionary<char, int>();
+            foreach (var character in s.ToCharArray())
+            {
+                result.AddOrUpdate(character, 1, (key, oldValue) => oldValue + 1);
+            }
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/LongestPalindromeUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class LongestPalindromeUnitTest
+    {
+        [TestMethod]
+        public void LongestPalindromeTestMethod()
+        {
+            LongestPalindrome lp = new LongestPalindrome();
+            int expected = 7;
+            int actual = lp.FindLongestPalindrome("abccccdd");
+            Assert.AreEqual(expected, actual);
+
+            //case 2
+            int expected2 = 3;
+            int acutal = lp.FindLongestPalindrome("ccc");
+            Assert.AreEqual(expected2, actual);
+        }
+    }
+}
