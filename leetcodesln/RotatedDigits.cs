f601bef6bfd8b87765b373bdfdbcbcf1a7ba165d
user3301 <stan_gai@Hotmail.com>
2018-06-10 23:10:53 +1000

    wip

---
+++ leetcodesln/RotatedDigits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace leetcodesln
+{
+    public class RotatedDigits
+    {
+        public int RotatedDigitsSln(int N)
+        {
+            for (int i = 0; i <= N; i++)
+            {
+                var str = N.ToString().ToCharArray();
+                str.Union<>
+            }
+        }
+    }
+}
