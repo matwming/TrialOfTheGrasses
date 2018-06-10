e3ab8cadc5c7e05fbf8554dd61c0264af4dd7b29
user3301 <stan_gai@Hotmail.com>
2018-06-11 09:26:42 +1000

    rotated digits

--- leetcodesln/RotatedDigits.cs
+++ leetcodesln/RotatedDigits.cs
@@ -7,13 +7,25 @@ namespace leetcodesln
 {
     public class RotatedDigits
     {
+        public static readonly char[] INVALIDNUMBERS = { '0', '1', '8'};
+
         public int RotatedDigitsSln(int N)
         {
+            int count = 0;
             for (int i = 0; i <= N; i++)
             {
-                var str = N.ToString().ToCharArray();
-                str.Union<>
+                var tmp = i.ToString().ToCharArray();
+                if (Array.Exists(tmp, element => element == '3' || element == '4' || element == '7'))
+                {
+                    continue;
+                }
+                if (!tmp.Distinct().ToHashSet().Except(INVALIDNUMBERS).Any())
+                {
+                    continue;
+                }
+                else count++;
             }
+            return count;
         }
     }
 }
---
+++ leetcodesln_test/RotatedDigitsUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RotatedDigitsUnitTest
+    {
+        [TestMethod]
+        public void RotatedDigitsTestMethod()
+        {
+            RotatedDigits rotatedDigits = new RotatedDigits();
+            int expected = 4;
+            int actual = rotatedDigits.RotatedDigitsSln(10);
+            Assert.AreEqual(expected, actual);
+
+            int expected2 = 0;
+            int actual2 = rotatedDigits.RotatedDigitsSln(1);
+            Assert.AreEqual(expected2, actual2);
+        }
+    }
+}
