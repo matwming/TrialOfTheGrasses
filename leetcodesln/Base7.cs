83e08fd5011fd92c3e194a47ed9051533730c7bb
CJ\zhelun <stan_gai@Hotmail.com>
2018-06-12 11:22:09 +1000

    base7

---
+++ leetcodesln/Base7.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class Base7
+    {
+        public static readonly int BASE = 7;
+
+        public string ConvertToBase7(int num)
+        {
+            //StringBuilder sb = new StringBuilder();
+            //int i;
+            //int remainder;
+            //if (num.ToString()[0]=='-')
+            //{
+            //    i = num.ToString().ToCharArray().Length -1;
+            //    remainder = num * -1;
+            //}
+            //else
+            //{
+            //    i = num.ToString().ToCharArray().Length;
+            //    remainder = num;
+            //}
+            //for (int power = i ; power >= 0; power--)
+            //{
+            //    var val = remainder / (int)Math.Pow(BASE, power) > 0 ? (remainder / (int)Math.Pow(BASE, power)).ToString() : "0";
+            //    remainder = remainder % (int)Math.Pow(BASE, power);
+            //    sb.Append(val);
+            //}
+            //if (sb.ToString()[0]=='0')
+            //{
+            //    sb.Remove(0, 1);
+            //}
+            //if (num< 0)
+            //{
+            //    sb.Insert(0, "-");
+            //}
+            //return sb.ToString();
+            if (num == 0) return "0";
+            string result = "";
+            bool positive = num > 0;
+            while (num!=0)
+            {
+                result = Math.Abs(num % BASE) + result;
+                num /= BASE;
+            }
+            return positive ? result : "-" + result;
+        }
+    }
+}
---
+++ leetcodesln_test/Base7UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class Base7UnitTest
+    {
+        [TestMethod]
+        public void Base7TestMethod()
+        {
+            Base7 base7 = new Base7();
+            string expected = "202";
+            string actual = base7.ConvertToBase7(100);
+            Assert.AreEqual(expected, actual);
+            string expected2 = "-10";
+            string actual2 = base7.ConvertToBase7(-7);
+            Assert.AreEqual(expected2, actual2);
+            string expected3 = "150666342";
+            string actual3 = base7.ConvertToBase7(9999999);
+            Assert.AreEqual(expected3, actual3);
+        }
+    }
+}
