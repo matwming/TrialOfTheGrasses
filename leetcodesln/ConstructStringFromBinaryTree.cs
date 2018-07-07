70d2ee1d362f9b1d43417d9d48e48b1b265a2b81
user3301 <stan_gai@Hotmail.com>
2018-07-07 20:12:35 +1000

    wip

---
+++ leetcodesln/ConstructStringFromBinaryTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ConstructStringFromBinaryTree
+    {
+        //public string Tree2Str(TreeNode t)
+        //{
+
+        //}
+    }
+}
