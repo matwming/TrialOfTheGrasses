58cdf0e42dc697590311ab56a0424cfb7e008a86
user3301 <stan_gai@Hotmail.com>
2018-07-29 21:28:02 +1000

    construct string from binary tree

--- leetcodesln/ConstructStringFromBinaryTree.cs
+++ leetcodesln/ConstructStringFromBinaryTree.cs
@@ -6,9 +6,30 @@ namespace leetcodesln
 {
     public class ConstructStringFromBinaryTree
     {
-        //public string Tree2Str(TreeNode t)
-        //{
+        public string Tree2Str(TreeNode t)
+        {
+            return PreOrder(t, new StringBuilder()).ToString();
+        }
 
-        //}
+        private StringBuilder PreOrder(TreeNode t, StringBuilder sb)
+        {
+            if (t!=null)
+            {
+                sb.Append(t.val);
+            }
+            if (t.left!=null || t.right!=null)
+            {
+                sb.Append("(");
+                PreOrder(t.left, sb);
+                sb.Append(")");
+            }
+            if (t.right!=null)
+            {
+                sb.Append("(");
+                PreOrder(t.right, sb);
+                sb.Append(")");
+            }
+            return sb;
+        }
     }
 }
---
+++ leetcodesln_test/ConstructStringFromBinaryTreeUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ConstructStringFromBinaryTreeUnitTest
+    {
+        [TestMethod]
+        public void ConstructStringFromBinaryTreeTestMethod()
+        {
+            ConstructStringFromBinaryTree csfbt = new ConstructStringFromBinaryTree();
+            TreeNode input = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4)
+                    {
+                        left = null,
+                        right = null
+                    },
+                    right = null
+                },
+                right = new TreeNode(3)
+                {
+                    left = null,
+                    right = null
+                }
+            };
+            var expected = "1(2(4))(3)";
+            Assert.AreEqual(expected, csfbt.Tree2Str(input));
+        }
+    }
+}
