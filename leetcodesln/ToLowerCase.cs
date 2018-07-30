8f2d8f349933ad1f63e59c346ef0939289377a47
CJ\zhelun <stan_gai@Hotmail.com>
2018-07-30 15:44:41 +1000

    to lower case

---
+++ leetcodesln/ToLowerCase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ToLowerCase
+    {
+        public string ToLower(string str)
+        {
+            var sb = new StringBuilder();
+            foreach (var chr in str)
+            {
+                if ('A'<=chr && chr <='Z')
+                {
+                    sb.Append((char)(chr + 32));
+                }
+                else
+                {
+                    sb.Append(chr);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ leetcodesln_test/ToLowerCaseUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ToLowerCaseUnitTest
+    {
+        [TestMethod]
+        public void ToLowerCaseTestMethod()
+        {
+            ToLowerCase toLowerCase = new ToLowerCase();
+            var expected1 = "hello";
+            var input1 = "Hello";
+            Assert.AreEqual(expected1, toLowerCase.ToLower(input1));
+
+            var expected2 = "here";
+            var input2 = "here";
+            Assert.AreEqual(expected2, toLowerCase.ToLower(input2));
+
+            var expected3 = "lovely";
+            var input3 = "LOVELY";
+            Assert.AreEqual(expected3, toLowerCase.ToLower(input3));
+        }
+    }
+}
