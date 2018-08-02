2bf441c72cbd725c25e5c8a775dc82b467b2614a
CJ\zhelun <stan_gai@Hotmail.com>
2018-08-02 21:19:27 +1000

    missing number

---
+++ leetcodesln/MissingNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class MissingNumber
+    {
+        public int FindMissingNumber(int[] nums)
+        {
+            int[] arr = new int[nums.Length + 1];
+            for (int i = 0; i <nums.Length; i++)
+            {
+                arr[nums[i]] -= nums[i];
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]==0 && i!=0)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ leetcodesln_test/MissingNumberUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MissingNumberUnitTest
+    {
+        [TestMethod]
+        public void MissingNumberTestMethod()
+        {
+            MissingNumber missingNumber = new MissingNumber();
+            var input1 = new int[] {3,0,1};
+            var expected1 = 2;
+            Assert.AreEqual(expected1, missingNumber.FindMissingNumber(input1));
+        }
+    }
+}
