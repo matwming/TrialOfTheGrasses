123cf0f94893d51a2fa314e27228126b8a49f8e0
user3301 <stan_gai@Hotmail.com>
2018-08-03 09:49:50 +1000

    n-ary tree postorder traversal

---
+++ leetcodesln/N_aryTreePostorderTraversal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class N_aryTreePostorderTraversal
+    {
+        public IList<int> Postorder(Node root)
+        {
+            List<int> res = new List<int>();
+            if (root == null)
+            {
+                return res;
+            }
+            AddChildrenToList(root, res);
+            return res;
+        }
+
+        private void AddChildrenToList(Node node, IList<int> list)
+        {
+            if (node.children != null)
+            {
+                foreach (var item in node.children)
+                {
+                    AddChildrenToList(item, list);
+                }
+            }
+            list.Add(node.val);
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
---
+++ leetcodesln_test/N_aryTreePostorderTraversalUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class N_aryTreePostorderTraversalUnitTest
+    {
+        [TestMethod]
+        public void N_aryTreePostorderTraversalTestMethod()
+        {
+            N_aryTreePostorderTraversal ntpt = new N_aryTreePostorderTraversal();
+            var node5 = new Node
+            {
+                val = 5,
+                children = null
+            };
+            var node6 = new Node
+            {
+                val = 6,
+                children = null
+            };
+
+            var node3 = new Node
+            {
+                val = 3,
+                children = new List<Node>
+                { node5, node6}
+            };
+            var node2 = new Node
+            {
+                val = 2,
+                children = null
+            };
+            var node4 = new Node
+            {
+                val = 4,
+                children = null
+            };
+            var root = new Node
+            {
+                val = 1,
+                children = new List<Node>
+                {
+                    node3, node2,node4
+                }
+            };
+            var expected = new List<int>{ 5,6,3,2,4,1};
+            var actual = ntpt.Postorder(root);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
