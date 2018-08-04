f94c07eee75c234584437e6b0b673df591ffe4f7
user3301 <stan_gai@Hotmail.com>
2018-08-04 21:16:52 +1000

    lemonade change test passed but submission pending on leetcode

---
+++ leetcodesln/LemonadeChange.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class LemonadeChange
+    {
+        public bool LemonadeChangeSln(int[] bills)
+        {
+            var notes = new Dictionary<string, int>();
+            notes.Add("5", 0);
+            notes.Add("10", 0);
+            notes.Add("20", 0);
+            foreach (var bill in bills)
+            {
+                switch(bill)
+                {
+                    case 5:
+                        notes["5"]++;
+                        break;
+                    case 10:
+                        if (notes["5"]<=0)
+                        {
+                            return false;
+                        }
+                        notes["5"]--;
+                        notes["10"]++;
+                        break;
+                    case 20:
+                        if (notes["10"]>0)
+                        {
+                            if (notes["5"]>0)
+                            {
+                                notes["5"]--;
+                                notes["10"]--;
+                                notes["20"]++;
+                                break;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            if (notes["5"]>=3)
+                            {
+                                notes["5"] -= 3;
+                                notes["20"]++;
+                                break;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/LemonadeChangeUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LemonadeChangeUnitTest
+    {
+        [TestMethod]
+        public void LemonadeChangeTestMethod()
+        {
+            LemonadeChange lemonadeChange = new LemonadeChange();
+            var input1 = new int[] { 5, 5, 10 };
+            Assert.AreEqual(true, lemonadeChange.LemonadeChangeSln(input1));
+            var input2 = new int[] { 10, 10 };
+            Assert.AreEqual(false, lemonadeChange.LemonadeChangeSln(input2));
+            var input3 = new int[] { 5, 5, 10, 10, 20 };
+            Assert.AreEqual(false, lemonadeChange.LemonadeChangeSln(input3));
+            var input4 = new int[] { 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20, 5, 10, 5, 20 };
+            Assert.AreEqual(true, lemonadeChange.LemonadeChangeSln(input4));
+            var input5 = new int[] { 5, 5, 10, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 5, 5, 20, 5, 20, 5 };
+            Assert.AreEqual(true, lemonadeChange.LemonadeChangeSln(input5));
+        }
+    }
+}
