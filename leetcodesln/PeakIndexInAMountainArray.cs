b63bff83cac76717320c0157690ae5c24ee9d55e
CJ\zhelun <stan_gai@Hotmail.com>
2018-08-06 14:29:13 +1000

    peak index in a mountain array (best solution to use binary search)

---
+++ leetcodesln/PeakIndexInAMountainArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class PeakIndexInAMountainArray
+    {
+        public int PeakIndexInMountainArray(int[] A)
+        {
+            if (A.Length ==1)
+            {
+                return 0;
+            }
+            for (int i = 0; i < A.Length -1; i++)
+            {
+                
+                if (A[i] - A[i+1]>0)
+                {
+                    return i;
+                }
+            }
+            throw new Exception("not a mountain array");
+        }
+    }
+}
---
+++ leetcodesln_test/PeakIndexInAMountainArrayUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PeakIndexInAMountainArrayUnitTest
+    {
+        [TestMethod]
+        public void PeakIndexInAMountainArrayTestMethod()
+        {
+            PeakIndexInAMountainArray peakIndexInAMountainArray = new PeakIndexInAMountainArray();
+            var input1 = new int[] { 0, 1, 0 };
+            var expected1 = 1;
+            Assert.AreEqual(expected1, peakIndexInAMountainArray.PeakIndexInMountainArray(input1));
+
+            var input2 = new int[] { 0, 2, 1, 0 };
+            var expected2 = 1;
+            Assert.AreEqual(expected2, peakIndexInAMountainArray.PeakIndexInMountainArray(input2));
+        }
+    }
+}
