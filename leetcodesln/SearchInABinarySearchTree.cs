6f87a0e32f6d9fc279416b93e9247af72a06bbe7
user3301 <stan_gai@Hotmail.com>
2018-08-07 14:41:36 +1000

    search in a binary search tree

---
+++ leetcodesln/SearchInABinarySearchTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class SearchInABinarySearchTree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root==null)
+            {
+                return null;
+            }
+            if (root.val==val)
+            {
+                return root;
+            }
+            if (root.val> val)
+            {
+                return SearchBST(root.left, val);
+            }
+            else
+            {
+                return SearchBST(root.right, val);
+            }
+            return null;
+        }
+    }
+}
---
+++ leetcodesln_test/SearchInABinarySearchTreeUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SearchInABinarySearchTreeUnitTest
+    {
+        [TestMethod]
+        public void SearchInABinarySearchTreeTestMethod()
+        {
+            SearchInABinarySearchTree searchInABinarySearchTree = new SearchInABinarySearchTree();
+            // need a helper class to compare trees
+        }
+    }
+}
