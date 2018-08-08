5da48ea4d8da0a256d0e577846c5a6f6f44a0b9b
user3301 <stan_gai@Hotmail.com>
2018-08-08 23:01:07 +1000

    convert bst to greater tree (timeout and no test case)

---
+++ leetcodesln/ConvertBSTToGreaterTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ConvertBSTToGreaterTree
+    {
+        public TreeNode ConvertBST(TreeNode root)
+        {
+            if (root==null)
+            {
+                return root;
+            }
+            root.right.val = SumRightNode(root.right);
+            root.left.val += root.val + SumRightNode(root.left.right);
+            return root;
+            
+        }
+
+        private int SumRightNode(TreeNode node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            if (node.right==null)
+            {
+                return node.val;
+            }
+            else
+            {
+                node.val += SumRightNode(node.right);
+                return node.val;
+            }
+        }
+       
+    }
+}
