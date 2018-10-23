db32faa574a45bfd6f8bd70d3f30f111839f5e62
zhelun <zhelun.gai@tmacomms.com>
2018-10-24 09:34:23 +1100

    sort array by parity 2

---
+++ leetcodesln/SortArrayByParityII.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class SortArrayByParityII
+    {
+        public int[] SortArrayByParityIISln(int[] A)
+        {
+            var result = new int[A.Length];
+            int even =0, odd = 1;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i]%2==0)
+                {
+                    result[even] = A[i];
+                    even += 2;
+                }
+                else
+                {
+                    result[odd] = A[i];
+                    odd += 2;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/SortArrayByParityIIUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SortArrayByParityIIUnitTest
+    {
+        [TestMethod]
+        public void SortArrayByParityIITestMethod()
+        {
+            SortArrayByParityII sortArrayByParityII = new SortArrayByParityII();
+            var input = new int[] { 4,2,5,7};
+            var expected = new int[] {4,5,2,7};
+            var actual = sortArrayByParityII.SortArrayByParityIISln(input);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
