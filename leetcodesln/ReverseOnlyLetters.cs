6f23cbc3ae02058e70231cde4c9faa2af7d46a66
zhelun <zhelun.gai@tmacomms.com>
2018-10-25 21:50:15 +1100

    wip

---
+++ leetcodesln/ReverseOnlyLetters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ReverseOnlyLetters
+    {
+        public string ReverseOnlyLettersSln(string S)
+        {
+            var res_arr = new char[S.Length];
+            int tail = S.Length-1;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (char.IsLetter(S[i]))
+                {
+                    tail = GetLetterPositionIndex(S, tail);
+                    res_arr[tail] = S[i];
+                }
+                else
+                {
+                    res_arr[i] = S[i];
+                }
+            }
+            return res_arr.ToString();
+        }
+
+        private int GetLetterPositionIndex(string S, int index)
+        {
+            if (char.IsLetter(S[index]))
+            {
+                return index;
+            }
+            return GetLetterPositionIndex(S, index--);
+        }
+    }
+}
---
+++ leetcodesln_test/ReverseOnlyLettersUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ReverseOnlyLettersUnitTest
+    {
+        [TestMethod]
+        public void ReverseOnlyLettersTestMethod()
+        {
+            var reverseOnlyLetters = new ReverseOnlyLetters();
+            var input1 = "ab-cd";
+            var expected1 = "dc-ba";
+            var actual1 = reverseOnlyLetters.ReverseOnlyLettersSln(input1);
+            Assert.AreEqual(expected1, actual1);
+        }
+    }
+}
