54307e4e80593aa587013620fb6a82fd7e421b26
zhelun <zhelun.gai@tmacomms.com>
2018-10-29 10:13:32 +1100

    monotonic array

---
+++ leetcodesln/MonotonicArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class MonotonicArray
+    {
+        public bool IsMonotonic(int[] A)
+        {
+            if (A.Length <= 1) return true;
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (A[i] - A[i]-1 ==0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (A[i] - A[i - 1] > 0)
+                    {
+                        for (int j = i + 1; j < A.Length; j++)
+                        {
+                            if (A[j] - A[j - 1] < 0)
+                            {
+                                return false;
+                            }
+                        }
+
+                    }
+                    else if (A[i] - A[i - 1] < 0)
+                    {
+                        for (int k = i + 1; k < A.Length; k++)
+                        {
+                            if (A[k] - A[k - 1] > 0)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/MonotonicArrayUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MonotonicArrayUnitTest
+    {
+        [TestMethod]
+        public void MonotonicArrayTestMethod()
+        {
+            var monotonicArray = new MonotonicArray();
+            var input1 = new int[] { 1, 2, 2, 3 };
+            var expected1 = true;
+            var actual1 = monotonicArray.IsMonotonic(input1);
+            Assert.AreEqual(expected1, actual1);
+
+            var input2 = new int[] { 6,5,4,4 };
+            var expected2 = true;
+            var actual2 = monotonicArray.IsMonotonic(input2);
+            Assert.AreEqual(expected2, actual2);
+
+            var input3 = new int[] { 1,3,2 };
+            var expected3 = false;
+            var actual3 = monotonicArray.IsMonotonic(input3);
+            Assert.AreEqual(expected3, actual3);
+
+            var input4 = new int[] { 1, 2,4,5 };
+            var expected4 = true;
+            var actual4 = monotonicArray.IsMonotonic(input4);
+            Assert.AreEqual(expected4, actual4);
+
+            var input5 = new int[] { 1, 1,1 };
+            var expected5 = true;
+            var actual5 = monotonicArray.IsMonotonic(input5);
+            Assert.AreEqual(expected5, actual5);
+
+            var input6 = new int[] { 1, 1, 0 };
+            var expected6 = true;
+            var actual6 = monotonicArray.IsMonotonic(input6);
+            Assert.AreEqual(expected6, actual6);
+
+        }
+    }
+}
