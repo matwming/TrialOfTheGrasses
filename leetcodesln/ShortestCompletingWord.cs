2ca1f3ff980f4e513782eff2684268c6804e2cec
zhelun <zhelun.gai@tmacomms.com>
2018-11-02 21:33:59 +1100

    shortest completing word

--- leetcodesln/ShortestCompletingWord.cs
+++ leetcodesln/ShortestCompletingWord.cs
@@ -1,10 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace leetcodesln
 {
     public class ShortestCompletingWord
     {
-        public string ShortestCompletingWord(string licensePlate, string[] words)
+        public string ShortestCompletingWordSln(string licensePlate, string[] words)
         {
-            
+            var lp = new List<char>();
+            foreach (var chr in licensePlate)
+            {
+                if (char.IsLetter(chr))
+                {
+                    lp.Add(char.ToLower(chr));
+                }
+            }
+            string result = string.Empty;
+            var shortest = int.MaxValue;
+
+            foreach (var word in words)
+            {
+                if (IsCompletingWord(lp, word.ToLower().ToList()))
+                {
+                    if (word.Length< shortest)
+                    {
+                        result = word;
+                        shortest = word.Length;
+                    }
+                }
+                
+
+            }
+            return result;
+        }
+
+        private bool IsCompletingWord(List<char> licenseplate, List<char> word)
+        {
+            foreach (var chr in licenseplate)
+            {
+                if (word.Contains(chr))
+                {
+                    word.Remove(chr);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
---
+++ leetcodesln_test/ShortestCompletingWordUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ShortestCompletingWordUnitTest
+    {
+        [TestMethod]
+        public void ShortestCompletingWordTestMethod()
+        {
+            var shortestCompletingWord = new ShortestCompletingWord();
+            var input1_licenseplate = "1s3 PSt";
+            var input1_words = new string[] { "step", "steps", "stripe", "stepple" };
+            var expected1 = "steps";
+            var actual1 = shortestCompletingWord.ShortestCompletingWordSln(input1_licenseplate, input1_words);
+            Assert.AreEqual(expected1, actual1);
+
+            var input2_licenseplate = "1s3 456";
+            var input2_words = new string[] { "looks", "pest", "stew", "show" };
+            var expected2 = "pest";
+            var actual2 = shortestCompletingWord.ShortestCompletingWordSln(input2_licenseplate, input2_words);
+            Assert.AreEqual(expected2, actual2);
+        }
+    }
+}
