d07fd4064100c2bd5334d332e1ea62e69fc1117c
user3301 <stan_gai@Hotmail.com>
2018-11-05 22:56:26 +1100

    design hash map (super slow AC solution)

--- leetcodesln/DesignHashMap.cs
+++ leetcodesln/DesignHashMap.cs
@@ -6,53 +6,51 @@ namespace leetcodesln
     {
         private List<int> storage { get; set; }
         /** Initialize your data structure here. */
-    public MyHashMap() {
-        storage = new List<int>(0);
-    }
-    
-    /** value will always be non-negative. */
-    public bool Put(int key, int value) {
-        for (int i = 0; i < storage.Count; i+=2)
+        public MyHashMap()
         {
-            if(storage[i]==key)
+            storage = new List<int>(0);
+        }
+
+        /** value will always be non-negative. */
+        public void Put(int key, int value)
+        {
+            for (int i = 0; i < storage.Count; i += 2)
             {
-                storage[i+1] = value;
-                return false;
+                if (storage[i] == key)
+                {
+                    storage[i + 1] = value;
+                    return;
+                }
+            }
+            storage.Add(key);
+            storage.Add(value);
+        }
+
+        /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+        public int Get(int key)
+        {
+            for (int i = 0; i < storage.Count; i += 2)
+            {
+                if (storage[i] == key)
+                {
+                    return storage[i + 1];
+                }
+            }
+            return -1;
+        }
+
+        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+        public void Remove(int key)
+        {
+            for (int i = 0; i < storage.Count; i += 2)
+            {
+                if (storage[i] == key)
+                {
+                    storage.RemoveAt(i);
+                    storage.RemoveAt(i);
+                }
             }
         }
-        storage.Add(key);
-        storage.Add(value);
-            return true;
-    }
-    
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    public int Get(int key) {
-        for (int i = 0; i < storage.Count; i+=2)
-        {
-            if(storage[i]==key)
-            {
-                return storage[i+1];
-            }
-        }
-        return -1;
-    }
-    
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    public bool Remove(int key) 
-    {
-        for (int i = 0; i < storage.Count; i+=2)
-        {
-            if(storage[i] == key)
-            {
-                var targetkey = storage[i];
-                var targetvalue = storage[i+1];
-                storage.Remove(targetkey);
-                storage.Remove(targetvalue);
-                    return true;
-            }
-        }
-            return false;
-    }
     }
 }
 
