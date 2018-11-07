6403572dc26b57e2909e93448211c53d2f9ecb85
zhelun <zhelun.gai@tmacomms.com>
2018-11-08 10:35:32 +1100

    fair candy swap

---
+++ leetcodesln/FairCandySwap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace leetcodesln
+{
+    public class FairCandySwap
+    {
+        public int[] FairSwap(int[] A, int[] B)
+        {
+            var totalcandy = A.Sum() + B.Sum();
+            var fairnumber = totalcandy / 2;
+            var suma = A.Sum();
+            var sumb = B.Sum();
+            foreach (var numa in A)
+            {
+                foreach (var numb in B)
+                {
+                    if (suma - numa + numb == sumb - numb + numa)
+                    {
+                        return new int[] { numa, numb };
+                    }
+                }
+            }
+            throw new Exception("cannot find a result");
+        }
+    }
+}
---
+++ leetcodesln_test/FairCandySwapUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FairCandySwapUnitTest
+    {
+        [TestMethod]
+        public void FairCandySwapTestMethod()
+        {
+            var fairCandySwap = new FairCandySwap();
+            var inputA1 = new int[] { 1, 1 };
+            var inputB1 = new int[] { 2, 2 };
+            var expected1 = new int[] { 1, 2 };
+            var actual1 = fairCandySwap.FairSwap(inputA1, inputB1);
+            foreach (var exp in expected1)
+            {
+                Assert.IsTrue(actual1.Contains(exp));
+            }
+
+            var inputA2 = new int[] { 1, 2 };
+            var inputB2 = new int[] { 2, 3 };
+            var expected2 = new int[] { 1, 2 };
+            var actual2 = fairCandySwap.FairSwap(inputA2, inputB2);
+            foreach (var exp in expected2)
+            {
+                Assert.IsTrue(actual2.Contains(exp));
+            }
+        }
+    }
+}
