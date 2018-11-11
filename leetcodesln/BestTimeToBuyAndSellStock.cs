8fb536d5474b5d9c7f5f238dc8f0505ffa8f3d15
zhelun <stan_gai@Hotmail.com>
2018-11-11 22:40:40 +1100

    time limit exceed answer

--- leetcodesln/BestTimeToBuyAndSellStock.cs
+++ leetcodesln/BestTimeToBuyAndSellStock.cs
@@ -7,20 +7,7 @@ namespace leetcodesln
     {
         public int MaxProfit(int[] prices)
         {
-            var max = 0;
-            for (int i = 0; i < prices.Length-1; i++)
-            {
-                var remains = new int[prices.Length -i-1];
-                Array.Copy(prices,i+1, remains, 0, prices.Length-i-1);
-                var temp = FindMax(remains)- prices[i];
-                if(temp>0 && temp > max) max = temp;
-            }
-            return max;
-        }
-
-        private int FindMax(int[] prices)
-        {
-            return prices.Max();
+           
         }
     }
 }
