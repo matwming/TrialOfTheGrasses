bfb7776aab2a9de612aa598ca51ed503b364e545
zhelun <stan_gai@Hotmail.com>
2018-11-14 13:15:51 +1100

    :sweat: long pressed name

--- leetcodesln/LongPressedName.cs
+++ leetcodesln/LongPressedName.cs
@@ -1,20 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace leetcodesln
 {
     public class LongPressedName
     {
         public bool IsLongPressedName(string name, string typed)
         {
-            var name_index_ptr = 0;
-            for (int i = 0; i < typed.Length; i++)
+            if (typed.Length < name.Length) return false;
+
+            var t_ptr = 0;
+            var previous = '1';
+
+            int i = 0;
+            while (i< name.Length && t_ptr< typed.Length)
             {
-                if(typed[i]== name[name_index_ptr]) continue;
-                else
+                if (typed[t_ptr] == name[i])
                 {
-                    if(typed[i] == name[++name_index_ptr]) continue;
+                    while (i < name.Length && t_ptr< typed.Length && typed[t_ptr] == name[i])
+                    {
+                        t_ptr++;
+                        previous = name[i];
+                        i++;
+                    }
+                }
+                else return false;
+
+                if(t_ptr< typed.Length)
+                {
+                    if (typed[t_ptr] == previous)
+                    {
+                        while (t_ptr< typed.Length && typed[t_ptr] == previous)
+                        {
+                            t_ptr++;
+                        }
+                    }
                     else return false;
                 }
+                
             }
-            return name_index_ptr == name.Length-1;
+            return i == name.Length;
         }
     }
 }
--- leetcodesln_test/LongPressedNameUnitTest.cs
+++ leetcodesln_test/LongPressedNameUnitTest.cs
@@ -33,6 +33,18 @@ namespace leetcodesln_test
             var expected4 = true;
             var actual4 = longPressedName.IsLongPressedName(input_name4, input_typed4);
             Assert.AreEqual(expected4, actual4);
+
+            var input_name5 = "pyplrz";
+            var input_typed5 = "ppyypllr";
+            var expected5 = false;
+            var actual5 = longPressedName.IsLongPressedName(input_name5, input_typed5);
+            Assert.AreEqual(expected5, actual5);
+
+            var input_name6 = "vtkgn";
+            var input_typed6 = "vttkgnn";
+            var expected6 = true;
+            var actual6 = longPressedName.IsLongPressedName(input_name6, input_typed6);
+            Assert.AreEqual(expected6, actual6);
         }
     }
 }
