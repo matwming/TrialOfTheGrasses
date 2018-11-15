ab674e5a728b4730fd7df169ef4b358f432e65b7
zhelun <stan_gai@Hotmail.com>
2018-11-15 15:48:04 +1100

    :sweat: reverse string 2

---
+++ leetcodesln/ReverseStringII.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ReverseStringII
+    {
+        public string ReverseStr(string s, int k)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length <= 1 || k <= 1) return s;
+            return Reverse(s, k).ToString();
+        } 
+
+        private StringBuilder Reverse(string substring, int k)
+        {
+            var sb = new StringBuilder();
+            if (substring.Length <= k)
+            {
+                for (int i = substring.Length - 1; i >= 0; i--)
+                {
+                    sb.Append(substring[i]);
+                }
+                return sb;
+            }
+            else if(substring.Length <= 2*k)
+            {
+                var pt1 = substring.Substring(0, k);
+                var pt2 = substring.Substring(k, substring.Length -k);
+                sb.Append(Reverse(pt1, k)).Append(pt2);
+                return sb;
+            }
+            else
+            {
+                var groups = substring.Length / (2 * k);
+                for (int i = 0; i <= groups; i++)
+                {
+                    if(2*k*(i+1)< substring.Length)
+                    {
+                        var pt = substring.Substring(2 * k * i, 2 * k);
+                        sb.Append(Reverse(pt, k));
+                    }
+                    else
+                    {
+                        var pt = substring.Substring(2 * k * i, substring.Length - 2*k*i);
+                        sb.Append(Reverse(pt,k));
+                    }
+                }
+                return sb;
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/ReverseStringIIUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ReverseStringIIUnitTest
+    {
+        [TestMethod]
+        public void ReverseStringIITestMethod()
+        {
+            var reverseStringII = new ReverseStringII();
+            var input1_s = "abcdefg";
+            var input1_k = 2;
+            var expected1 = "bacdfeg";
+            var actual1 = reverseStringII.ReverseStr(input1_s, input1_k);
+            Assert.AreEqual(expected1, actual1);
+
+            var input2_s = "abc";
+            var input2_k = 2;
+            var expected2 = "bac";
+            var actual2 = reverseStringII.ReverseStr(input2_s, input2_k);
+            Assert.AreEqual(expected2, actual2);
+
+            var input3_s = "abcd";
+            var input3_k = 2;
+            var expected3 = "bacd";
+            var actual3 = reverseStringII.ReverseStr(input3_s, input3_k);
+            Assert.AreEqual(expected3, actual3);
+
+            var input4_s = "abcdefg";
+            var input4_k = 1;
+            var expected4 = "abcdefg";
+            var actual4 = reverseStringII.ReverseStr(input4_s, input4_k);
+            Assert.AreEqual(expected4, actual4);
+
+            var input5_s = "abcdefg";
+            var input5_k = 4;
+            var expected5 = "dcbaefg";
+            var actual5 = reverseStringII.ReverseStr(input5_s, input5_k);
+            Assert.AreEqual(expected5, actual5);
+
+            var input6_s = "abcdefg";
+            var input6_k = 3;
+            var expected6 = "cbadefg";
+            var actual6 = reverseStringII.ReverseStr(input6_s, input6_k);
+            Assert.AreEqual(expected6, actual6);
+        }
+    }
+}
