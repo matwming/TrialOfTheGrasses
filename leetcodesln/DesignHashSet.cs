4ab33660696f712580171ec7f8bedbf2b46ce708
zhelun <stan_gai@Hotmail.com>
2018-11-19 16:47:17 +1100

    :sweat::question: design hashset

--- leetcodesln/DesignHashSet.cs
+++ leetcodesln/DesignHashSet.cs
@@ -4,51 +4,51 @@ using System.Text;
 
 namespace leetcodesln
 {
-    private class Node
-    {
-        public int Value { get; set; }
-        public Node Previous { get; set; }
-        public Node Next { get; set; }
-    }
 
     public class DesignHashSet
     {
-        public List<KeyValuePair<int, Node>> Bucket { get; set; }
+
+        public int Column { get; set; } = 1000;
+        public int Row { get; set; } = 1001;
+
+        public bool[][] Bucket { get; set; }
+
         /** Initialize your data structure here. */
         public DesignHashSet()
         {
-            Bucket = new List<KeyValuePair<int, Node>>();
+            Bucket = new bool[Column][];    
         }
 
         public void Add(int key)
         {
-            var hash = Hash(key);
-            foreach (var item in Bucket)
+            var hashvalue = Hash(key);
+           if(Bucket[Hash(key)] == null)
             {
-                if(item.Key == hash)
-                {
-                    while(item.Value.val != key)
-                    {
-                        //item.Value = item.Value.Next;
-                    }
-                }
+                Bucket[hashvalue] = new bool[Row];
             }
+            Bucket[hashvalue][Index(key)] = true;
         }
 
         public void Remove(int key)
         {
-
+            var hashvalue = Hash(key);
+            if (Bucket[hashvalue] != null)
+            Bucket[hashvalue][Index(key)] = false;
         }
 
         /** Returns true if this set contains the specified element */
         public bool Contains(int key)
         {
-            throw new NotImplementedException();
+            var hashvalue = Hash(key);
+            return Bucket[hashvalue] != null && Bucket[hashvalue][Index(key)];
         }
 
-        private int Hash(int value) => value / 10000;
+        private int Hash(int value) => value % Column;
+        private int Index(int value) => value / Column;
+
     }
 }
+
 
 /**
  * Your MyHashSet object will be instantiated and called as such:
---
+++ leetcodesln_test/DesignHashSetUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DesignHashSetUnitTest
+    {
+        [TestMethod]
+        public void DesignHashSetTestMethod()
+        {
+            var myHashSet = new DesignHashSet();
+            myHashSet.Add(1);
+            myHashSet.Add(2);
+            var expected1 = myHashSet.Contains(1);
+            Assert.AreEqual(true, expected1);
+            var expected2 = myHashSet.Contains(3);
+            Assert.AreEqual(false, expected2);
+            myHashSet.Add(2);
+            var expected3 = myHashSet.Contains(2);
+            Assert.AreEqual(true, expected3);
+            myHashSet.Remove(2);
+            var expected4 = myHashSet.Contains(2);
+            Assert.AreEqual(false, expected4);
+        }
+    }
+}
