9e1aa26c6fffaa244e338628a3b288f89df4b92d
zhelun <stan_gai@Hotmail.com>
2018-11-19 22:08:05 +1100

    merge two sorted list (recursive, beat 30.45%)

---
+++ leetcodesln_test/MergeTwoSortedListUnitTest.cs
@@ -0,0 +1,67 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MergeTwoSortedListUnitTest
+    {
+        [TestMethod]
+        public void MergeTwoSortedListTestMethod()
+        {
+            var mergeTwoSortedList = new MergeTwoSortedList();
+
+            var input1_l1 = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(4)
+                    {
+                        next = null
+                    }
+                }
+            };
+            var input1_l2 = new ListNode(1)
+            {
+                next = new ListNode(3)
+                {
+                    next = new ListNode(4)
+                    {
+                        next = null
+                    }
+                }
+            };
+            var expected1 = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(3)
+                        {
+                            next = new ListNode(4)
+                            {
+                                next = new ListNode(4)
+                                {
+                                    next = null
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            var actual1 = mergeTwoSortedList.MergeTwoLists(input1_l1, input1_l2);
+
+            while(expected1.next!=null && actual1.next!=null)
+            {
+                Assert.AreEqual(expected1.val, actual1.val);
+                expected1 = expected1.next;
+                actual1 = actual1.next;
+            }
+
+        }
+    }
+}
