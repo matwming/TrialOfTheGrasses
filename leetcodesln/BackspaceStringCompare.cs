058ad57f5b25061a27152bc5dbe26da1eea8da01
zhelun <stan_gai@Hotmail.com>
2018-11-21 10:16:23 +1100

    backspace string compare AC solution, beats 27.82%

---
+++ leetcodesln/BackspaceStringCompare.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class BackspaceStringCompare
+    {
+        public bool BackspaceCompare(string S, string T)
+        {
+            var stackS = new Stack<char>();
+            var stackT = new Stack<char>();
+
+            for (int s = 0, t = 0; s < S.Length || t< T.Length; )
+            {
+                if(s< S.Length)
+                {
+                    if (S[s] == '#')
+                    {
+                        if(stackS.Count!=0)
+                        {
+                            stackS.Pop();
+                        }
+                        s++;
+                    }
+                    else { stackS.Push(S[s]); s++; }
+                }
+                if (t < T.Length)
+                {
+                    if (T[t] == '#')
+                    {
+                        if (stackT.Count != 0)
+                        {
+                            stackT.Pop();
+                        }
+                        t++;
+                    }
+                    else { stackT.Push(T[t]); t++; }
+                }
+            }
+
+            return DeepCompare(stackS, stackT);
+        }
+
+        private bool DeepCompare(Stack<char> stackS, Stack<char> stackT)
+        {
+            if (stackS.Count != stackT.Count) return false;
+            else
+            {
+                for (int i = 0; i < stackT.Count; i++)
+                {
+                    if (stackS.Pop() != stackT.Pop()) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/BackspaceStringCompareUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class BackspaceStringCompareUnitTest
+    {
+        [TestMethod]
+        public void BackspaceStringCompareTestMethod()
+        {
+            var backspaceStringCompare = new BackspaceStringCompare();
+            var inputS1 = "ab#c";
+            var inputT1 = "ad#c";
+            var expected1 = true;
+            var actual1 = backspaceStringCompare.BackspaceCompare(inputS1, inputT1);
+            Assert.AreEqual(expected1, actual1);
+
+            var inputS2 = "a##c";
+            var inputT2 = "#a#c";
+            var expected2 = true;
+            var actual2 = backspaceStringCompare.BackspaceCompare(inputS2, inputT2);
+            Assert.AreEqual(expected2, actual2);
+
+            var inputS3 = "y#fo##f";
+            var inputT3 = "y#f#0##f";
+            var expected3 = true;
+            var actual3 = backspaceStringCompare.BackspaceCompare(inputS3, inputT3);
+            Assert.AreEqual(expected3, actual3);
+
+        }
+    }
+}
