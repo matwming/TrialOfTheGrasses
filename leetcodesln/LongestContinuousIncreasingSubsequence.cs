6cddb4624b3a8f9410b3bfd0e93d2865e5dc5f70
zhelun <stan_gai@Hotmail.com>
2018-11-22 09:51:31 +1100

    longest continuous increasing subsequence AC solution (beats 55.95%)

--- leetcodesln/LongestContinuousIncreasingSubsequence.cs
+++ leetcodesln/LongestContinuousIncreasingSubsequence.cs
@@ -12,28 +12,23 @@ namespace leetcodesln
             var max = int.MinValue;
             var current = 0;
             while(current < nums.Length)
-            {
-                var remain = new int[nums.Length - current];
-                for (int i = current, j =0; i < nums.Length; i++, j++)
-                {
-                    remain[j] = nums[i];
-                }
-
-                var partialLongest = FindPartialLongest(remain);
+            {              
+                var partialLongest = FindPartialLongest(nums, current);
                 current += partialLongest;
                 max = max < partialLongest? partialLongest : max;
             }
             return max;
         }
 
-        private int FindPartialLongest(int[] part)
+        private int FindPartialLongest(int[] part, int current)
         {
-            int index = 0;
-            while (index + 1 < part.Length && part[index] < part[index + 1])
+            var partialMax = 0;
+            while (current + 1 < part.Length && part[current] < part[current + 1])
             {
-                index++;
+                partialMax++;
+                current++;
             }
-            return index+1;
+            return partialMax+1;
         }
     }
 }
