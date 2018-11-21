d44181abfa0f2e9d8d7a32843875af6a1a8f4035
zhelun <stan_gai@Hotmail.com>
2018-11-21 11:33:13 +1100

    happy number, beats 1.12% only

---
+++ leetcodesln/HappyNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            var arr = new List<double>();
+            return IsHappyAux(arr, n);
+        }
+
+        private bool IsHappyAux(List<double> arr, double n)
+        {
+            if (n == 1) return true;
+
+            var strnum = n.ToString();
+
+            double sum = 0;
+            for (int i = 0; i < strnum.Length; i++)
+            {
+                double foo = strnum[i] - '0';
+                sum += Math.Pow(foo, 2);
+            }
+            if (arr.Contains(sum)) return false;
+            arr.Add(sum);
+
+            return IsHappyAux(arr, sum);
+        }
+    }
+}
---
+++ leetcodesln_test/HappyNumberUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class HappyNumberUnitTest
+    {
+        [TestMethod]
+        public void HappyNumberTestMethod()
+        {
+            var happyNumber = new HappyNumber();
+            var input1 = 19;
+            var expected1 = true;
+            var actual1 = happyNumber.IsHappy(input1);
+            Assert.AreEqual(expected1, actual1);
+        }
+    }
+}
