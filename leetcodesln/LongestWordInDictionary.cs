9465eb63796a7a8bf5d6598d2cfdb6c9bbb1ffcc
zhelun <stan_gai@Hotmail.com>
2018-11-26 11:11:32 +1100

    longest word in dictionary (very verbose beat 64.71% using dictionary<int, string>)

--- leetcodesln/LongestWordInDictionary.cs
+++ leetcodesln/LongestWordInDictionary.cs
@@ -1,70 +1,87 @@
-// using System.Collections.Generic;
+using System.Collections.Generic;
 
-// namespace leetcodesln
-// {
-//     public class LongestWordInDictionary
-//     {
-//         public string LongestWord(string[] words)
-//         {
-//             var dict = new Dictionary<int, List<string>>();
-//             foreach (var word in words)
-//             {
-//                 if(dict.ContainsKey(word.Length)) 
-//                 {
-//                     dict[word.Length].Add(word);
-//                 }
-//                 else
-//                 {
-//                     dict.Add(word.Length, new List<string>{word});
-//                 }
-//             }
-//             if(dict.TryGetValue(1, out var startchars))
-//             {
-//                 foreach (var chr in startchars)
-//                 {
-                    
-//                 }
-//             }
-//             else
-//             {
-//                 return string.Empty;
-//             }
-//         }
+namespace leetcodesln
+{
+    public class LongestWordInDictionary
+    {
+        public string LongestWord(string[] words)
+        {
+            if (words.Length == 0) return string.Empty;
+            var dict = new Dictionary<int, List<string>>();
+            foreach (var word in words)
+            {
+                if (dict.ContainsKey(word.Length))
+                {
+                    dict[word.Length].Add(word);
+                }
+                else
+                {
+                    dict.Add(word.Length, new List<string> { word });
+                }
+            }
+            var list = new List<string>();
+            FindWords( ref list, dict[1], 2, dict);
+            
+            if (list.Count == 0) return string.Empty;
+            var max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].Length > max.Length)
+                {
+                    max = list[i];
+                    continue;
+                }
+                if (list[i].Length == max.Length )
+                {
+                    for (int j = 0; j < max.Length; j++)
+                    {
+                        if(list[i][j] > max[j])
+                        {
+                            break;
+                        }
+                        else if(list[i][j]< max[j])
+                        {
+                            max = list[i];
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+            return max;
+        }
 
-//         private List<string> FindWords(string currentWord, int layer, Dictionary<int, List<string>> dict, List<string> list)
-//         {
-//             if(!dict.ContainsKey(layer))
-//             {
-//                 return list;
-//             }
-//             else
-//             {
-//                 foreach (var chr in dict[layer])
-//                 {
-//                     if(IsPrefix(currentWord, chr))
-//                     {
-//                         list.Clear();
-//                         list.Add(chr);
-//                         if(dict.ContainsKey(layer+1))
-//                         {
-//                             list = FindWords(chr, layer+1, dict, list);
-//                         }
-//                         else
-//                         {
-//                             return list;
-//                         }
-//                     }
-//                 }
-//             }
-//         }
+        private void FindWords(ref List<string> finalList, List<string> list, int layer, Dictionary<int, List<string>> dict)
+        {
+           if(!dict.ContainsKey(layer))
+            {
+                finalList.AddRange(list);
+                return;
+            }
+            
+            foreach (var current in list)
+            {
+                var templist = new List<string>();
+                foreach (var word in dict[layer])
+                {
+                    if (IsPrefix(current, word)) templist.Add(word);
+                }
+                if (templist.Count == 0) FindWords(ref finalList, list, int.MaxValue, dict);
 
-//         private bool IsPrefix(string current, string wordFromList)
-//         {
-//             for (int i = 0; i < current.Length; i++)
-//             {
-//                 if(current[i]!=wordFromList[i]) return false;
-//             }
-//             return true;
-//         }
-//     }
-// }
+                else FindWords(ref finalList, templist, layer + 1, dict);
+            }
+        }
+
+        private bool IsPrefix(string current, string wordFromList)
+        {
+            for (int i = 0; i < current.Length; i++)
+            {
+                if (current[i] != wordFromList[i]) return false;
+            }
+            return true;
+        }
+    }
+}
--- leetcodesln_test/LongestWordInDictionaryUnitTest.cs
+++ leetcodesln_test/LongestWordInDictionaryUnitTest.cs
@@ -11,7 +11,29 @@ namespace leetcodesln_test
         {
             var longestWordInDictionary = new LongestWordInDictionary();
             var input1 = new string[] {"w", "b","wo", "wor", "worl", "world"};
+            var expected1 = "world";
             var actual1 = longestWordInDictionary.LongestWord(input1);
+            Assert.AreEqual(expected1, actual1);
+
+            var input2 = new string[] {  };
+            var expected2 = "";
+            var actual2 = longestWordInDictionary.LongestWord(input2);
+            Assert.AreEqual(expected2, actual2);
+
+            var input3 = new string[] {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+            var expected3 = "apple";
+            var actual3 = longestWordInDictionary.LongestWord(input3);
+            Assert.AreEqual(expected3, actual3);
+
+            var input4 = new string[] { "m", "mo", "moc", "moch", "mocha", "l", "la", "lat", "latt", "latte", "c", "ca", "cat"};
+            var expected4 = "latte";
+            var actual4 = longestWordInDictionary.LongestWord(input4);
+            Assert.AreEqual(expected4, actual4);
+
+            var input5 = new string[] {"t", "ti", "tig", "tige", "tiger", "e", "en", "eng", "engl", "engli", "englis", "english", "h", "hi", "his", "hist", "histo", "histor", "history"};
+            var expected5 = "english";
+            var actual5 = longestWordInDictionary.LongestWord(input5);
+            Assert.AreEqual(expected5, actual5);
         }
     }
 }
