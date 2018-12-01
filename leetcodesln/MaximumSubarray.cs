21d70cf321808be4e485bd5ecbc30012540dc764
zhelun <stan_gai@Hotmail.com>
2018-12-01 23:45:56 +1100

    wip

---
+++ leetcodesln/MaximumSubarray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 1) return nums[0];
+            var max = int.MinValue;
+            var tempsum = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                tempsum += nums[i];
+                if(tempsum >= max)
+                {
+                    max = tempsum > nums[i]? tempsum : nums[i];
+                    tempsum = max;
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ leetcodesln_test/MaximumSubarrayUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MaximumSubarrayUnitTest
+    {
+        public MaximumSubarray maximumSubarray { get; set; }
+        [TestInitialize]
+        public void BeforeEach()
+        {
+           maximumSubarray = new MaximumSubarray();
+        }
+
+
+
+        [TestMethod, Timeout(10_000)]
+        public void MaximumSubarrayTestMethod1()
+        {
+            var input = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            var expected = 6;
+            var actual = maximumSubarray.MaxSubArray(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(10_000)]
+        public void MaximumSubarrayTestMethod2()
+        {
+            var input = new int[] { -2, -1 };
+            var expected = -1;
+            var actual = maximumSubarray.MaxSubArray(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(10_000)]
+        public void MaximumSubarrayTestMethod3()
+        {
+            var input = new int[] { -1, -2 };
+            var expected = -1;
+            var actual = maximumSubarray.MaxSubArray(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
