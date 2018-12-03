64b204e9f667f6934a951afca06da79ea90a6ba4
zhelun <stan_gai@Hotmail.com>
2018-12-03 22:34:44 +1100

    wip

---
+++ leetcodesln/ConvertANumberToHexdecimal.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ConvertANumberToHexdecimal
+    {
+        public string ToHex(int num)
+        {
+            var sb = new StringBuilder();
+            if(num >= 0)
+            {
+                while(num % 16 > 0)
+                {
+                    var remain = num % 16;
+                    num = num / 16;
+                    switch (remain)
+                    {
+                        case 10:
+                            sb.Append("a");
+                            break;
+                        case 11:
+                            sb.Append("b");
+                            break;
+                        case 12:
+                            sb.Append("c");
+                            break;
+                        case 13:
+                            sb.Append("d");
+                            break;
+                        case 14:
+                            sb.Append("e");
+                            break;
+                        case 15:
+                            sb.Append("f");
+                            break;
+                        default:
+                            sb.Append(remain);
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                num = -num;
+                while (num % 16 > 0)
+                {
+                    var remain = num % 16;
+                    num = num / 16;
+                    switch (remain)
+                    {
+                        case 10:
+                            sb.Append("a");
+                            break;
+                        case 11:
+                            sb.Append("b");
+                            break;
+                        case 12:
+                            sb.Append("c");
+                            break;
+                        case 13:
+                            sb.Append("d");
+                            break;
+                        case 14:
+                            sb.Append("e");
+                            break;
+                        case 15:
+                            sb.Append("f");
+                            break;
+                        default:
+                            sb.Append(remain);
+                            break;
+                    }
+                }
+                var chrarr = sb.ToString().ToCharArray();
+                Array.Reverse(chrarr);
+
+                chrarr.Select(c => 15 - c).ToArray();
+
+            }
+            var charArr = sb.ToString().ToCharArray();
+            Array.Reverse(charArr);
+            return new string(charArr);
+        }
+    }
+}
---
+++ leetcodesln_test/ConvertANumberToHexdecimalUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ConvertANumberToHexdecimalUnitTest
+    {
+        public ConvertANumberToHexdecimal convertANumberToHexdecimal { get; set; }
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            convertANumberToHexdecimal = new ConvertANumberToHexdecimal();
+        }
+
+
+        [TestMethod, Timeout(10_000)]
+        public void ConvertANumberToHexdecimalTestMethod1()
+        {
+            var input = 26;
+            var expected = "1a";
+            var actual = convertANumberToHexdecimal.ToHex(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
