649b9a5839541af7886721b97cab00e08539fce5
zhelun <stan_gai@Hotmail.com>
2018-12-05 23:05:13 +1100

    remove duplicates from sorted list (recursive ps)

---
+++ leetcodesln/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class RemoveDuplicatesFromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            head.next = DeleteDuplicates(head.next);
+            return head = head.val == head.next.val ? head.next : head;
+        }
+    }
+}
---
+++ leetcodesln_test/RemoveDuplicatesFromSortedListUnitTest.cs
@@ -0,0 +1,112 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RemoveDuplicatesFromSortedListUnitTest
+    {
+        public RemoveDuplicatesFromSortedList removeDuplicatesFromSortedList { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            removeDuplicatesFromSortedList = new RemoveDuplicatesFromSortedList();
+        }
+
+        [TestMethod, Timeout(10_000)]
+        public void RemoveDuplicatesFromSortedListTestMethod1()
+        {
+            ListNode input = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(3)
+                        {
+                            next = new ListNode(3)
+                            {
+                                next = null
+                            }
+                        }
+                    }
+                }
+            };
+
+            var expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = null
+                    }
+                }
+            };
+
+            var actual = removeDuplicatesFromSortedList.DeleteDuplicates(input);
+            try
+            {
+                while (actual.next != null || expected.next != null)
+                {
+                    Assert.AreEqual(expected.val, actual.val);
+                    expected = expected.next;
+                    actual = actual.next;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Assert.Fail(e.ToString());
+            }
+            
+        }
+
+        [TestMethod, Timeout(10_000)]
+        public void RemoveDuplicatesFromSortedListTestMethod2()
+        {
+            ListNode input = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(1)
+                    {
+                        next = new ListNode(2)
+                        {
+                            next = null
+                        }
+                    }
+                }
+            };
+
+            var expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = null
+                }
+            };
+
+            var actual = removeDuplicatesFromSortedList.DeleteDuplicates(input);
+            try
+            {
+                while (actual.next != null || expected.next != null)
+                {
+                    Assert.AreEqual(expected.val, actual.val);
+                    expected = expected.next;
+                    actual = actual.next;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Assert.Fail(e.ToString());
+            }
+
+        }
+    }
+}
