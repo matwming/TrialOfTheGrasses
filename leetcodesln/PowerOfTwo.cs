7e4cdf36a61227a0cc3e59359cc4b374574ef8fc
zhelun <stan_gai@Hotmail.com>
2018-12-09 20:48:02 +1100

    power of two (beats 10% bitwise solution)

---
+++ leetcodesln/PowerOfTwo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class PowerOfTwo
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n <= 0) return false;
+            var poss = new int[] { 1, 2, 4, 8 };
+            var list = new List<int>();
+            uint un = (uint)n;
+
+            while(un !=0)
+            {
+                if (poss.Contains((int)(un & 15))) list.Add(1);
+                else if((un & 15) == 0)
+                {
+
+                }
+                else
+                {
+                    return false;
+                }
+                un = un >> 4;
+            }
+            return list.Count == 1;
+        }
+    }
+}
---
+++ leetcodesln_test/PowerOfTwoUnitTest.cs
@@ -0,0 +1,83 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PowerOfTwoUnitTest
+    {
+        public PowerOfTwo powerOfTwo { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            powerOfTwo = new PowerOfTwo();
+        }
+
+        [TestMethod]
+        public void PowerOfTwoTestMethod1()
+        {
+            var input = 1;
+            var expected = true;
+            var actual = powerOfTwo.IsPowerOfTwo(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfTwoTestMethod2()
+        {
+            var input = 16;
+            var expected = true;
+            var actual = powerOfTwo.IsPowerOfTwo(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfTwoTestMethod3()
+        {
+            var input = 218;
+            var expected = false;
+            var actual = powerOfTwo.IsPowerOfTwo(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfTwoTestMethod4()
+        {
+            var input = 20;
+            var expected = false;
+            var actual = powerOfTwo.IsPowerOfTwo(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfTwoTestMethod5()
+        {
+            var input = 3;
+            var expected = false;
+            var actual = powerOfTwo.IsPowerOfTwo(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfTwoTestMethod6()
+        {
+            var input = 19;
+            var expected = false;
+            var actual = powerOfTwo.IsPowerOfTwo(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfTwoTestMethod7()
+        {
+            var input = -1;
+            var expected = false;
+            var actual = powerOfTwo.IsPowerOfTwo(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
