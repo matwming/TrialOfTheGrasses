755abcb31f8ba29e26815f576edc7ba354929ef4
zhelun <stan_gai@Hotmail.com>
2018-12-12 10:10:50 +1100

    most common word (not AC, not allowed to use Regex?)

---
+++ leetcodesln/MostCommonWord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace leetcodesln
+{
+    public class MostCommonWord
+    {
+        public string MostCommonWordSln(string paragraph, string[] banned)
+        {
+            var fixinput = Regex.Replace(paragraph, "[^a-zA-Z0-9% ._]", " ");
+            var tempo = new Regex("[ ]{2,}", RegexOptions.None).Replace(fixinput, " ");
+            var wordDict = new Dictionary<string, int>();
+            string[] words = tempo.Split(" ");
+            string mostFrequent = string.Empty;
+            var maxOccur = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToLower();
+                if (wordDict.ContainsKey(words[i])) wordDict[words[i]]++;
+                else
+                {
+                    wordDict.Add(words[i], 1);
+                }
+                if (wordDict[words[i]] > maxOccur && !banned.Contains(words[i]))
+                {
+                    mostFrequent = words[i];
+                    maxOccur = wordDict[words[i]];
+                }
+            }
+            return mostFrequent;
+        }
+    }
+}
---
+++ leetcodesln_test/MostCommonWordUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MostCommonWordUnitTest
+    {
+        public MostCommonWord mostCommonWord { get; set; }
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            mostCommonWord = new MostCommonWord();
+        }
+
+        [TestMethod]
+        public void MostCommonWordTestMethod1()
+        {
+            var paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            string[] banned = { "hit" };
+            var expected = "ball";
+            var actual = mostCommonWord.MostCommonWordSln(paragraph, banned);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MostCommonWordTestMethod2()
+        {
+            var paragraph = "a, a, a, a, b,b,b,c, c";
+            string[] banned = { "a" };
+            var expected = "b";
+            var actual = mostCommonWord.MostCommonWordSln(paragraph, banned);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
