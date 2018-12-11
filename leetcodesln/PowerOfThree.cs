66789962dae794c31ee9bc216636c2784ec9fb20
zhelun <stan_gai@Hotmail.com>
2018-12-11 11:04:33 +1100

    power of three (recursive beats 14.38%)

---
+++ leetcodesln/PowerOfThree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class PowerOfThree
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n == 1) return true;
+            double dn = (double)n;
+            while(dn >= 3)
+            {
+                if (dn / 3 == 1) return true;
+                dn = dn / 3;
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/PowerOfThreeUnitTest.cs
@@ -0,0 +1,65 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PowerOfThreeUnitTest
+    {
+        public PowerOfThree powerOfThree { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            powerOfThree = new PowerOfThree();
+        }
+
+        [TestMethod]
+        public void PowerOfThreeTestMethod1()
+        {
+            var input = 27;
+            var expected = true;
+            var actual = powerOfThree.IsPowerOfThree(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfThreeTestMethod2()
+        {
+            var input = 0;
+            var expected = false;
+            var actual = powerOfThree.IsPowerOfThree(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfThreeTestMethod3()
+        {
+            var input = 9;
+            var expected = true;
+            var actual = powerOfThree.IsPowerOfThree(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfThreeTestMethod4()
+        {
+            var input = 45;
+            var expected = false;
+            var actual = powerOfThree.IsPowerOfThree(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerOfThreeTestMethod5()
+        {
+            var input = 1;
+            var expected = true;
+            var actual = powerOfThree.IsPowerOfThree(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
