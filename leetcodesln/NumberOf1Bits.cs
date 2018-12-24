287891951702ce0783f38dc5db4d95eb9202e7b0
zhelun <stan_gai@Hotmail.com>
2018-12-24 15:36:01 +1100

    clean code

--- leetcodesln/NumberOf1Bits.cs
+++ leetcodesln/NumberOf1Bits.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace leetcodesln
 {
     public class NumberOf1Bits
     {
-        Dictionary<uint, int> map = new Dictionary<uint, int>
+        private readonly Dictionary<uint, int> map = new Dictionary<uint, int>
         {
             [0] = 0,
             [1] = 1,
@@ -23,17 +21,17 @@ namespace leetcodesln
             [12] = 2,
             [13] = 3,
             [14] = 3,
-            [15] =4
+            [15] = 4
         };
         public int HammingWeight(uint n)
         {
             var countOf1s = 0;
 
-            while(n !=0)
+            while (n != 0)
             {
-                var x = (n & 15);
+                var x = n & 15;
                 countOf1s += map[x];
-                n = (n >> 4);
+                n = n >> 4;
             }
             return countOf1s;
         }
