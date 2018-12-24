270703d1743c3acddefe38ad26c8f4af69884b32
zhelun <stan_gai@Hotmail.com>
2018-12-24 11:11:41 +1100

    number of 1 bits (bit shifting, Runtime: 44 ms, faster than 94.29%)

---
+++ leetcodesln/NumberOf1Bits.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class NumberOf1Bits
+    {
+        public int HammingWeight(uint n)
+        {
+            var countOf1s = 0;
+
+            while(n !=0)
+            {
+                uint x = (n & 15);
+                switch (x)
+                {
+                    case 1:
+                    case 2:
+                    case 4:
+                    case 8:
+                        countOf1s++;
+                        n = (n >> 4);
+                        break;
+                    case 3:
+                    case 5:
+                    case 6:
+                    case 9:
+                    case 10:
+                    case 12:
+                        countOf1s += 2;
+                        n = (n >> 4);
+                        break;
+                    case 7:
+                    case 11:
+                    case 13:
+                    case 14:
+                        countOf1s += 3;
+                        n = (n >> 4);
+                        break;
+                    case 15:
+                        countOf1s += 4;
+                        n = (n >> 4);
+                        break;
+                    default:
+                        n = (n >> 4);
+                        break;
+                }
+            }
+            return countOf1s;
+        }
+    }
+}
---
+++ leetcodesln_test/NumberOf1BitsUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class NumberOf1BitsUnitTest
+    {
+        public NumberOf1Bits numberOf1Bits { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            numberOf1Bits = new NumberOf1Bits();
+        }
+
+        [TestMethod]
+        public void NumberOf1BitsTestMethod1()
+        {
+            var input = "00000000000000000000000000001011";
+            uint input_uint = Convert.ToUInt32(input,2);
+            var expected = 3;
+            var actual = numberOf1Bits.HammingWeight(input_uint);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NumberOf1BitsTestMethod2()
+        {
+            var input = "00000000000000000000000010000000";
+            uint input_uint = Convert.ToUInt32(input, 2);
+            var expected = 1;
+            var actual = numberOf1Bits.HammingWeight(input_uint);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NumberOf1BitsTestMethod3()
+        {
+            var input = "00000000000000000000000000000111";
+            uint input_uint = Convert.ToUInt32(input, 2);
+            var expected = 3;
+            var actual = numberOf1Bits.HammingWeight(input_uint);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
