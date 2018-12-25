c4b98abd04cfa8c08a7f7e3a5ee7d142e8a87b01
zhelun <stan_gai@Hotmail.com>
2018-12-25 22:06:30 +1100

    implement queue using stacks (Runtime: 128 ms, faster than 43.01% )

---
+++ leetcodesln/ImplementQueueUsingStacks.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class ImplementQueueUsingStacks
+    {
+        private Stack<int> StackIn { get; set; }
+
+        private int Count { get; set; } = 0;
+
+        public ImplementQueueUsingStacks()
+        {
+            StackIn = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            StackIn.Push(x);
+            Count++;
+        }
+
+        public int Pop()
+        {
+            var stack = new Stack<int>();
+            int res = default(int);
+            while (StackIn.Count != 0 && StackIn.TryPop(out res))
+            {
+                stack.Push(res);
+            }
+            stack.Pop();
+            while (stack.TryPop(out int temp))
+            {
+                StackIn.Push(temp);
+            }
+            Count--;
+            return res;
+
+        }
+
+        public int Peek()
+        {
+            var stack = new Stack<int>();
+            int res = default(int);
+            while (StackIn.Count!=0 && StackIn.TryPop(out res))
+            {
+                stack.Push(res);
+            }
+            while (stack.TryPop(out int temp))
+            {
+                StackIn.Push(temp);
+            }
+            return res;
+        }
+
+        public bool Empty()
+        {
+            return Count == 0;
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Peek();
+ * bool param_4 = obj.Empty();
+ */
---
+++ leetcodesln_test/ImplementQueueUsingStacksUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ImplementQueueUsingStacksUnitTest
+    {
+        public ImplementQueueUsingStacks myQueue { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            myQueue = new ImplementQueueUsingStacks();
+        }
+
+        [TestCleanup]
+        public void AfterEach()
+        {
+            myQueue = new ImplementQueueUsingStacks();
+        }
+
+        [TestMethod, Timeout(10_000)]
+        public void ImplementQueueUsingStacksTestMethod1()
+        {
+            myQueue.Push(1);
+            myQueue.Push(2);
+            Assert.AreEqual(1, myQueue.Peek());
+            Assert.AreEqual(1, myQueue.Pop());
+            Assert.IsFalse(myQueue.Empty());
+        }
+    }
+}
