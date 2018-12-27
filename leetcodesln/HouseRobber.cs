281e03ff3799cddc9fdba9ae43695103658e2acc
zhelun <stan_gai@Hotmail.com>
2018-12-27 20:20:08 +1100

    house robber ( dp recusive Runtime: 104 ms, faster than 92.74% )

---
+++ leetcodesln/HouseRobber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class HouseRobber
+    {
+        private int[] dp { get; set; }
+
+        public int Rob(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+
+            int prepre = nums[0];
+            int pre = Math.Max(nums[0], nums[1]);
+
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                var curMax = Math.Max(prepre + nums[i], pre);
+                prepre = pre;
+                pre = curMax;
+            }
+            return pre;
+        }
+
+
+    }
+}
---
+++ leetcodesln_test/HouseRobberUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class HouseRobberUnitTest
+    {
+        public HouseRobber houseRobber { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            houseRobber = new HouseRobber();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void HouseRobberTestMethod1()
+        {
+            var input = new int[] { 1, 2, 3, 1 };
+            var expected = 4;
+            var actual = houseRobber.Rob(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void HouseRobberTestMethod2()
+        {
+            var input = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            var expected = 0;
+            var actual = houseRobber.Rob(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
