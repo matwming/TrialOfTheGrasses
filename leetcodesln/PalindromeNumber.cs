42b63f92d1561a5f4ccd0a07ac2e08e334307f6c
zhelun <stan_gai@Hotmail.com>
2018-12-28 23:54:44 +1100

    wip

---
+++ leetcodesln/PalindromeNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || x % 10 == 0) return false;
+
+            var queue = new Queue<uint>();
+            var original = x;
+            var reversed = 0;
+            uint ux = (uint)x;
+            while (ux != 0)
+            {
+                var cur = (ux & 15);
+                queue.Enqueue(cur);
+                ux = ux >> 4;
+            }
+
+            while (queue.Count != 0)
+            {
+                reversed += (int)Math.Pow(queue.Count - 1, 10) * (int)queue.Dequeue();
+            }
+            return reversed == original;
+        }
+    }
+}
---
+++ leetcodesln_test/PalindromeNumberUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PalindromeNumberUnitTest
+    {
+        public PalindromeNumber palindromeNumber { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            palindromeNumber = new PalindromeNumber();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void PalindromeNumberTestMethod1()
+        {
+            var input = 121;
+            var expected = true;
+            var actual = palindromeNumber.IsPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
