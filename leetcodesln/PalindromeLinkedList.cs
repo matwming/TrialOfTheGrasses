171da11a831ed02787cf0546a871dc683cc88996
zhelun <stan_gai@Hotmail.com>
2018-12-31 22:07:15 +1100

    2018-wip

---
+++ leetcodesln/PalindromeLinkedList.cs
@@ -0,0 +1,50 @@
+﻿namespace leetcodesln
+{
+    public class PalindromeLinkedList
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null || head.next == null) return true;
+
+            var explorer = head.next.next;
+
+            if (explorer == null) return head.val == head.next.val;
+
+            var left = head;
+            ListNode right = null;
+
+
+
+            while (explorer.next != null)
+            {
+                if (explorer.next.next != null)
+                {
+                    left = left.next;
+                    right = left.next.next;
+                    explorer = explorer.next.next;
+                }
+                else
+                {
+                    left = left.next;
+                    right = left.next;
+                    explorer = explorer.next;
+                }
+
+
+            }
+
+
+
+            while (right != null)
+            {
+                if (right.val == left.val)
+                {
+                    right = right.next;
+                    left = left.next;
+                }
+                else return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/PalindromeLinkedListUnitTest.cs
@@ -0,0 +1,108 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PalindromeLinkedListUnitTest
+    {
+        public PalindromeLinkedList palindromeLinkedList { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            palindromeLinkedList = new PalindromeLinkedList();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void PalindromeLinkedListTestMethod1()
+        {
+            var head = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = null
+                }
+            };
+
+            var expected = false;
+            var actual = palindromeLinkedList.IsPalindrome(head);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void PalindromeLinkedListTestMethod2()
+        {
+            var head = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(1)
+                        {
+                            next = null
+                        }
+                    }
+                }
+            };
+
+            var expected = true;
+            var actual = palindromeLinkedList.IsPalindrome(head);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void PalindromeLinkedListTestMethod3()
+        {
+            var head = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(2)
+                        {
+                            next = new ListNode(1)
+                            {
+                                next = null
+                            }
+                        }
+                    }
+                }
+            };
+
+            var expected = true;
+            var actual = palindromeLinkedList.IsPalindrome(head);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void PalindromeLinkedListTestMethod4()
+        {
+            var head = new ListNode(1)
+            {
+                next = new ListNode(4)
+                {
+                    next = new ListNode(-1)
+                    {
+                        next = new ListNode(-1)
+                        {
+                            next = new ListNode(4)
+                            {
+                                next = new ListNode(1)
+                                {
+                                    next = null
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var expected = true;
+            var actual = palindromeLinkedList.IsPalindrome(head);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
