2d4e85c08724bb95259640c7d613cb55f01f008f
zhelun <stan_gai@Hotmail.com>
2019-01-02 10:20:51 +1100

    valid palindrome (Runtime: 96 ms, faster than 59.25%, two pointer AC)

---
+++ leetcodesln/ValidPalindrome.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcodesln
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            for (int front = 0, end = s.Length - 1; front < end; front++, end--)
+            {
+                while (!char.IsLetterOrDigit(s[front]) && front < s.Length - 1)
+                {
+                    front++;
+                }
+                while (!char.IsLetterOrDigit(s[end]) && end > 0)
+                {
+                    end--;
+                }
+
+                if (front <= end)
+                {
+                    if (char.ToLower(s[front]) == char.ToLower(s[end]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/ValidPalindromeUnitTest.cs
@@ -0,0 +1,71 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ValidPalindromeUnitTest
+    {
+        public ValidPalindrome validPalindrome { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            validPalindrome = new ValidPalindrome();
+        }
+
+        [TestMethod]
+        public void ValidPalindromeTestMethod1()
+        {
+            var input = "A man, a plan, a canal: Panama";
+            var expected = true;
+            var actual = validPalindrome.IsPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeTestMethod2()
+        {
+            var input = "race a car";
+            var expected = false;
+            var actual = validPalindrome.IsPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeTestMethod3()
+        {
+            var input = string.Empty;
+            var expected = true;
+            var actual = validPalindrome.IsPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeTestMethod4()
+        {
+            var input = "A!!!!!";
+            var expected = true;
+            var actual = validPalindrome.IsPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeTestMethod5()
+        {
+            var input = ".,";
+            var expected = true;
+            var actual = validPalindrome.IsPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeTestMethod6()
+        {
+            var input = "0P";
+            var expected = false;
+            var actual = validPalindrome.IsPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
