455ecfd879c069fdbd5ac2222a11d0275eabec0c
zhelun <stan_gai@Hotmail.com>
2019-01-03 14:48:42 +1100

    valid palindrom 2 (Runtime: 140 ms, faster than 56.55% )

---
+++ leetcodesln/ValidPalindromeII.cs
@@ -0,0 +1,62 @@
+﻿namespace leetcodesln
+{
+    public class ValidPalindromeII
+    {
+        public bool ValidPalindrome(string s)
+        {
+            if (s.Length <= 1) return true;
+
+            return IsPalindromeLeft(s, 0, s.Length-1) || IsPalindromeRight(s, 0, s.Length-1);
+        }
+
+        private bool IsPalindromeLeft(string s, int left, int right)
+        {
+            if (s.Length <= 1) return true;
+
+            var chance = 1;
+
+            while (left <= right)
+            {
+                if (IsSameChar(s[left], s[right]) && chance >= 0)
+                {
+                    left++;
+                    right--;
+                }
+                else if (IsSameChar(s[left + 1], s[right]) && chance > 0)
+                {
+                    left++;
+                    chance--;
+                    continue;
+                }
+                else return false;
+            }
+            return true;
+        }
+
+        private bool IsPalindromeRight(string s, int left, int right)
+        {
+            if (s.Length <= 1) return true;
+
+            var chance = 1;
+
+            while (left <= right)
+            {
+                if (IsSameChar(s[left], s[right]) && chance >= 0)
+                {
+                    left++;
+                    right--;
+                }
+                else if (IsSameChar(s[left], s[right-1]) && chance > 0)
+                {
+                    right--;
+                    chance--;
+                    continue;
+                }
+                else return false;
+            }
+            return true;
+        }
+
+        private bool IsSameChar(char a, char b) => a == b;
+    }
+}
---
+++ leetcodesln_test/ValidPalindromeIIUnitTest.cs
@@ -0,0 +1,71 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ValidPalindromeIIUnitTest
+    {
+        public ValidPalindromeII validPalindromeII { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            validPalindromeII = new ValidPalindromeII();
+        }
+
+        [TestMethod]
+        public void ValidPalindromeII1()
+        {
+            var input = "aba";
+            var expected = true;
+            var actual = validPalindromeII.ValidPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeII2()
+        {
+            var input = "abca";
+            var expected = true;
+            var actual = validPalindromeII.ValidPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeII3()
+        {
+            var input = "aabdeenddbaagbddeedbaa";
+            var expected = false;
+            var actual = validPalindromeII.ValidPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeII4()
+        {
+            var input = "eedede";
+            var expected = true;
+            var actual = validPalindromeII.ValidPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeII5()
+        {
+            var input = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
+            var expected = true;
+            var actual = validPalindromeII.ValidPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidPalindromeII6()
+        {
+            var input = "acjakebdccdeedccdbeaca";
+            var expected = false;
+            var actual = validPalindromeII.ValidPalindrome(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
