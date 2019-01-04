afdec6d592d44d237a360d47182da133d49429ad
zhelun <stan_gai@Hotmail.com>
2019-01-04 14:49:54 +1100

    reverse words in a string 3 (Runtime: 128 ms, faster than 54.49%)

---
+++ leetcodesln/ReverseWordsInAStringIII.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace leetcodesln
+{
+    public class ReverseWordsInAStringIII
+    {
+        public string ReverseWords(string s)
+        {
+            var sb = new StringBuilder();
+            var words = s.Split(' ');
+            foreach (var word in words)
+            {
+                sb.Append(ReverseWord(word));
+                sb.Append(" ");
+            }
+            sb.Length--;
+            return sb.ToString();
+        }
+
+        private string ReverseWord(string word)
+        {
+            var strarr = new char[word.Length];
+            var left = 0;
+            var right = word.Length - 1;
+            while (left <= right)
+            {
+                strarr[left] = word[right];
+                strarr[right] = word[left];
+                left++;
+                right--;
+            }
+            return new string(strarr);
+        }
+    }
+}
---
+++ leetcodesln_test/ReverseWordsInAStringIIIUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ReverseWordsInAStringIIIUnitTest
+    {
+        public ReverseWordsInAStringIII reverseWordsInAStringIII { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            reverseWordsInAStringIII = new ReverseWordsInAStringIII();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseWordsInAStringIIITestMethod1()
+        {
+            var input = "Let's take LeetCode contest";
+            var expected = "s'teL ekat edoCteeL tsetnoc";
+            var actual = reverseWordsInAStringIII.ReverseWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseWordsInAStringIIITestMethod2()
+        {
+            var input = "let";
+            var expected = "tel";
+            var actual = reverseWordsInAStringIII.ReverseWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
