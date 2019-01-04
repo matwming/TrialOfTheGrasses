81fec7c540abfe25bb7224c633ae129197e219d5
zhelun <stan_gai@Hotmail.com>
2019-01-04 11:08:38 +1100

    reverse vowels of a string (Runtime: 200 ms, faster than 0.88%, two pointer solution. slow :question:)

---
+++ leetcodesln/ReverseVowelsOfAString.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace leetcodesln
+{
+    public class ReverseVowelsOfAString
+    {
+        public string ReverseVowels(string s)
+        {
+            if (s.Length <= 1) return s;
+
+            var vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            var str = s.ToCharArray();
+
+            var left = 0;
+            var right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (vowels.Contains(s[left]) && vowels.Contains(s[right]))
+                {
+                    var temp = s[left];
+                    str[left] = s[right];
+                    str[right] = temp;
+                    left++;
+                    right--;
+                }
+                if (!vowels.Contains(s[left])) left++;
+                if (!vowels.Contains(s[right])) right--;
+            }
+            return new string(str);
+        }
+    }
+}
---
+++ leetcodesln_test/ReverseVowelsOfAStringUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ReverseVowelsOfAStringUnitTest
+    {
+        public ReverseVowelsOfAString reverseVowelsOfAString { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            reverseVowelsOfAString = new ReverseVowelsOfAString();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseVowelsOfAStringTestMethod1()
+        {
+            var input = "hello";
+            var expected = "holle";
+            var actual = reverseVowelsOfAString.ReverseVowels(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseVowelsOfAStringTestMethod2()
+        {
+            var input = "leetcode";
+            var expected = "leotcede";
+            var actual = reverseVowelsOfAString.ReverseVowels(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseVowelsOfAStringTestMethod3()
+        {
+            var input = "a";
+            var expected = "a";
+            var actual = reverseVowelsOfAString.ReverseVowels(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseVowelsOfAStringTestMethod4()
+        {
+            var input = "a.";
+            var expected = "a.";
+            var actual = reverseVowelsOfAString.ReverseVowels(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseVowelsOfAStringTestMethod5()
+        {
+            var input = "aA";
+            var expected = "Aa";
+            var actual = reverseVowelsOfAString.ReverseVowels(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
