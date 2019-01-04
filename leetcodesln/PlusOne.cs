de50950622ef59997b1d73c8c8609e44bf6b089d
zhelun <stan_gai@Hotmail.com>
2019-01-05 08:20:51 +1100

    plus one (Runtime: 280 ms, faster than 96.02%)

---
+++ leetcodesln/PlusOne.cs
@@ -0,0 +1,50 @@
+﻿namespace leetcodesln
+{
+    public class PlusOne
+    {
+        public int[] PlusOneSln(int[] digits)
+        {
+            var startingIndex = digits.Length - 1;
+            while (TryAddUp(ref digits, startingIndex))
+            {
+                startingIndex--;
+            }
+            return digits;
+
+        }
+
+        private bool TryAddUp(ref int[] digits, int index)
+        {
+            var res = false;
+            if (index < 0)
+            {
+                digits = new int[digits.Length + 1];
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        digits[i] = 1;
+                    }
+                    else
+                    {
+                        digits[i] = 0;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                if (digits[index] + 1 >= 10)
+                {
+                    digits[index] = 0;
+                    res = true;
+                }
+                else
+                {
+                    digits[index] += 1;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ leetcodesln_test/PlusOneUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PlusOneUnitTest
+    {
+        public PlusOne plusOne { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            plusOne = new PlusOne();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void PlusOneTestMethod1()
+        {
+            var input = new int[] { 1, 2, 3 };
+            var expected = new int[] { 1, 2, 4 };
+            var actual = plusOne.PlusOneSln(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void PlusOneTestMethod2()
+        {
+            var input = new int[] { 1, 9, 9 };
+            var expected = new int[] { 2, 0, 0 };
+            var actual = plusOne.PlusOneSln(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
