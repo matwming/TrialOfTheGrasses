f484bd5ffd60287045a1a31d220c7b4713185570
zhelun <stan_gai@Hotmail.com>
2019-01-04 14:33:11 +1100

    reverse string (Runtime: 104 ms, faster than 97.19% using two pointers)

---
+++ leetcodesln/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class ReverseString
+    {
+        public string ReverseStringSln(string s)
+        {
+            var strarr = new char[s.Length];
+            var left = 0;
+            var right = s.Length - 1;
+
+            while (left <= right)
+            {
+                strarr[left] = s[right];
+                strarr[right] = s[left];
+                left++;
+                right--;
+            }
+            return new string(strarr);
+        }
+    }
+}
---
+++ leetcodesln_test/ReverseStringUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ReverseStringUnitTest
+    {
+        public ReverseString reverseString { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            reverseString = new ReverseString();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseStringTestMethod1()
+        {
+            var input = "hello";
+            var expected = "olleh";
+            var actual = reverseString.ReverseStringSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseStringTestMethod2()
+        {
+            var input = "hello1";
+            var expected = "1olleh";
+            var actual = reverseString.ReverseStringSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseStringTestMethod3()
+        {
+            var input = "1";
+            var expected = "1";
+            var actual = reverseString.ReverseStringSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ReverseStringTestMethod4()
+        {
+            var input = "A man, a plan, a canal: Panama";
+            var expected = "amanaP :lanac a ,nalp a ,nam A";
+            var actual = reverseString.ReverseStringSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
