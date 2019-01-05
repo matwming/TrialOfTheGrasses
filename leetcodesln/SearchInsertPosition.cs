2ee899bf485873e4c21ddfff40bca52136734795
zhelun <stan_gai@Hotmail.com>
2019-01-06 08:36:49 +1100

    search insert position (Runtime: 108 ms, faster than 100.00% , binary search)

---
+++ leetcodesln/SearchInsertPosition.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcodesln
+{
+    public class SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            var start = 0;
+            var end = nums.Length - 1;
+            var mid = (start + end) / 2;
+
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                if (nums[mid] == target)
+                {
+                    break;
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) return mid;
+            else return mid + 1;
+        }
+
+        //private int BinarySearch(int[] nums, int target, int start, int end)
+        //{
+        //    if (nums[(end - start) / 2] == target) return (end - start) / 2;
+        //    if ((end - start) / 2 > target) return BinarySearch(nums, target, start, (end - start) / 2);
+        //    if ((end - start) / 2 < target) return BinarySearch(nums, target, (end - start) / 2, end);
+        //    else return -1;
+        //}
+    }
+}
---
+++ leetcodesln_test/SearchInsertPositionUnitTest.cs
@@ -0,0 +1,80 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SearchInsertPositionUnitTest
+    {
+        public SearchInsertPosition searchInsertPosition { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            searchInsertPosition = new SearchInsertPosition();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SearchInsertPositionTestMethod1()
+        {
+            var input = new int[] { 1, 3, 5, 6 };
+            var target = 5;
+            var expected = 2;
+            var actual = searchInsertPosition.SearchInsert(input, target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SearchInsertPositionTestMethod2()
+        {
+            var input = new int[] { 1, 3, 5, 6 };
+            var target = 2;
+            var expected = 1;
+            var actual = searchInsertPosition.SearchInsert(input, target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SearchInsertPositionTestMethod3()
+        {
+            var input = new int[] { 1, 3, 5, 6 };
+            var target = 7;
+            var expected = 4;
+            var actual = searchInsertPosition.SearchInsert(input, target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SearchInsertPositionTestMethod4()
+        {
+            var input = new int[] { 1, 3, 5, 6 };
+            var target = 0;
+            var expected = 0;
+            var actual = searchInsertPosition.SearchInsert(input, target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SearchInsertPositionTestMethod5()
+        {
+            var input = new int[] { 1, 3 };
+            var target = 2;
+            var expected = 1;
+            var actual = searchInsertPosition.SearchInsert(input, target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SearchInsertPositionTestMethod6()
+        {
+            var input = new int[] { 1, 3,5 };
+            var target = 4;
+            var expected = 2;
+            var actual = searchInsertPosition.SearchInsert(input, target);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
