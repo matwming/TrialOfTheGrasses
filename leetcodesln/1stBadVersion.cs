b4e38c32c16593be513f7e15f9695eac455c23e6
zhelun <stan_gai@Hotmail.com>
2019-01-07 12:23:10 +1100

    first bad version (40ms, beats 100%)

---
+++ leetcodesln/1stBadVersion.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class VersionControl
+    {
+        public Dictionary<int, bool> VersionStatus { get; set; } = new Dictionary<int, bool>();
+        public VersionControl(int n, int firstBadVersion)
+        {
+            for (int i = 1; i < firstBadVersion; i++)
+            {
+                VersionStatus.Add(i, false);
+            }
+            for (int i = firstBadVersion; i <= n; i++)
+            {
+                VersionStatus.Add(i, true);
+            }
+        }
+
+        public bool IsBadVersion(int n)
+        {
+            return VersionStatus[n];
+        }
+    }
+    public class _1stBadVersion : VersionControl
+    {
+        public _1stBadVersion(int n, int firstBadVersion) : base(n, firstBadVersion)
+        {
+        }
+
+        public int FirstBadVersion(int n)
+        {
+            if (IsBadVersion(1)) return 1;
+
+            var L = 1;
+            var R = n;
+            while (L < R)
+            {
+                var m = L / 2 + R / 2;
+                if (IsBadVersion(m)) R = m;
+                else L = m + 1;
+            }
+            return R;
+        }
+    }
+}
---
+++ leetcodesln_test/1stBadVersionUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class _1stBadVersionUnitTest
+    {
+        [TestMethod, Timeout(20_000)]
+        public void UnitTest1()
+        {
+            var init = new _1stBadVersion(5, 4);
+            var expected = 4;
+            var actual = init.FirstBadVersion(5);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void UnitTest2()
+        {
+            var init = new _1stBadVersion(6, 3);
+            var expected = 3;
+            var actual = init.FirstBadVersion(6);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void UnitTest3()
+        {
+            var init = new _1stBadVersion(1, 1);
+            var expected = 1;
+            var actual = init.FirstBadVersion(1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void UnitTest4()
+        {
+            var init = new _1stBadVersion(2, 1);
+            var expected = 1;
+            var actual = init.FirstBadVersion(2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void UnitTest5()
+        {
+            var init = new _1stBadVersion(2, 2);
+            var expected = 2;
+            var actual = init.FirstBadVersion(2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //[TestMethod, Timeout(20_000)]
+        //public void UnitTest6()
+        //{
+        //    var init = new _1stBadVersion(2_126_753_390, 1_702_766_719);
+        //    var expected = 1_702_766_719;
+        //    var actual = init.FirstBadVersion(2_126_753_390);
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+    }
+}
