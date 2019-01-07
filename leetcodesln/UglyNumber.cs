39f226437e85ccb055ce1c1ebd253d7e49c34c90
zhelun <zhelun.gai@tmacomms.com>
2019-01-08 10:48:23 +1100

    ugly number (Runtime: 48 ms, faster than 55.00% )

---
+++ leetcodesln/UglyNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcodesln
+{
+    public class UglyNumber
+    {
+        public bool IsUgly(int num)
+        {
+            if (num == 1 || num == 2 || num == 3 || num == 5) return true;
+            else if (num == 0) return false;
+            else if (num % 2 == 0) return IsUgly(num / 2);
+            else if (num % 3 == 0) return IsUgly(num / 3);
+            else if (num % 5 == 0) return IsUgly(num / 5);
+            else return false;
+        }
+    }
+}
---
+++ leetcodesln_test/UglyNumberUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class UglyNumberUnitTest
+    {
+        public UglyNumber uglyNumber { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            uglyNumber = new UglyNumber();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void UglyNumberTestMethod1()
+        {
+            var input = 6;
+            var expected = true;
+            var actual = uglyNumber.IsUgly(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void UglyNumberTestMethod2()
+        {
+            var input = 8;
+            var expected = true;
+            var actual = uglyNumber.IsUgly(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void UglyNumberTestMethod3()
+        {
+            var input = 14;
+            var expected = false;
+            var actual = uglyNumber.IsUgly(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void UglyNumberTestMethod4()
+        {
+            var input = 0;
+            var expected = false;
+            var actual = uglyNumber.IsUgly(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
