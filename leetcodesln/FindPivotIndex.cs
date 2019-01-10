f9d69c2747bae5943f686d09faf071d74e03eca3
zhelun <zhelun.gai@tmacomms.com>
2019-01-10 14:19:56 +1100

    find pivot index (Runtime: 144 ms)

---
+++ leetcodesln/FindPivotIndex.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcodesln
+{
+    public class FindPivotIndex
+    {
+        public int PivotIndex(int[] nums)
+        {
+            if (nums.Length == 0) return -1;
+
+            var pivot = 0;
+            var left = 0;
+            var right = 0;
+
+            for (int i = 0; i < pivot; i++)
+            {
+                left += nums[i];
+            }
+
+            for (int i = nums.Length - 1; i > pivot; i--)
+            {
+                right += nums[i];
+            }
+
+            while (pivot < nums.Length)
+            {
+                if (left == right) return pivot;
+
+                if (pivot + 1 < nums.Length)
+                {
+                    left += nums[pivot];
+                    right -= nums[pivot + 1];
+                    pivot++;
+                }
+                else return -1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ leetcodesln_test/FindPivotIndexUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindPivotIndexUnitTest
+    {
+        public FindPivotIndex findPivotIndex { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            findPivotIndex = new FindPivotIndex();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void FindPivotIndexTestMethod1()
+        {
+            var input = new int[] { 1, 7, 3, 6, 5, 6 };
+            var expected = 3;
+            var actual = findPivotIndex.PivotIndex(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void FindPivotIndexTestMethod2()
+        {
+            var input = new int[] { 1, 2, 3 };
+            var expected = -1;
+            var actual = findPivotIndex.PivotIndex(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void FindPivotIndexTestMethod3()
+        {
+            var input = new int[] { -1, -1, -1, -1, -1, -1 };
+            var expected = -1;
+            var actual = findPivotIndex.PivotIndex(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void FindPivotIndexTestMethod4()
+        {
+            var input = new int[] { -1, -1, -1, 0, 1, 1 };
+            var expected = 0;
+            var actual = findPivotIndex.PivotIndex(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
