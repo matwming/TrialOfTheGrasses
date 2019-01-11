fc28e56923c01f4bf5dadada5d4d938b2e560d4f
zhelun <zhelun.gai@tmacomms.com>
2019-01-11 16:28:26 +1100

    maximize distance to closest person (Runtime: 136 ms, dynamic programming)

---
+++ leetcodesln/MaximizeDistanceToClosestPerson.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class MaximizeDistanceToClosestPerson
+    {
+        public int MaxDistToClosest(int[] seats)
+        {
+            var maxDistance = 0;
+
+            var distanceMapLeft = new Dictionary<int, int>();
+            var distanceMapRight = new Dictionary<int, int>();
+
+
+            for (int i = 0; i < seats.Length; i++)
+            {
+                if (seats[i] == 0)
+                {
+                    var left = MaxDistanceLeft(seats, i, distanceMapLeft);
+                    var right = MaxDistanceRight(seats, i, distanceMapRight);
+                    var max = 0;
+                    if (left != 0 && right != 0)
+                    {
+                        max = left > right ? right : left;
+
+                    }
+                    else if (left == 0)
+                    {
+                        max = right;
+                    }
+                    else if (right == 0)
+                    {
+                        max = left;
+                    }
+                    maxDistance = maxDistance > max ? maxDistance : max;
+                }
+
+            }
+            return maxDistance;
+        }
+
+        private int MaxDistanceLeft(int[] seats, int position, Dictionary<int, int> dp)
+        {
+
+            if (dp.ContainsKey(position)) return dp.GetValueOrDefault(position);
+
+            if (position == 0) return 0;
+            if (seats[position - 1] == 1) return 1;
+
+            return MaxDistanceLeft(seats, position - 1, dp) + 1;
+        }
+
+        private int MaxDistanceRight(int[] seats, int position, Dictionary<int, int> dp)
+        {
+
+            if (dp.ContainsKey(position)) return dp.GetValueOrDefault(position);
+
+            if (position == seats.Length - 1) return 0;
+            if (seats[position + 1] == 1) return 1;
+
+            return MaxDistanceRight(seats, position + 1, dp) + 1;
+        }
+    }
+}
---
+++ leetcodesln_test/MaximizeDistanceToClosestPersonUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MaximizeDistanceToClosestPersonUnitTest
+    {
+        public MaximizeDistanceToClosestPerson maximizeDistanceToClosestPerson { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            maximizeDistanceToClosestPerson = new MaximizeDistanceToClosestPerson();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void MaximizeDistanceToClosestPersonTestMethod1()
+        {
+            var input = new int[] { 1, 0, 0, 0, 1, 0, 1 };
+            var expected = 2;
+            var actual = maximizeDistanceToClosestPerson.MaxDistToClosest(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void MaximizeDistanceToClosestPersonTestMethod2()
+        {
+            var input = new int[] { 1, 0, 0, 0 };
+            var expected = 3;
+            var actual = maximizeDistanceToClosestPerson.MaxDistToClosest(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
