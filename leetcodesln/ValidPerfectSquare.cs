0c151d636203327b30f4a89b03e966b7073932c3
zhelun <zhelun.gai@tmacomms.com>
2019-01-14 15:33:42 +1100

    valid perfect square (Runtime: 44 ms, faster than 72.73% using binary search)

---
+++ leetcodesln/ValidPerfectSquare.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcodesln
+{
+    /// <summary>
+    /// find perfect square using binary search
+    /// </summary>
+    public class ValidPerfectSquare
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            if (num < 0) return false;
+            if (num == 1) return true;
+
+            long l = 0, r = num;
+
+            while (l <= r)
+            {
+                long mid = (l + r) / 2;
+
+                if (num / mid == mid && num % mid == 0) return true;
+                else if (num / mid > mid) l = mid + 1;
+                else if (num / mid < mid) r = mid - 1;
+                else return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/ValidPerfectSquareUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ValidPerfectSquareUnitTest
+    {
+        public ValidPerfectSquare validPerfectSquare { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            validPerfectSquare = new ValidPerfectSquare();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ValidPerfectSquareTestMethod1()
+        {
+            var input = 16;
+            var expected = true;
+            var actual = validPerfectSquare.IsPerfectSquare(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ValidPerfectSquareTestMethod2()
+        {
+            var input = 14;
+            var expected = false;
+            var actual = validPerfectSquare.IsPerfectSquare(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ValidPerfectSquareTestMethod3()
+        {
+            var input = 1;
+            var expected = true;
+            var actual = validPerfectSquare.IsPerfectSquare(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void ValidPerfectSquareTestMethod4()
+        {
+            var input = 5;
+            var expected = false;
+            var actual = validPerfectSquare.IsPerfectSquare(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
