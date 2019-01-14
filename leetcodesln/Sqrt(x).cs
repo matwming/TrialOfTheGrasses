d244fc4b4c7350877c877ff4ed806b13a2506133
zhelun <zhelun.gai@tmacomms.com>
2019-01-14 22:23:05 +1100

    sqrt(x) (Runtime: 48 ms, faster than 100.00% )

---
+++ leetcodesln/Sqrt(x).cs
@@ -0,0 +1,29 @@
+﻿namespace leetcodesln
+{
+    public class Sqrt_x_
+    {
+        public int MySqrt(int x)
+        {
+            if (x == 0) return 0;
+
+            int l = 1, r = x;
+
+            var mid = 0;
+
+            while (l + 1 < r)
+            {
+                mid = (l + r) >> 1;
+                if (x / mid < mid)
+                {
+                    r = mid;
+                }
+                else if (x / mid > mid)
+                {
+                    l = mid;
+                }
+                else return mid;
+            }
+            return l;
+        }
+    }
+}
---
+++ leetcodesln_test/Sqrt_x_UnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class Sqrt_x_UnitTest
+    {
+        public Sqrt_x_ sqrt_x_ { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            sqrt_x_ = new Sqrt_x_();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Sqrt_x_TestMethod1()
+        {
+            var input = 4;
+            var expected = 2;
+            var actual = sqrt_x_.MySqrt(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Sqrt_x_TestMethod2()
+        {
+            var input = 8;
+            var expected = 2;
+            var actual = sqrt_x_.MySqrt(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Sqrt_x_TestMethod3()
+        {
+            var input = 1;
+            var expected = 1;
+            var actual = sqrt_x_.MySqrt(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Sqrt_x_TestMethod4()
+        {
+            var input = 0;
+            var expected = 0;
+            var actual = sqrt_x_.MySqrt(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
