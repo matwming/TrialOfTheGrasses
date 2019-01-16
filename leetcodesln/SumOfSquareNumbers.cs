ba7a206649bafd2c4d5550ec74942c98a757b87b
zhelun <zhelun.gai@tmacomms.com>
2019-01-17 09:54:54 +1100

    sum of square numbers (Runtime: 44 ms, faster than 100.00%)

---
+++ leetcodesln/SumOfSquareNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class SumOfSquareNumbers
+    {
+        public bool JudgeSquareSum(int c)
+        {
+            int l = 0, r = (int)Math.Sqrt(c);
+            while (l <= r)
+            {
+                if (l * l + r * r == c) return true;
+                if (l * l + r * r < c) l++;
+                if (l * l + r * r > c) r--;
+
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/SumOfSquareNumbersUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SumOfSquareNumbersUnitTest
+    {
+        public SumOfSquareNumbers sumOfSquareNumbers { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            sumOfSquareNumbers = new SumOfSquareNumbers();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SumOfSquareNumbersTestMethod1()
+        {
+            var input = 5;
+            var expected = true;
+            var actual = sumOfSquareNumbers.JudgeSquareSum(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SumOfSquareNumbersTestMethod2()
+        {
+            var input = 3;
+            var expected = false;
+            var actual = sumOfSquareNumbers.JudgeSquareSum(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void SumOfSquareNumbersTestMethod3()
+        {
+            var input = 10_000_000;
+            var expected = true;
+            var actual = sumOfSquareNumbers.JudgeSquareSum(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
