e63337dd0c718959fa7481465529ef57b8360086
zhelun <zhelun.gai@tmacomms.com>
2019-01-18 10:30:04 +1100

    repeated substring pattern (Runtime: 136 ms, faster than 68.57%)

---
+++ leetcodesln/RepeatedSubstringPattern.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcodesln
+{
+    public class RepeatedSubstringPattern
+    {
+        public bool RepeatedSubstringPatternSln(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length == 0) return false;
+
+            var strlen = s.Length;
+            for (int i = 1; i < strlen; i++)
+            {
+                if (strlen % i == 0)
+                {
+                    if (IsRepeatedSubstring(s, i)) return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsRepeatedSubstring(string s, int i)
+        {
+            
+            var baseline = s.Substring(0, i);
+
+            for (int j = 0; j < s.Length; j += i)
+            {
+                if (s.Substring(j, i) != baseline) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/RepeatedSubstringPatternUnitTest.cs
@@ -0,0 +1,80 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RepeatedSubstringPatternUnitTest
+    {
+        public RepeatedSubstringPattern repeatedSubstringPattern { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            repeatedSubstringPattern = new RepeatedSubstringPattern();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void RepeatedSubstringPatternTestMethod1()
+        {
+            var input = "abab";
+            var expected = true;
+            var actual = repeatedSubstringPattern.RepeatedSubstringPatternSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void RepeatedSubstringPatternTestMethod2()
+        {
+            var input = "aba";
+            var expected = false;
+            var actual = repeatedSubstringPattern.RepeatedSubstringPatternSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void RepeatedSubstringPatternTestMethod3()
+        {
+            var input = "abcabcabcabc";
+            var expected = true;
+            var actual = repeatedSubstringPattern.RepeatedSubstringPatternSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void RepeatedSubstringPatternTestMethod4()
+        {
+            var input = "bb";
+            var expected = true;
+            var actual = repeatedSubstringPattern.RepeatedSubstringPatternSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void RepeatedSubstringPatternTestMethod5()
+        {
+            var input = "abac";
+            var expected = false;
+            var actual = repeatedSubstringPattern.RepeatedSubstringPatternSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void RepeatedSubstringPatternTestMethod6()
+        {
+            var input = "ababab";
+            var expected = true;
+            var actual = repeatedSubstringPattern.RepeatedSubstringPatternSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void RepeatedSubstringPatternTestMethod7()
+        {
+            var input = "babbabbabbabbab";
+            var expected = true;
+            var actual = repeatedSubstringPattern.RepeatedSubstringPatternSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
