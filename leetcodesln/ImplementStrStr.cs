677b40728f64fa5a3eb30bcd368e1a7528b1d7b2
YoungStan <stan_gai@Hotmail.com>
2019-01-19 22:57:30 +1100

    wip

---
+++ leetcodesln/ImplementStrStr.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace leetcodesln
+{
+    public class ImplementStrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) throw new NullReferenceException();
+
+            return -1;
+        }
+    }   
+}
