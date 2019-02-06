4f8221d8e895c33cbe0e5371e0f2bb40017c0a29
user3301 <stan_gai@Hotmail.com>
2019-02-06 12:24:51 +0800

    powerful integers (using optimised brutal force,Runtime: 268 ms, faster than 72.73% )

--- leetcodesln/PowerfulIntegers.cs
+++ leetcodesln/PowerfulIntegers.cs
@@ -9,21 +9,23 @@ namespace leetcodesln
         {
             if(bound < 2) return new List<int>();
 
-            var resultList = new List<int>();
+            var hashset = new HashSet<int>();
 
             for (int i = 0; Math.Pow(x, i) <= bound; i++)
             {
                 for (int j = 0; Math.Pow(y, j) <= bound; j++)
                 {
                     var temp = Math.Pow(x, i) + Math.Pow(y, j);
-                    if( temp <= bound)
+                    if( temp <= bound) 
                     {
-                        if(!resultList.Contains((int)temp)) resultList.Add((int)temp);
+                        hashset.Add((int)temp);
+                        if(y==0 || y ==1) break;
                     }
                     else break;
                 }
+                if(x==0 || x == 1) break;
             }
-            return resultList;
+            return new List<int>(hashset);
         }
     }
 }
