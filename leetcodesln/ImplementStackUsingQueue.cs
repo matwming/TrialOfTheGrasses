2b2ebb92eda931b8af7a76411679e866d4dd1705
zhelun <zhelun.gai@tmacomms.com>
2019-02-18 15:36:17 +1100

    implement stack using queue(Runtime: 120 ms, faster than 98.68%)

---
+++ leetcodesln/ImplementStackUsingQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class ImplementStackUsingQueue
+    {
+        private Queue<int> Storage { get; set; }
+
+        /** Initialize your data structure here */
+        public ImplementStackUsingQueue()
+        {
+            Storage = new Queue<int>();
+        }
+
+        /** Push element x onto stack */
+        public void Push(int x)
+        {
+            Storage.Enqueue(x);
+        }
+
+        /** Removes the element on top of the stack and returns that element */
+        public int Pop()
+        {
+            if (Storage.Count <= 0) throw new NullReferenceException("the stack is empty");
+
+            var aux = new Queue<int>();
+            while(Storage.Count!=0)
+            {
+                var current = Storage.Dequeue();
+                if (Storage.Count == 0)
+                {
+                    Storage = aux;
+                    return current;
+                }
+                aux.Enqueue(current);
+            }
+            throw new Exception();
+        }
+
+        /** Get the top element */
+        public int Top()
+        {
+            if (Storage.Count <= 0) throw new NullReferenceException("the stack is empty");
+
+            var aux = new Queue<int>();
+            var current = default(int);
+            while (Storage.Count != 0)
+            {
+                current = Storage.Dequeue();
+                aux.Enqueue(current);
+            }
+            Storage = aux;
+            return current;
+
+        }
+
+        /** Returns whether the stack is empty */
+        public bool Empty()
+        {
+            return Storage.Count == 0;
+
+        }
+    }
+
+    /**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Top();
+ * bool param_4 = obj.Empty();
+ */
+}
---
+++ leetcodesln_test/ImplementStackUsingQueueUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ImplementStackUsingQueueUnitTest
+    {
+        public ImplementStackUsingQueue ImplementStackUsingQueue { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ImplementStackUsingQueue = new ImplementStackUsingQueue();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            ImplementStackUsingQueue.Push(1);
+            ImplementStackUsingQueue.Push(2);
+            Assert.AreEqual(2, ImplementStackUsingQueue.Top());
+            Assert.AreEqual(2, ImplementStackUsingQueue.Pop());
+            Assert.IsFalse(ImplementStackUsingQueue.Empty());
+        }
+    }
+}
