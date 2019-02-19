13fed938982de6a5c4eb10b08197f76d04444d1f
zhelun <zhelun.gai@tmacomms.com>
2019-02-19 14:36:36 +1100

    arranging coins(Runtime: 84 ms, faster than 21.57%)

---
+++ leetcodesln/ArrangingCoins.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcodesln
+{
+    public class ArrangingCoins
+    {
+        public int ArrangeCoins(int n)
+        {
+            var i = 0;
+
+            while (n > 0)
+            {
+                if (n - i >= 0) n -= i;
+                i++;
+                if (n - i < 0)
+                {
+                    i--;
+                    break;
+                }
+            }
+            return i;
+        }
+    }
+}
---
+++ leetcodesln_test/ArrangingCoinsUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ArrangingCoinsUnitTest
+    {
+        public ArrangingCoins ArrangingCoins { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ArrangingCoins = new ArrangingCoins();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var n = 5;
+            var expected = 2;
+            var actual = ArrangingCoins.ArrangeCoins(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+
+}
