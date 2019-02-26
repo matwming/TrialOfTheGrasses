f55787d87b0117db8743eb5d7969409a53e33c46
zhelun <zhelun.gai@tmacomms.com>
2019-02-26 14:23:07 +1100

    isomorphic strings (online good solution, Runtime: 72 ms, faster than 100.00%)

--- leetcodesln/IsomorphicStrings.cs
+++ leetcodesln/IsomorphicStrings.cs
@@ -1,34 +1,51 @@
-using System;
-using System.Collections.Generic;
-
 namespace leetcodesln
 {
     public class IsomorphicStrings
     {
+        //public bool IsIsomorphic(string s, string t)
+        //{
+        //    return s == t ? true : s.Length == t.Length ? IsIsomorphicAux(s, t): false;
+        //}
+
+        //private bool IsIsomorphicAux(string s, string t)
+        //{
+        //    var dicts = new Dictionary<char, int>();
+        //    var dictt = new Dictionary<char, int>();
+        //    var ds = 0;
+        //    var dt = 0;
+        //    var nstrs = string.Empty;
+        //    var nstrt = string.Empty;
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        if(!dicts.ContainsKey(s[i])) dicts.Add(s[i],ds++);
+        //        if(!dictt.ContainsKey(t[i])) dictt.Add(t[i], dt++);
+        //    }
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        nstrs += dicts[s[i]];
+        //        nstrt += dictt[t[i]];
+        //    }
+        //    return nstrs == nstrt;
+        //}
+
+        //a good solution
         public bool IsIsomorphic(string s, string t)
         {
-            return s == t ? true : s.Length == t.Length ? IsIsomorphicAux(s, t): false;
+            return s == t ? true : s.Length == t.Length ? IsIsomorphicAux(s, t) : false;
         }
 
         private bool IsIsomorphicAux(string s, string t)
         {
-            var dicts = new Dictionary<char, int>();
-            var dictt = new Dictionary<char, int>();
-            var ds = 0;
-            var dt = 0;
-            var nstrs = string.Empty;
-            var nstrt = string.Empty;
+            int[] sm = new int[128];
+            int[] tm = new int[128];
+
             for (int i = 0; i < s.Length; i++)
             {
-                if(!dicts.ContainsKey(s[i])) dicts.Add(s[i],ds++);
-                if(!dictt.ContainsKey(t[i])) dictt.Add(t[i], dt++);
+                if (sm[s[i]] != tm[t[i]]) return false;
+                sm[s[i]] = i + 1;  // use i+1 instead of i as arrays are initialized as 0, to avoid putting 0 into the array for avoiding this ambiguity
+                tm[t[i]] = i + 1;
             }
-            for (int i = 0; i < s.Length; i++)
-            {
-                nstrs += dicts[s[i]];
-                nstrt += dictt[t[i]];
-            }
-            return nstrs == nstrt;
+            return true;
         }
     }
 }
