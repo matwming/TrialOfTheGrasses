f556300499d9d29fe9dedb50bd21d87006a8aff0
zhelun <zhelun.gai@tmacomms.com>
2019-02-27 17:18:23 +1100

    number of segments in a string(using build-in split method, Runtime: 72 ms, faster than 100.00%)

---
+++ leetcodesln/NumberOfSegmentsInAString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class NumberOfSegmentsInAString
+    {
+        public int CountSegments(string s)
+        {
+            return s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
---
+++ leetcodesln_test/NumberOfSegmentsInAStringUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class NumberOfSegmentsInAStringUnitTest
+    {
+        public NumberOfSegmentsInAString NumberOfSegmentsInAString { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            NumberOfSegmentsInAString = new NumberOfSegmentsInAString();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = "Hello, my name is Join";
+            var expected = 5;
+            var actual = NumberOfSegmentsInAString.CountSegments(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
