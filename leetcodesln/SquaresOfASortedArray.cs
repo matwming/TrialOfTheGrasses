b4a4330f74a1d4593be628e157eb9df9eb1f2062
zhelun <zhelun.gai@tmacomms.com>
2019-02-28 16:47:36 +1100

    squares of a sorted array (using two pointer, Runtime: 292 ms, faster than 100.00%)

---
+++ leetcodesln/SquaresOfASortedArray.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcodesln
+{
+    public class SquaresOfASortedArray
+    {
+        /// <summary>
+        /// Since the input array is sorted in non-decreasing order, the most intutive way is to create an array of the squares of each element and sort them
+        /// This solution is using two pointer with O(N) complexity
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public int[] SortedSquares(int[] A)
+        {
+            var front = 0;
+            var rear = A.Length - 1;
+            var result = new int[A.Length];
+            var n = A.Length - 1;
+
+            while (front <= rear)
+            {
+                if (A[front] * A[front] > A[rear] * A[rear])
+                {
+                    result[n--] = A[front] * A[front];
+                    front++;
+                }
+                else
+                {
+                    result[n--] = A[rear] * A[rear];
+                    rear--;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/SquaresOfASortedArrayUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SquaresOfASortedArrayUnitTest
+    {
+        public SquaresOfASortedArray SquaresOfASortedArray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            SquaresOfASortedArray = new SquaresOfASortedArray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { -4, -1, 0, 3, 10 };
+            var expected = new int[] { 0, 1, 9, 16, 100 };
+            var actual = SquaresOfASortedArray.SortedSquares(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { -7, -3, 2, 3, 11 };
+            var expected = new int[] { 4, 9, 9, 49, 121 };
+            var actual = SquaresOfASortedArray.SortedSquares(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
