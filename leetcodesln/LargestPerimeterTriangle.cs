2e61ce8b17cd238e20790ed313c07c7dfff13dfd
zhelun <zhelun.gai@tmacomms.com>
2019-03-02 14:55:47 +1100

    largest perimeter triangle(Runtime: 176 ms, faster than 40.00% )

---
+++ leetcodesln/LargestPerimeterTriangle.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace leetcodesln
+{
+    public class LargestPerimeterTriangle
+    {
+        public int LargestPerimeter(int[] A)
+        {
+            var sortedAscending = (from i in A orderby i ascending select i).ToArray();
+
+            for (int i = A.Length - 1; i > 1; i--)
+            {
+                if (sortedAscending[i] < sortedAscending[i - 1] + sortedAscending[i - 2]) return sortedAscending[i] + sortedAscending[i - 1] + sortedAscending[i - 2];
+            }
+            return 0;
+        }
+    }
+}
---
+++ leetcodesln_test/LargestPerimeterTriangleUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LargestPerimeterTriangleUnitTest
+    {
+        public LargestPerimeterTriangle LargestPerimeterTriangle { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            LargestPerimeterTriangle = new LargestPerimeterTriangle();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 2, 1, 2 };
+            var expected = 5;
+            var actual = LargestPerimeterTriangle.LargestPerimeter(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1, 2, 1 };
+            var expected = 0;
+            var actual = LargestPerimeterTriangle.LargestPerimeter(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 3, 2, 3, 4 };
+            var expected = 10;
+            var actual = LargestPerimeterTriangle.LargestPerimeter(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var input = new int[] { 3, 6, 2, 3 };
+            var expected = 8;
+            var actual = LargestPerimeterTriangle.LargestPerimeter(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
