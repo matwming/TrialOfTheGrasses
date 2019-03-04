a6a1c0ca500f49a7ca346f9d4faefa5d3564bab5
zhelun <zhelun.gai@tmacomms.com>
2019-03-04 12:47:30 +1100

    find common characters(using frequency list,Runtime: 252 ms, faster than 100.00%  )

---
+++ leetcodesln/FindCommonCharacters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class FindCommonCharacters
+    {
+        public IList<string> CommonChars(string[] A)
+        {
+            var frequencyList = new int[26];
+            Array.Fill(frequencyList, int.MaxValue);
+
+            foreach (var str in A)
+            {
+                var countList = BuildStrFrequencyList(str);
+                for (int i = 0; i < frequencyList.Length; i++)
+                {
+                    frequencyList[i] = Math.Min(countList[i], frequencyList[i]);
+                }
+            }
+            return FormResultList(frequencyList);
+        }
+
+        private IList<string> FormResultList(int[] frequencyList)
+        {
+            var resultList = new List<string>();
+
+            for (int i = 0; i < frequencyList.Length; i++)
+            {
+                for (int j = frequencyList[i]; j > 0; j--)
+                {
+                    resultList.Add((((char)(i + 'a')).ToString()));
+                }
+            }
+            return resultList;
+        }
+
+        private int[] BuildStrFrequencyList(string str)
+        {
+            int[] count = new int[26];
+            foreach (var chr in str)
+            {
+                count[chr - 'a']++;
+            }
+            return count;
+        }
+    }
+}
---
+++ leetcodesln_test/FindCommonCharactersUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindCommonCharactersUnitTest
+    {
+        public FindCommonCharacters FindCommonCharacters { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            FindCommonCharacters = new FindCommonCharacters();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new string[] { "bella", "label", "roller" };
+            var expected = new string[] { "e", "l", "l" };
+            var actual = FindCommonCharacters.CommonChars(input);
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, actual));
+
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new string[] { "cool", "lock", "cook" };
+            var expected = new string[] { "c", "o" };
+            var actual = FindCommonCharacters.CommonChars(input);
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, actual));
+
+        }
+    }
+}
