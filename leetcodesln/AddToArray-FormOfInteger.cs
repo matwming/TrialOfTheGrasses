9831c41c776265855570a53e82407d7fc8cfed91
zhelun <zhelun.gai@tmacomms.com>
2019-03-06 16:26:15 +1100

    add to array-form of integer(Runtime: 332 ms, faster than 88.24%)

--- leetcodesln/AddToArray-FormOfInteger.cs
+++ leetcodesln/AddToArray-FormOfInteger.cs
@@ -6,52 +6,18 @@ namespace leetcodesln
     {
         public IList<int> AddToArrayForm(int[] A, int K)
         {
-            var result = new int[256];
-            for (int i = A.Length - 1, j = 255; i >= 0; i--, j--)
+            var carry = 0;
+            var result = new List<int>();
+
+            for (int i = A.Length - 1; i >= 0 || K != 0 || carry !=0; i--, K/=10)
             {
-                result[j] = A[i];
+                var valueToBeInserted = (i >= 0 ? A[i] : 0) + carry + (K == 0 ? 0 : K % 10);
+
+                carry = valueToBeInserted > 9 ? 1:0;
+
+                result.Insert(0, valueToBeInserted % 10);
             }
-
-            bool r = false;
-
-            var ptr = result.Length - 1;
-            while (K != 0)
-            {
-                if (r) result[ptr] += 1;
-                var res = result[ptr] + K % 10;
-                r = res > 9;
-                result[ptr] = res > 9 ? res - 10 : res;
-                ptr--;
-                K /= 10;
-            }
-
-            while (r)
-            {
-                var temp = result[ptr] + 1;
-                r = temp > 9;
-                result[ptr--] = temp > 9 ? temp - 10 : temp;
-            }
-
-            bool flag = false;
-            var list = new List<int>();
-            for (int i = 0; i < result.Length; i++)
-            {
-                if (!flag)
-                {
-                    if (result[i] != 0)
-                    {
-                        list.Add(result[i]);
-                        flag = true;
-                    }
-                }
-                else
-                {
-                    list.Add(result[i]);
-                }
-            }
-
-            if (list.Count == 0) return new int[] { 0 };
-            return list;
+            return result;
         }
     }
 }
