75ba8843ba27dc3110440dfb44bbc710460e5f5e
zhelun <zhelun.gai@tmacomms.com>
2019-03-06 16:02:06 +1100

    wip

--- leetcodesln/AddToArray-FormOfInteger.cs
+++ leetcodesln/AddToArray-FormOfInteger.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace leetcodesln
@@ -7,21 +6,52 @@ namespace leetcodesln
     {
         public IList<int> AddToArrayForm(int[] A, int K)
         {
-            var result = new List<int>();
-            uint arrValue = 0;
-            for (int i = A.Length-1, j = 0; i >= 0; i--)
+            var result = new int[256];
+            for (int i = A.Length - 1, j = 255; i >= 0; i--, j--)
             {
-                arrValue += (uint)(A[i] * Math.Pow(10,j));
-                j++;
+                result[j] = A[i];
             }
-            arrValue = (uint)(arrValue + K);
-            while(arrValue!=0)
+
+            bool r = false;
+
+            var ptr = result.Length - 1;
+            while (K != 0)
             {
-                result.Add((int)arrValue%10);
-                arrValue/=10;
+                if (r) result[ptr] += 1;
+                var res = result[ptr] + K % 10;
+                r = res > 9;
+                result[ptr] = res > 9 ? res - 10 : res;
+                ptr--;
+                K /= 10;
             }
-            result.Reverse();
-            return result;
+
+            while (r)
+            {
+                var temp = result[ptr] + 1;
+                r = temp > 9;
+                result[ptr--] = temp > 9 ? temp - 10 : temp;
+            }
+
+            bool flag = false;
+            var list = new List<int>();
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (!flag)
+                {
+                    if (result[i] != 0)
+                    {
+                        list.Add(result[i]);
+                        flag = true;
+                    }
+                }
+                else
+                {
+                    list.Add(result[i]);
+                }
+            }
+
+            if (list.Count == 0) return new int[] { 0 };
+            return list;
         }
     }
 }
