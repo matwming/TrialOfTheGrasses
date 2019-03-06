3e461b97c806e526d263e7af5e2bead21ca6e52c
zhelun <zhelun.gai@tmacomms.com>
2019-03-07 10:11:06 +1100

    pascal's triangle (Runtime: 224 ms, faster than 88.32%)

---
+++ leetcodesln/PascalsTriangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class PascalsTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var result = new int[numRows][];
+            for (int i = 0; i < numRows; i++)
+            {
+                result[i] = new int[i + 1];
+                result[i][0] = 1;
+                result[i][result[i].Length - 1] = 1;
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = 0; j < result[i].Length; j++)
+                {
+                    if(result[i][j]==0)
+                    {
+                        result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/PascalsTriangleUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PascalsTriangleUnitTest
+    {
+        public PascalsTriangle PascalsTriangle { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            PascalsTriangle = new PascalsTriangle();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var numRows = 5;
+            var expected = new int[5][];
+            expected[0] = new int[] { 1 };
+            expected[1] = new int[] { 1, 1 };
+            expected[2] = new int[] { 1, 2, 1 };
+            expected[3] = new int[] { 1, 3, 3, 1 };
+            expected[4] = new int[] { 1, 4, 6, 4, 1 };
+            var actual = PascalsTriangle.Generate(numRows);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+
+        }
+    }
+}
