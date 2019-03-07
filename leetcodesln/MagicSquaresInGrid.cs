98abe60d146cac262f47165ae3c0a0073c1c5ea4
user3301 <stan_gai@Hotmail.com>
2019-03-08 10:42:36 +1100

    magic squares in grid(not AC solution, this solution works for 3*3 square with distinct number NOT from 1-9 using brute force)

---
+++ leetcodesln/MagicSquaresInGrid.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcodesln
+{
+    public class MagicSquaresInGrid
+    {
+        public int NumMagicSquaresInside(int[][] grid)
+        {
+            var count = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (IsValidGrid(i, j, grid))
+                    {
+                        if (grid[i][j] == grid[i + 1][j] || grid[i + 1][j] == grid[i + 2][j] || grid[i][j] == grid[i + 2][j]) continue;
+                        var sumOfRow = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
+                        if (CheckRest(sumOfRow, i, j, grid)) count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private bool CheckRest(int sumOfRow, int i, int j, int[][] grid)
+        {
+            if (grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] != sumOfRow || grid[i + 1][j] == grid[i + 1][j + 1] || grid[i + 1][j + 1] == grid[i + 1][j + 2] || grid[i + 1][j] == grid[i + 1][j + 2]) return false;
+            if (grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2] != sumOfRow || grid[i + 2][j] == grid[i + 2][j + 1] || grid[i + 2][j + 1] == grid[i + 2][j + 2] || grid[i + 2][j] == grid[i + 2][j + 2]) return false;
+            if (grid[i][j] + grid[i + 1][j] + grid[i + 2][j] != sumOfRow || grid[i][j] == grid[i + 1][j] || grid[i + 1][j] == grid[i + 2][j] || grid[i][j] == grid[i + 2][j]) return false;
+            if (grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1] != sumOfRow || grid[i][j + 1] == grid[i + 1][j + 1] || grid[i + 1][j + 1] == grid[i + 2][j + 1] || grid[i][j + 1] == grid[i + 2][j + 1]) return false;
+            if (grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2] != sumOfRow || grid[i + 2][j] == grid[i + 2][j + 1] || grid[i + 2][j + 1] == grid[i + 2][j + 2] || grid[i + 2][j] == grid[i + 2][j + 2]) return false;
+            if (grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2] != sumOfRow || grid[i][j] == grid[i + 1][j + 1] || grid[i + 1][j + 1] == grid[i + 2][j + 2] || grid[i][j] == grid[i + 2][j + 2]) return false;
+            if (grid[i + 2][j] + grid[i + 1][j + 1] + grid[i][j + 2] != sumOfRow || grid[i + 2][j] == grid[i + 1][j + 1] || grid[i + 1][j + 1] == grid[i][j + 2] || grid[i + 2][j] == grid[i][j + 2]) return false;
+            return true;
+
+        }
+
+        private bool IsValidGrid(int i, int j, int[][] grid)
+        {
+            return i + 2 <= grid.Length - 1 && j + 2 <= grid[0].Length - 1;
+        }
+    }
+}
---
+++ leetcodesln_test/MagicSquaresInGridUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MagicSquaresInGridUnitTest
+    {
+        public MagicSquaresInGrid MagicSquaresInGrid { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MagicSquaresInGrid = new MagicSquaresInGrid();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[3][];
+            input[0] = new int[] { 4, 3, 8, 4 };
+            input[1] = new int[] { 9, 5, 1, 9 };
+            input[2] = new int[] { 2, 7, 6, 2 };
+            var expected = 1;
+            int actual = MagicSquaresInGrid.NumMagicSquaresInside(input);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[3][];
+            input[0] = new int[] { 5, 5, 5 };
+            input[1] = new int[] { 5, 5, 5 };
+            input[2] = new int[] { 5, 5, 5 };
+            var expected = 0;
+            int actual = MagicSquaresInGrid.NumMagicSquaresInside(input);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[3][];
+            input[0] = new int[] { 10, 3, 5 };
+            input[1] = new int[] { 1, 6, 11 };
+            input[2] = new int[] { 7, 9, 2 };
+            var expected = 0;
+            int actual = MagicSquaresInGrid.NumMagicSquaresInside(input);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
