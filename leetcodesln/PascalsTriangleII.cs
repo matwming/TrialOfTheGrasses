15fefa5a2b86d46b330d6b00a92dab9f38d2c2f6
user3301 <stan_gai@Hotmail.com>
2019-03-07 15:12:08 +1100

    pascal's triangle 2 (using dp, Runtime: 212 ms, faster than 96.02% )

--- leetcodesln/PascalsTriangleII.cs
+++ leetcodesln/PascalsTriangleII.cs
@@ -7,24 +7,33 @@ namespace leetcodesln
     {
         public IList<int> GetRow(int rowIndex)
         {
+            var dp = new int[rowIndex][];
+            for (int i = 0; i < rowIndex; i++)
+            {
+                dp[i] = new int[i + 1];
+                Array.Fill(dp[i], -1);
+            }
+
             var result = new int[rowIndex + 1];
             Array.Fill(result, 1);
             for (int i = 1; i < result.Length - 1; i++)
             {
                 if (result[i] == 1)
                 {
-                    result[i] = GetValue(rowIndex-1, i);
+                    result[i] = GetValue(rowIndex - 1, i, ref dp);
                 }
             }
 
             return result;
         }
 
-        private int GetValue(int rowIndex, int i)
+        private int GetValue(int rowIndex, int i, ref int[][] dp)
         {
             if (rowIndex == i - 1 || i == 0) return 1;
+            if (dp[rowIndex][i] != -1) return dp[rowIndex][i];
 
-            return GetValue(rowIndex - 1, i - 1) + GetValue(rowIndex - 1, i);
+            dp[rowIndex][i] = GetValue(rowIndex - 1, i - 1, ref dp) + GetValue(rowIndex - 1, i, ref dp);
+            return dp[rowIndex][i];
         }
     }
 }
--- leetcodesln_test/PascalsTriangleIIUnitTest.cs
+++ leetcodesln_test/PascalsTriangleIIUnitTest.cs
@@ -1,8 +1,5 @@
 ﻿using leetcodesln;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace leetcodesln_test
 {
@@ -22,6 +19,21 @@ namespace leetcodesln_test
         {
             var input = 3;
             var expected = new int[] { 1, 3, 3, 1 };
+            var actual = PascalsTriangleII.GetRow(input);
+            for (int i = 0; i < actual.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        /// <summary>
+        ///  This test case will exceed time limit if using non-dp recursion
+        /// </summary>
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = 30;
+            var expected = new int[] { 1, 30, 435, 4060, 27405, 142506, 593775, 2035800, 5852925, 14307150, 30045015, 54627300, 86493225, 119759850, 145422675, 155117520, 145422675, 119759850, 86493225, 54627300, 30045015, 14307150, 5852925, 2035800, 593775, 142506, 27405, 4060, 435, 30, 1 };
             var actual = PascalsTriangleII.GetRow(input);
             for (int i = 0; i < actual.Count; i++)
             {
