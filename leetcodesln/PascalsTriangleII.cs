8d78f6c63aa5c234d8bc8793091c080185642905
user3301 <stan_gai@Hotmail.com>
2019-03-07 14:53:54 +1100

    pascal's triangle 2 (time limit exceeded)

---
+++ leetcodesln/PascalsTriangleII.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class PascalsTriangleII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var result = new int[rowIndex + 1];
+            Array.Fill(result, 1);
+            for (int i = 1; i < result.Length - 1; i++)
+            {
+                if (result[i] == 1)
+                {
+                    result[i] = GetValue(rowIndex-1, i);
+                }
+            }
+
+            return result;
+        }
+
+        private int GetValue(int rowIndex, int i)
+        {
+            if (rowIndex == i - 1 || i == 0) return 1;
+
+            return GetValue(rowIndex - 1, i - 1) + GetValue(rowIndex - 1, i);
+        }
+    }
+}
---
+++ leetcodesln_test/PascalsTriangleIIUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PascalsTriangleIIUnitTest
+    {
+        public PascalsTriangleII PascalsTriangleII { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            PascalsTriangleII = new PascalsTriangleII();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = 3;
+            var expected = new int[] { 1, 3, 3, 1 };
+            var actual = PascalsTriangleII.GetRow(input);
+            for (int i = 0; i < actual.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
