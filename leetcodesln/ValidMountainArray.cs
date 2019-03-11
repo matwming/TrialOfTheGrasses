9cac36d0d24678266e3a83801122d30d3309de7c
user3301 <stan_gai@Hotmail.com>
2019-03-12 09:28:21 +1100

    valid mountain array(nasty code but AC, Runtime: 136 ms, faster than 85.29%)

---
+++ leetcodesln/ValidMountainArray.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcodesln
+{
+    public class ValidMountainArray
+    {
+        public bool IsValidMountainArray(int[] A)
+        {
+            if (A.Length < 3) return false;
+            if (A[0] > A[1]) return false;
+
+            var turningPointCount = 0;
+
+            for (int i = 2; i < A.Length; i++)
+            {
+                if (A[i] - A[i - 1] >= 0 && turningPointCount == 0) continue;
+                if (A[i] - A[i - 1] >= 0 && turningPointCount != 0) return false;
+                if (A[i] - A[i - 1] < 0 && turningPointCount == 0)
+                {
+                    turningPointCount++;
+                }
+                if (turningPointCount > 1) return false;
+            }
+            return turningPointCount == 1;
+        }
+    }
+}
---
+++ leetcodesln_test/ValidMountainArrayUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ValidMountainArrayUnitTest
+    {
+        public ValidMountainArray ValidMountainArray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ValidMountainArray = new ValidMountainArray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 2, 1 };
+            var expected = false;
+            var actual = ValidMountainArray.IsValidMountainArray(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 3, 5, 5 };
+            var expected = false;
+            var actual = ValidMountainArray.IsValidMountainArray(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 0, 3, 2, 1 };
+            var expected = true;
+            var actual = ValidMountainArray.IsValidMountainArray(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var input = new int[] { 0, 1, 2, 1, 2 };
+            var expected = false;
+            var actual = ValidMountainArray.IsValidMountainArray(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
