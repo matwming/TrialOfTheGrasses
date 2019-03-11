518bbb01d99e88e12e42113a952a304d2c68611c
user3301 <stan_gai@Hotmail.com>
2019-03-12 09:44:53 +1100

    valid mountain array (two ways climbing mountain algorithm)

--- leetcodesln/ValidMountainArray.cs
+++ leetcodesln/ValidMountainArray.cs
@@ -2,27 +2,38 @@
 {
     public class ValidMountainArray
     {
+
+        // one way climbing mountain solution
+        //public bool IsValidMountainArray(int[] A)
+        //{
+        //    if (A.Length < 3) return false;
+        //    if (A[0] > A[1]) return false;
+
+        //    var turningPointCount = 0;
+
+        //    for (int i = 2; i < A.Length; i++)
+        //    {
+        //        if (turningPointCount > 1) return false;
+
+        //        if (A[i] - A[i - 1] >= 0)
+        //        {
+        //            if (turningPointCount != 0) return false;
+        //        }
+        //        if (A[i] - A[i - 1] < 0 && turningPointCount == 0)
+        //        {
+        //            turningPointCount++;
+        //        }
+        //    }
+        //    return turningPointCount == 1;
+        //}
+
+        // two ways climbing mountain solution
         public bool IsValidMountainArray(int[] A)
         {
-            if (A.Length < 3) return false;
-            if (A[0] > A[1]) return false;
-
-            var turningPointCount = 0;
-
-            for (int i = 2; i < A.Length; i++)
-            {
-                if (turningPointCount > 1) return false;
-
-                if (A[i] - A[i - 1] >= 0)
-                {
-                    if (turningPointCount != 0) return false;
-                }
-                if (A[i] - A[i - 1] < 0 && turningPointCount == 0)
-                {
-                    turningPointCount++;
-                }
-            }
-            return turningPointCount == 1;
+            int n = A.Length, left = 0, right = n - 1;
+            while (left + 1 < n && A[left] < A[left + 1]) left++;
+            while (right - 1 > 0 && A[right - 1] > A[right]) right--;
+            return left > 0 && left == right && right < n - 1;
         }
     }
 }
