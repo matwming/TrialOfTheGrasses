2370bed4dbc4a328d219e6911d692e15ecd20ccb
user3301 <stan_gai@Hotmail.com>
2019-03-12 15:32:54 +1100

    can place flowers (Runtime: 112 ms, faster than 89.47%)

---
+++ leetcodesln/CanPlaceFlowers.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcodesln
+{
+    public class CanPlaceFlowers
+    {
+        public bool CanPlaceFlowersSln(int[] flowerbed, int n)
+        {
+
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (n == 0) return true;
+                if (CanPlace(ref flowerbed, i)) n--;
+            }
+            return n == 0;
+        }
+
+        private bool CanPlace(ref int[] flowerbed, int pos)
+        {
+            if (flowerbed[pos] == 0)
+            {
+                if (pos == 0)
+                {
+                    if (pos + 1 >= flowerbed.Length || flowerbed[pos + 1] == 0)
+                    {
+                        flowerbed[pos] = 1;
+                        return true;
+                    }
+                }
+                else
+                {
+                    if (flowerbed[pos - 1] == 0 && (pos + 1 >= flowerbed.Length || flowerbed[pos + 1] == 0))
+                    {
+                        flowerbed[pos] = 1;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/CanPlaceFlowersUnitTest.cs
@@ -0,0 +1,77 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class CanPlaceFlowersUnitTest
+    {
+        public CanPlaceFlowers CanPlaceFlowers { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            CanPlaceFlowers = new CanPlaceFlowers();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var flowerbed = new int[] { 1, 0, 0, 0, 1 };
+            var n = 1;
+            var expected = true;
+            var actual = CanPlaceFlowers.CanPlaceFlowersSln(flowerbed, n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var flowerbed = new int[] { 1, 0, 0, 0, 1 };
+            var n = 2;
+            var expected = false;
+            var actual = CanPlaceFlowers.CanPlaceFlowersSln(flowerbed, n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var flowerbed = new int[] { 0, 0, 1, 0, 1 };
+            var n = 1;
+            var expected = true;
+            var actual = CanPlaceFlowers.CanPlaceFlowersSln(flowerbed, n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var flowerbed = new int[] { 0, 1, 0 };
+            var n = 1;
+            var expected = false;
+            var actual = CanPlaceFlowers.CanPlaceFlowersSln(flowerbed, n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            var flowerbed = new int[] { 1, 0, 0, 0, 1, 0, 0 };
+            var n = 2;
+            var expected = true;
+            var actual = CanPlaceFlowers.CanPlaceFlowersSln(flowerbed, n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod6()
+        {
+            var flowerbed = new int[] { 0 };
+            var n = 1;
+            var expected = true;
+            var actual = CanPlaceFlowers.CanPlaceFlowersSln(flowerbed, n);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
