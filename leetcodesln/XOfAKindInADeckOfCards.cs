60c2eaaccb8412a44e7d8cd1f408290815309b8b
user3301 <stan_gai@Hotmail.com>
2019-03-12 12:25:01 +1100

    x of a kind in a deck of cards (find greatest common divisor among dictionary values, Runtime: 108 ms, faster than 100.00%)

---
+++ leetcodesln/XOfAKindInADeckOfCards.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class XOfAKindInADeckOfCards
+    {
+        public bool HasGroupSizeX(int[] deck)
+        {
+
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < deck.Length; i++)
+            {
+                if (dict.ContainsKey(deck[i])) dict[deck[i]]++;
+                else dict.Add(deck[i], 1);
+            }
+
+            var gcd = 0;
+
+            foreach (var item in dict)
+            {
+                gcd = GetGreatestCommonDivisor(item.Value, gcd);
+            }
+            return deck.Length > 1 && gcd > 1;
+        }
+
+        private int GetGreatestCommonDivisor(int a, int b)
+        {
+            return b > 0 ? GetGreatestCommonDivisor(b, a % b) : a;
+        }
+    }
+}
---
+++ leetcodesln_test/XOfAKindInADeckOfCardsUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class XOfAKindInADeckOfCardsUnitTest
+    {
+        public XOfAKindInADeckOfCards XOfAKindInADeckOfCards { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            XOfAKindInADeckOfCards = new XOfAKindInADeckOfCards();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 2, 3, 4, 4, 3, 2, 1 };
+            var expected = true;
+            var actual = XOfAKindInADeckOfCards.HasGroupSizeX(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1, 1, 1, 2, 2, 2, 3, 3 };
+            var expected = false;
+            var actual = XOfAKindInADeckOfCards.HasGroupSizeX(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 1 };
+            var expected = false;
+            var actual = XOfAKindInADeckOfCards.HasGroupSizeX(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var input = new int[] { 1, 1 };
+            var expected = true;
+            var actual = XOfAKindInADeckOfCards.HasGroupSizeX(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            var input = new int[] { 1, 1, 2, 2, 2, 2 };
+            var expected = true;
+            var actual = XOfAKindInADeckOfCards.HasGroupSizeX(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
