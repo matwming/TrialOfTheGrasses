a7e634c3ad38ed4d7516a2a89c2c452a86e9a190
user3301 <stan_gai@Hotmail.com>
2019-03-13 18:12:01 +1100

    k-diff pairs in an array (using brutal force, Runtime: 1784 ms, faster than 5.03%)

---
+++ leetcodesln/K-DiffPairsInAnArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class K_DiffPairsInAnArray
+    {
+        public int FindPairs(int[] nums, int k)
+        {
+            var count = 0;
+
+            if (k == 0)
+            {
+                var set = new HashSet<int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (set.Contains(nums[i])) continue;
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[i] == nums[j])
+                        {
+                            count++;
+                            set.Add(nums[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                var set = new HashSet<int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    set.Add(nums[i]);
+                }
+                var noDupArray = new int[set.Count];
+                set.CopyTo(noDupArray);
+
+                for (int i = 0; i < noDupArray.Length; i++)
+                {
+                    for (int j = i + 1; j < noDupArray.Length; j++)
+                    {
+                        if (Math.Abs(noDupArray[j] - noDupArray[i]) == k) count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ leetcodesln_test/K-DiffPairsInAnArrayUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class K_DiffPairsInAnArrayUnitTest
+    {
+        public K_DiffPairsInAnArray K_DiffPairsInAnArray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            K_DiffPairsInAnArray = new K_DiffPairsInAnArray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 3, 1, 3, 1, 5 };
+            var k = 2;
+            var expected = 2;
+            var actual = K_DiffPairsInAnArray.FindPairs(input, k);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5 };
+            var k = 1;
+            var expected = 4;
+            var actual = K_DiffPairsInAnArray.FindPairs(input, k);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 1, 3, 1, 5, 4 };
+            var k = 0;
+            var expected = 1;
+            var actual = K_DiffPairsInAnArray.FindPairs(input, k);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var input = new int[] { 1, 1, 1, 1, 1 };
+            var k = 0;
+            var expected = 1;
+            var actual = K_DiffPairsInAnArray.FindPairs(input, k);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
