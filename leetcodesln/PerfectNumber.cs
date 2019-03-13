8ad89d49bd61bebf67cddf99b1741b4b8ea565fc
user3301 <stan_gai@Hotmail.com>
2019-03-13 22:24:14 +1100

    perfect number (Runtime: 4396 ms, faster than 10.25%)

---
+++ leetcodesln/PerfectNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcodesln
+{
+    public class PerfectNumber
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            if (num <= 0) return false;
+
+            var count = 0;
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0) count += i;
+            }
+
+            return num == count;
+        }
+    }
+}
---
+++ leetcodesln_test/PerfectNumberUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PerfectNumberUnitTest
+    {
+        public PerfectNumber PerfectNumber { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            PerfectNumber = new PerfectNumber();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = 28;
+            var expected = true;
+            var actual = PerfectNumber.CheckPerfectNumber(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = 0;
+            var expected = false;
+            var actual = PerfectNumber.CheckPerfectNumber(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
