a46af711aa2700b787445cd7ad701c67556a9d4a
user3301 <stan_gai@Hotmail.com>
2019-03-14 05:17:28 +1100

    perfect number(better algorithm, Runtime: 36 ms, faster than 100.00%)

--- leetcodesln/PerfectNumber.cs
+++ leetcodesln/PerfectNumber.cs
@@ -2,18 +2,36 @@
 {
     public class PerfectNumber
     {
+        // public bool CheckPerfectNumber(int num)
+        // {
+        //     if (num <= 0) return false;
+
+        //     var count = 0;
+
+        //     for (int i = 1; i < num; i++)
+        //     {
+        //         if (num % i == 0) count += i;
+        //     }
+
+        //     return num == count;
+        // }
+
+        // optimized answer
         public bool CheckPerfectNumber(int num)
         {
             if (num <= 0) return false;
 
             var count = 0;
 
-            for (int i = 1; i < num; i++)
+            for (int i = 1; i * i < num; i++)
             {
-                if (num % i == 0) count += i;
+                if (num % i == 0 && i * i != num)
+                {
+                    count+=i;
+                    count += num/i;
+                }
             }
-
-            return num == count;
+            return count / 2 == num;
         }
     }
 }
