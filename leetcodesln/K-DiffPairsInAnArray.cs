47c29d09751a96aea1b168e2938cfd98373f38cb
user3301 <stan_gai@Hotmail.com>
2019-03-14 09:41:34 +1100

    k-diff pairs in an array (O(n) using dictionary, Runtime: 112 ms, faster than 100.00%)

--- leetcodesln/K-DiffPairsInAnArray.cs
+++ leetcodesln/K-DiffPairsInAnArray.cs
@@ -1,47 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace leetcodesln
 {
     public class K_DiffPairsInAnArray
     {
+        //public int FindPairs(int[] nums, int k)
+        //{
+        //    var count = 0;
+
+        //    if (k == 0)
+        //    {
+        //        var set = new HashSet<int>();
+        //        for (int i = 0; i < nums.Length; i++)
+        //        {
+        //            if (set.Contains(nums[i])) continue;
+        //            for (int j = i + 1; j < nums.Length; j++)
+        //            {
+        //                if (nums[i] == nums[j])
+        //                {
+        //                    count++;
+        //                    set.Add(nums[i]);
+        //                    break;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        var set = new HashSet<int>();
+        //        for (int i = 0; i < nums.Length; i++)
+        //        {
+        //            set.Add(nums[i]);
+        //        }
+        //        var noDupArray = new int[set.Count];
+        //        set.CopyTo(noDupArray);
+
+        //        for (int i = 0; i < noDupArray.Length; i++)
+        //        {
+        //            for (int j = i + 1; j < noDupArray.Length; j++)
+        //            {
+        //                if (Math.Abs(noDupArray[j] - noDupArray[i]) == k) count++;
+        //            }
+        //        }
+        //    }
+        //    return count;
+        //}
+
         public int FindPairs(int[] nums, int k)
         {
-            var count = 0;
+            if (nums.Length == 0 || k < 0) return 0;
 
-            if (k == 0)
+            var dict = new Dictionary<int, int>();
+            int count = 0;
+
+            foreach (var num in nums)
             {
-                var set = new HashSet<int>();
-                for (int i = 0; i < nums.Length; i++)
+                if (!dict.ContainsKey(num))
                 {
-                    if (set.Contains(nums[i])) continue;
-                    for (int j = i + 1; j < nums.Length; j++)
-                    {
-                        if (nums[i] == nums[j])
-                        {
-                            count++;
-                            set.Add(nums[i]);
-                            break;
-                        }
-                    }
+                    dict.Add(num, dict.GetValueOrDefault(num, 0) + 1);
+                }
+                else
+                {
+                    dict[num] = dict.GetValueOrDefault(num, 0) + 1;
                 }
             }
-            else
-            {
-                var set = new HashSet<int>();
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    set.Add(nums[i]);
-                }
-                var noDupArray = new int[set.Count];
-                set.CopyTo(noDupArray);
 
-                for (int i = 0; i < noDupArray.Length; i++)
+            foreach (var entry in dict)
+            {
+                if (k == 0)
                 {
-                    for (int j = i + 1; j < noDupArray.Length; j++)
-                    {
-                        if (Math.Abs(noDupArray[j] - noDupArray[i]) == k) count++;
-                    }
+                    if (entry.Value >= 2) count++;
+                }
+                else
+                {
+                    if (dict.ContainsKey(entry.Key + k)) count++;
                 }
             }
             return count;
