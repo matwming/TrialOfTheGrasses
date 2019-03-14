3c3193ef9b0f83b3914277669b8f369f24c8d3fc
user3301 <stan_gai@Hotmail.com>
2019-03-14 15:23:39 +1100

    rotate array (Runtime: 256 ms, faster than 90.96%)

---
+++ leetcodesln/RotateArray.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcodesln
+{
+    public class RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k %= nums.Length;
+            var arr2 = new int[nums.Length * 2];
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (i < nums.Length)
+                {
+                    arr2[i] = nums[i];
+                }
+                else
+                {
+                    arr2[i] = nums[i - nums.Length];
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = arr2[nums.Length - k + i];
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/RotateArrayUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RotateArrayUnitTest
+    {
+        public RotateArray RotateArray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            RotateArray = new RotateArray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            var k = 3;
+            var expected = new int[] { 5, 6, 7, 1, 2, 3, 4 };
+            RotateArray.Rotate(input, k);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.AreEqual(expected[i], input[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { -1 };
+            var k = 2;
+            var expected = new int[] { -1 };
+            RotateArray.Rotate(input, k);
+            for (int i = 0; i < input.Length; i++)
+            {
+                Assert.AreEqual(expected[i], input[i]);
+            }
+        }
+    }
+}
