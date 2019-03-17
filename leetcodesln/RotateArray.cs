ddb54625b21dc59f9ecee92040e8419405f17412
user3301 <stan_gai@Hotmail.com>
2019-03-18 09:37:48 +1100

    clean code

--- leetcodesln/RotateArray.cs
+++ leetcodesln/RotateArray.cs
@@ -5,23 +5,16 @@
         public void Rotate(int[] nums, int k)
         {
             k %= nums.Length;
+
             var arr2 = new int[nums.Length * 2];
 
             for (int i = 0; i < arr2.Length; i++)
             {
-                if (i < nums.Length)
-                {
-                    arr2[i] = nums[i];
-                }
-                else
-                {
-                    arr2[i] = nums[i - nums.Length];
-                }
+                if (i < nums.Length) arr2[i] = nums[i];
+
+                else arr2[i] = nums[i - nums.Length]; ;
             }
-            for (int i = 0; i < nums.Length; i++)
-            {
-                nums[i] = arr2[nums.Length - k + i];
-            }
+            for (int i = 0; i < nums.Length; i++) nums[i] = arr2[nums.Length - k + i];
         }
     }
 }
