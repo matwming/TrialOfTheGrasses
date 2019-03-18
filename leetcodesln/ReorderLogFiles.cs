efbc0cc4680218a66c2547f1c678c12e5eb94855
user3301 <stan_gai@Hotmail.com>
2019-03-19 10:22:10 +1100

    [ci skip] reorder log files(bubble sort, time limit exceeded)

---
+++ leetcodesln/ReorderLogFiles.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class ReorderLogFiles
+    {
+        public string[] ReorderLogFilesSln(string[] logs)
+        {
+            string[] result = new string[logs.Length];
+
+            var letterLogsList = new List<string>();
+            var digitLogsList = new List<string>();
+
+            foreach (var log in logs)
+            {
+                if (char.IsNumber(log.Split(' ')[1][0])) digitLogsList.Add(log);
+                else letterLogsList.Add(log);
+            }
+
+            string[] letterArray = letterLogsList.ToArray();
+
+            BubbleSortLetterLogs(ref letterArray);
+            for (int i = 0; i < letterArray.Length; i++)
+            {
+                result[i] = letterArray[i];
+            }
+
+            var list_ptr = 0;
+            for (int i = letterArray.Length; i < result.Length; i++)
+            {
+                result[i] = digitLogsList[list_ptr++];
+            }
+            return result;
+        }
+
+        private void BubbleSortLetterLogs(ref string[] letterArray)
+        {
+            for (int i = 0; i < letterArray.Length - 1; i++)
+            {
+                for (int j = 0; j < letterArray.Length - 1 - i; j++)
+                {
+                    if (Comp(letterArray[j], letterArray[j + 1]))
+                    {
+                        var temp = letterArray[j + 1];
+                        letterArray[j + 1] = letterArray[j];
+                        letterArray[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private bool Comp(string v1, string v2)
+        {
+            string[] v1s = v1.Split(' ');
+            string[] v2s = v2.Split(' ');
+
+            var i = 1;
+            while (i < v1s.Length && i < v2s.Length)
+            {
+                for (int j = 0; j < v1s[i].Length && i < v2s[i].Length; j++)
+                {
+                    if (v1s[i][j] > v2s[i][j]) return true;
+                    else return false;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/ReorderLogFilesUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ReorderLogFilesUnitTest
+    {
+        public ReorderLogFiles ReorderLogFiles { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ReorderLogFiles = new ReorderLogFiles();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new string[] { "a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo" };
+            var expected = new string[] { "g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7" };
+            var actual = ReorderLogFiles.ReorderLogFilesSln(input);
+
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new string[] { "j mo", "5 m w", "g 07", "o 2 0", "t q h" };
+            var expected = new string[] { "j mo", "5 m w", "t q h", "g 07", "o 2 0" };
+            var actual = ReorderLogFiles.ReorderLogFilesSln(input);
+
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
