4119ac970179dee353fe7e21e9f9a10e155402d9
user3301 <stan_gai@Hotmail.com>
2019-03-19 15:28:19 +1100

    [ci skip] add comment

--- leetcodesln/MostCommonWord.cs
+++ leetcodesln/MostCommonWord.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;  // for some reason, this statement is needed in leetcode in order to run successfully
 
 namespace leetcodesln
 {
