910e2caf5266a5b8c3130ebb7ef87769930d5871
user3301 <stan_gai@Hotmail.com>
2019-03-19 18:16:06 +1100

    count and say (recursive, Runtime: 96 ms, faster than 77.34%)

---
+++ leetcodesln/CountAndSay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class CountAndSay
+    {
+        public string CountAndSaySln(int n)
+        {
+            if (n == 1) return "1";
+
+            var second = new List<int> { 1, 1 };
+
+            return GetFinalLine(second, n - 2);
+        }
+
+        private string GetFinalLine(List<int> pre_list, int n)
+        {
+            if (n == 0) return string.Join("", pre_list);
+
+            var next_list = new List<int>();
+
+            int current = pre_list[0];
+            int count = 1;
+
+            for (int i = 1; i < pre_list.Count; i++)
+            {
+                if (pre_list[i] == current) count++;
+                else
+                {
+                    next_list.Add(count);
+                    next_list.Add(current);
+                    current = pre_list[i];
+                    count = 1;
+                }
+            }
+            next_list.Add(count);
+            next_list.Add(current);
+
+            return GetFinalLine(next_list, --n);
+        }
+    }
+}
---
+++ leetcodesln_test/CountAndSayUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class CountAndSayUnitTest
+    {
+        public CountAndSay CountAndSay { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            CountAndSay = new CountAndSay();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = 1;
+            var expected = "1";
+            var actual = CountAndSay.CountAndSaySln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = 4;
+            var expected = "1211";
+            var actual = CountAndSay.CountAndSaySln(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
