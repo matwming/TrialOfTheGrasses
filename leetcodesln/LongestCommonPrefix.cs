67d80cf80c090a0ebc3946c95409ba40f1824dd6
user3301 <stan_gai@Hotmail.com>
2019-03-20 14:48:43 +1100

    longest common prefix (Runtime: 100 ms, faster than 97.08% )

---
+++ leetcodesln/LongestCommonPrefix.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace leetcodesln
+{
+    public class LongestCommonPrefix
+    {
+        public string LongestCommonPrefixSln(string[] strs)
+        {
+            if (strs.Length == 0) return string.Empty;
+
+            var result = new StringBuilder();
+
+            string shortest = strs[0];
+
+            foreach (var str in strs)
+            {
+                shortest = str.Length < shortest.Length ? str : shortest;
+            }
+
+            bool flag = true;
+
+            for (int i = 0; i < shortest.Length && flag; i++)
+            {
+                var current = shortest[i];
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (current != strs[j][i])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) result.Append(current);
+
+
+            }
+            return result.Length == 1 ? (strs.Length == 1 ? strs[0] : result.ToString()) : result.ToString();
+        }
+    }
+}
---
+++ leetcodesln_test/LongestCommonPrefixUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LongestCommonPrefixUnitTest
+    {
+        public LongestCommonPrefix LongestCommonPrefix { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            LongestCommonPrefix = new LongestCommonPrefix();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new string[] { "flower", "flow", "flight" };
+            var expected = "fl";
+            var actual = LongestCommonPrefix.LongestCommonPrefixSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new string[] { "dog", "racecar", "car" };
+            var expected = string.Empty;
+            var actual = LongestCommonPrefix.LongestCommonPrefixSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new string[] { };
+            var expected = string.Empty;
+            var actual = LongestCommonPrefix.LongestCommonPrefixSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var input = new string[] { "a" };
+            var expected = "a";
+            var actual = LongestCommonPrefix.LongestCommonPrefixSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            var input = new string[] { "c", "c" };
+            var expected = "c";
+            var actual = LongestCommonPrefix.LongestCommonPrefixSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
