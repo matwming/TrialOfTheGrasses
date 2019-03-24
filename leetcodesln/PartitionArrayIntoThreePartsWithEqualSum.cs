c56167f9c0192c2736a0298f85ab48fb956ae424
user3301 <stan_gai@Hotmail.com>
2019-03-25 10:49:44 +1100

    partition array into three parts with equal sum(Runtime: 164 ms, faster than 100.00%)

---
+++ leetcodesln/PartitionArrayIntoThreePartsWithEqualSum.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace leetcodesln
+{
+    public class PartitionArrayIntoThreePartsWithEqualSum
+    {
+        public bool CanThreePartsEqualSum(int[] A)
+        {
+            var total = A.Sum();
+            if (total % 3 != 0) return false;
+
+            var partialSum = total / 3;
+
+            for (int i = 0, currentSum = 0, count = 0; i < A.Length; ++i)
+            {
+                currentSum += A[i];
+                if (currentSum != partialSum) continue;
+                if (++count == 3) return true;
+                currentSum = 0;
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/PartitionArrayIntoThreePartsWithEqualSumUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PartitionArrayIntoThreePartsWithEqualSumUnitTest
+    {
+        public PartitionArrayIntoThreePartsWithEqualSum PartitionArrayIntoThreePartsWithEqualSum { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            PartitionArrayIntoThreePartsWithEqualSum = new PartitionArrayIntoThreePartsWithEqualSum();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1 };
+            Assert.IsTrue(PartitionArrayIntoThreePartsWithEqualSum.CanThreePartsEqualSum(input));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1 };
+            Assert.IsFalse(PartitionArrayIntoThreePartsWithEqualSum.CanThreePartsEqualSum(input));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 3, 3, 6, 5, -2, 2, 5, 1, -9, 4 };
+            Assert.IsTrue(PartitionArrayIntoThreePartsWithEqualSum.CanThreePartsEqualSum(input));
+        }
+    }
+}
