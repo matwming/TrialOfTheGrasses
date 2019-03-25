03411a9b069582e707bf4b19f813d795e91e9046
user3301 <stan_gai@Hotmail.com>
2019-03-26 10:04:41 +1100

    DI String Match(two pointer, Runtime: 252 ms, faster than 86.46%)

---
+++ leetcodesln/DIStringMatch.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcodesln
+{
+    public class DIStringMatch
+    {
+        public int[] DiStringMatch(string S)
+        {
+            if (S.Length == 0) return null;
+
+            var result = new int[S.Length + 1];
+
+            var currentMax = S.Length;
+            var currentMin = 0;
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                result[i] = S[i] == 'I' ? currentMin++ : currentMax--;
+            }
+
+            result[S.Length] = currentMax;
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/DIStringMatchUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DIStringMatchUnitTest
+    {
+        public DIStringMatch DIStringMatch { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DIStringMatch = new DIStringMatch();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void MestMethod1()
+        {
+            var input = "IDID";
+            var expected = new int[] { 0, 4, 1, 3, 2 };
+            var actual = DIStringMatch.DiStringMatch(input);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void MestMethod2()
+        {
+            var input = "III";
+            var expected = new int[] { 0, 1, 2, 3 };
+            var actual = DIStringMatch.DiStringMatch(input);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void MestMethod3()
+        {
+            var input = "DDI";
+            var expected = new int[] { 3, 2, 0, 1 };
+            var actual = DIStringMatch.DiStringMatch(input);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
