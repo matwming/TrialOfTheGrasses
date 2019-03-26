07755d386eac55bf0da4d1a6d33f94683537518c
user3301 <stan_gai@Hotmail.com>
2019-03-27 10:46:34 +1100

    range sum query- immutable (dp solution, Runtime: 632 ms, faster than 5.49%)

---
+++ leetcodesln/RangeSumQuery-Immutable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class RangeSumQuery_Immutable
+    {
+        private int[] _nums { get; set; }
+
+        public RangeSumQuery_Immutable(int[] nums)
+        {
+            _nums = nums;
+        }
+
+        public int SumRange(int i, int j)
+        {
+            if (i == j) return _nums[i];
+
+            var dp = new int[_nums.Length + 1];
+            Array.Fill(dp, int.MinValue);
+            dp[0] = 0;
+
+            return GetZeroToIndex(ref dp, j + 1) - GetZeroToIndex(ref dp, i);
+        }
+
+        private int GetZeroToIndex(ref int[] dp, int i)
+        {
+
+            if (dp[i] != int.MinValue) return dp[i];
+            else
+            {
+                var sum = 0;
+                for (int v = 0; v < i; v++)
+                {
+                    sum += _nums[v];
+                }
+                dp[i] = sum;
+                return dp[i];
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/RangeSumQuery-ImmutableUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RangeSumQuery_ImmutableUnitTest
+    {
+        public RangeSumQuery_Immutable RangeSumQuery_Immutable { get; set; }
+
+
+        [TestMethod, Timeout(20_000)]
+        [DataRow(0, 2, 1)]
+        [DataRow(2, 5, -1)]
+        [DataRow(0, 5, -3)]
+        public void TestMethod1(int i, int j, int sum)
+        {
+            var input = new int[] { -2, 0, 3, -5, 2, -1 };
+            RangeSumQuery_Immutable = new RangeSumQuery_Immutable(input);
+            Assert.AreEqual(sum, RangeSumQuery_Immutable.SumRange(i, j));
+
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { -1 };
+            RangeSumQuery_Immutable = new RangeSumQuery_Immutable(input);
+            var expected = -1;
+            var actual = RangeSumQuery_Immutable.SumRange(0, 0);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        [DataRow(0, 2, -1)]
+        [DataRow(1, 2, -2)]
+        [DataRow(0, 1, 5)]
+        public void TestMethod3(int i, int j, int sum)
+        {
+            var input = new int[] { 1, 4, -6 };
+            RangeSumQuery_Immutable = new RangeSumQuery_Immutable(input);
+            Assert.AreEqual(sum, RangeSumQuery_Immutable.SumRange(i, j));
+
+        }
+
+        [TestCleanup]
+        public void AfterEach()
+        {
+            RangeSumQuery_Immutable = null;
+        }
+    }
+}
