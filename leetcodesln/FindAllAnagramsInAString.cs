da4ce56b9da9843399e7c35387cc86e6091b36bf
user3301 <stan_gai@Hotmail.com>
2019-03-27 23:18:41 +1100

    :boom: find all anagrams in a string(using sliding window, Runtime: 244 ms, faster than 99.16%)

---
+++ leetcodesln/FindAllAnagramsInAString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcodesln
+{
+    public class FindAllAnagramsInAString
+    {
+        public IList<int> FindAnagrams(string s, string p)
+        {
+            var resList = new List<int>();
+
+            int[] hash = new int[256];
+            foreach (var chr in p)
+            {
+                hash[chr]++;
+            }
+
+            int left = 0, right = 0, count = p.Length;
+
+
+            while (right < s.Length)
+            {
+                if (hash[s[right++]]-- >= 1) count--;
+
+                if (count == 0) resList.Add(left);
+
+                if (right - left == p.Length && hash[s[left++]]++ >= 0) count++;
+            }
+
+            return resList;
+        }
+    }
+}
