90226fae03c419de4980eb7d4b2389ae878f5d9a
user3301 <stan_gai@Hotmail.com>
2019-03-27 16:16:32 +1100

    N-repected element in size 2N array(hashtable, Runtime: 124 ms, faster than 98.25% )

---
+++ leetcodesln/N-RepeatedElementInSize2NArray.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class N_RepeatedElementInSize2NArray
+    {
+        public int RepeatedNTimes(int[] A)
+        {
+            var N = A.Length / 2;
+
+            var dict = new Dictionary<int, int>();
+
+            foreach (var num in A)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    ++dict[num];
+                    if (dict[num] > 1) return num;
+
+                }
+                else dict.Add(num, 1);
+            }
+            throw null;
+        }
+    }
+}
---
+++ leetcodesln_test/N-RepeatedElementInSize2NArrayUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class N_RepeatedElementInSize2NArrayUnitTest
+    {
+        public N_RepeatedElementInSize2NArray N_RepeatedElementInSize2NArray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            N_RepeatedElementInSize2NArray = new N_RepeatedElementInSize2NArray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 2, 3, 3 };
+            var expected = 3;
+            var actual = N_RepeatedElementInSize2NArray.RepeatedNTimes(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 2, 1, 2, 5, 3, 2 };
+            var expected = 2;
+            var actual = N_RepeatedElementInSize2NArray.RepeatedNTimes(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 5, 1, 5, 2, 5, 3, 5, 4 };
+            var expected = 5;
+            var actual = N_RepeatedElementInSize2NArray.RepeatedNTimes(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
