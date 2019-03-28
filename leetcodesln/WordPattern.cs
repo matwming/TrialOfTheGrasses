e3de69e7dceb8e8d9afa5381c9c4fa316079668b
user3301 <stan_gai@Hotmail.com>
2019-03-28 22:27:18 +1100

    :boom: word pattern (using hashtable, Runtime: 76 ms, faster than 100.00%)

---
+++ leetcodesln/WordPattern.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class WordPattern
+    {
+        public bool WordPatternSln(string pattern, string str)
+        {
+            char[] chrs = pattern.ToCharArray();
+            string[] words = str.Trim().Split(" ");
+
+            if (chrs.Length != words.Length) return false;
+
+            var dict = new Dictionary<string, char>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (dict.ContainsKey(words[i]))
+                {
+                    if (dict[words[i]] != chrs[i]) return false;
+                }
+                else if (dict.ContainsValue(chrs[i])) return false;
+                else dict.Add(words[i], chrs[i]);
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/WordPatternUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class WordPatternUnitTest
+    {
+        public WordPattern WordPattern { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            WordPattern = new WordPattern();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var pattern = "abba";
+            var str = "dog cat cat dog";
+            Assert.IsTrue(WordPattern.WordPatternSln(pattern, str));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var pattern = "abba";
+            var str = "dog cat cat fish";
+            Assert.IsFalse(WordPattern.WordPatternSln(pattern, str));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var pattern = "aaaa";
+            var str = "dog cat cat dog";
+            Assert.IsFalse(WordPattern.WordPatternSln(pattern, str));
+        }
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var pattern = "abba";
+            var str = "dog dog dog dog";
+            Assert.IsFalse(WordPattern.WordPatternSln(pattern, str));
+        }
+    }
+}
