159b0ab6a4acb561e0de82cce05b13e26656953c
user3301 <stan_gai@Hotmail.com>
2019-03-28 15:32:55 +1100

    verifying an alien dictionary (using hashtable, Runtime: 92 ms, faster than 84.02%)

---
+++ leetcodesln/VerifyingAnAlienDictionary.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcodesln
+{
+    public class VerifyingAnAlienDictionary
+    {
+        public bool IsAlienSorted(string[] words, string order)
+        {
+            var dict = new int[256];
+            for (int i = 0; i < order.Length; i++)
+            {
+                dict[order[i]] = i;
+            }
+
+            for (int i = words.Length - 1; i > 0; i--)
+            {
+                if (CompareTwo(words[i], words[i - 1], ref dict)) continue;
+                else return false;
+            }
+            return true;
+        }
+
+        private bool CompareTwo(string right, string left, ref int[] dict)
+        {
+
+            for (int i = 0; i <= (left.Length >= right.Length ? left.Length - 1 : right.Length - 1); i++)
+            {
+                if (i >= left.Length) return true;
+                if (i < right.Length && i < left.Length && dict[right[i]] > dict[left[i]]) return true;
+                else if (i < right.Length && i < left.Length && dict[right[i]] == dict[left[i]])
+                {
+                    continue;
+                }
+                else return false;
+            }
+            throw null;
+        }
+    }
+}
---
+++ leetcodesln_test/VerifyingAnAlienDictionaryUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class VerifyingAnAlienDictionaryUnitTest
+    {
+        public VerifyingAnAlienDictionary VerifyingAnAlienDictionary { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            VerifyingAnAlienDictionary = new VerifyingAnAlienDictionary();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var words = new string[] { "hello", "leetcode" };
+            var order = "hlabcdefgijkmnopqrstuvwxyz";
+            var actual = VerifyingAnAlienDictionary.IsAlienSorted(words, order);
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var words = new string[] { "word", "world", "row" };
+            var order = "worldabcefghijkmnpqstuvxyz";
+            var actual = VerifyingAnAlienDictionary.IsAlienSorted(words, order);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var words = new string[] { "apple", "app" };
+            var order = "abcdefghijklmnopqrstuvwxyz";
+            var actual = VerifyingAnAlienDictionary.IsAlienSorted(words, order);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var words = new string[] { "l", "h" };
+            var order = "xkbwnqozvterhpjifgualycmds";
+            var actual = VerifyingAnAlienDictionary.IsAlienSorted(words, order);
+            Assert.IsFalse(actual);
+        }
+    }
+}
