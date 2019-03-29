bcf088e4496a3b5627a2a3d1217a0d2ae3bd6dfc
user3301 <stan_gai@Hotmail.com>
2019-03-29 11:42:02 +1100

    binary search(Runtime: 132 ms, faster than 98.11%)

---
+++ leetcodesln/BinarySearch.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcodesln
+{
+    public class BinarySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+            int high = nums.Length - 1;
+            int low = 0;
+
+            while (low <= high)
+            {
+                var mid = (high + low) / 2;
+                if (nums[mid] == target) return mid;
+                else if (nums[mid] > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ leetcodesln_test/BinarySearchUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+[Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+public class BinarySearchUnitTest
+{
+    public BinarySearch BinarySearch { get; set; }
+
+    [TestInitialize]
+    public void BeforeEach()
+    {
+        BinarySearch = new BinarySearch();
+    }
+
+    [TestMethod, Timeout(5_000)]
+    public void TestMethod1()
+    {
+        var nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        var target = 9;
+        var expected = 4;
+        Assert.AreEqual(expected, BinarySearch.Search(nums, target));
+    }
+
+    [TestMethod, Timeout(5_000)]
+    public void TestMethod2()
+    {
+        var nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        var target = 2;
+        var expected = -1;
+        Assert.AreEqual(expected, BinarySearch.Search(nums, target));
+    }
+
+    [TestMethod, Timeout(5_000)]
+    public void TestMethod3()
+    {
+        var nums = new int[] { 5 };
+        var target = 5;
+        var expected = 0;
+        Assert.AreEqual(expected, BinarySearch.Search(nums, target));
+    }
+
+}
