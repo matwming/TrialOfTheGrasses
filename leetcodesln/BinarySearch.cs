71d0365aadda33623e0a8aeb6efda19724ecf29b
user3301 <stan_gai@Hotmail.com>
2019-03-29 11:44:15 +1100

    clean code

--- leetcodesln/BinarySearch.cs
+++ leetcodesln/BinarySearch.cs
@@ -11,14 +11,8 @@
             {
                 var mid = (high + low) / 2;
                 if (nums[mid] == target) return mid;
-                else if (nums[mid] > target)
-                {
-                    high = mid - 1;
-                }
-                else
-                {
-                    low = mid + 1;
-                }
+                else if (nums[mid] > target) high = mid - 1;
+                else low = mid + 1;
             }
             return -1;
         }
