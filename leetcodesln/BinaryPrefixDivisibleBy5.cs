eae34aec4ad9f7579cf3e14290769fbc59e66a2e
user3301 <stan_gai@Hotmail.com>
2019-03-31 16:58:16 +1100

    binary prefix divisible by 5 (:boom:bit shifting, Runtime: 260 ms, faster than 100.00%)

---
+++ leetcodesln/BinaryPrefixDivisibleBy5.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class BinaryPrefixDivisibleBy5
+    {
+
+        // this method will result in Runtime Error (System.OverflowException: Value was either too large or too small for a UInt64.)
+        //public IList<bool> PrefixesDivBy5(int[] A)
+        //{
+        //    var result = new List<bool>();
+
+        //    for (int i = 1; i <= A.Length; i++)
+        //    {
+        //        var sb = new StringBuilder();
+        //        for (int j = 0; j < i; j++)
+        //        {
+        //            sb.Append(A[j]);
+        //        }
+        //        var inte = Convert.ToDecimal(sb.ToString()); // overflow
+        //        result.Add(inte % 5 == 0);
+        //    }
+
+        //    return result;
+        //}
+
+        public IList<bool> PrefixesDivBy5(int[] A)
+        {
+            var sum = 0;
+            var result = new List<bool>();
+
+            foreach (var bit in A)
+            {
+                sum <<= 1;
+                sum += bit;
+                result.Add(sum % 5 == 0);
+                sum %= 5;  //prevent overflow
+            }
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/BinaryPrefixDivisibleBy5UnitTest.cs
@@ -0,0 +1,114 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class BinaryPrefixDivisibleBy5UnitTest
+    {
+        public BinaryPrefixDivisibleBy5 BinaryPrefixDivisibleBy5 { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            BinaryPrefixDivisibleBy5 = new BinaryPrefixDivisibleBy5();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 0, 1, 1 };
+            var expected = new bool[] { true, false, false };
+            var actual = BinaryPrefixDivisibleBy5.PrefixesDivBy5(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1, 1, 1 };
+            var expected = new bool[] { false, false, false };
+            var actual = BinaryPrefixDivisibleBy5.PrefixesDivBy5(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 0, 1, 1, 1, 1, 1 };
+            var expected = new bool[] { true, false, false, false, true, false };
+            var actual = BinaryPrefixDivisibleBy5.PrefixesDivBy5(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var input = new int[] { 1, 1, 1, 0, 1 };
+            var expected = new bool[] { false, false, false, false, false };
+            var actual = BinaryPrefixDivisibleBy5.PrefixesDivBy5(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            var input = new int[] { 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1 };
+            var expected = new bool[] { false, false, false, false, false };
+            var actual = BinaryPrefixDivisibleBy5.PrefixesDivBy5(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod6()
+        {
+            var input = new int[] { 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1 };
+            var expected = new bool[] { false, false, false, false, false };
+            var actual = BinaryPrefixDivisibleBy5.PrefixesDivBy5(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        //[TestMethod, Timeout(20_000)]
+        //public void TestMethod7()
+        //{
+        //    var input = new int[] { 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0 };
+        //    var expected = new bool[] { false, false, false, false, false };
+        //    var actual = BinaryPrefixDivisibleBy5.PrefixesDivBy5(input);
+        //    for (int i = 0; i < expected.Length; i++)
+        //    {
+        //        Assert.AreEqual(expected[i], actual[i]);
+        //    }
+        //}
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod8()
+        {
+            var input = new int[] { 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1 };
+            var expected = new bool[] { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, true, true, true, true, false };
+            var actual = BinaryPrefixDivisibleBy5.PrefixesDivBy5(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
