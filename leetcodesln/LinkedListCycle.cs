328bab35cffda512542786dc273f1409b9226609
user3301 <stan_gai@Hotmail.com>
2019-03-31 14:34:31 +1100

    :boom:linked list cycle(using two pointer, fast and slow, Runtime: 96 ms, faster than 97.65%)

---
+++ leetcodesln/LinkedListCycle.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcodesln
+{
+    public class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+
+            var node_fast = head;
+            var node_slow = head;
+
+            while (node_fast.next != null && node_slow.next != null)
+            {
+                if (node_fast.next.next == null) return false;
+                node_fast = node_fast.next.next;
+                node_slow = node_slow.next;
+                if (node_fast == node_slow) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/LinkedListCycleUnitTest.cs
@@ -0,0 +1,65 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class LinkedListCycleUnitTest
+    {
+        public LinkedListCycle LinkedListCycle { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            LinkedListCycle = new LinkedListCycle();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var head = new ListNode(3);
+            var node1 = new ListNode(2);
+            var node2 = new ListNode(0);
+            var node3 = new ListNode(-4);
+            head.next = node1;
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = node1;
+            var expected = true;
+            var actual = LinkedListCycle.HasCycle(head);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var head = new ListNode(1);
+            var node1 = new ListNode(2);
+            head.next = node1;
+            node1.next = head;
+            var expected = true;
+            var actual = LinkedListCycle.HasCycle(head);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var head = new ListNode(1);
+            var expected = false;
+            var actual = LinkedListCycle.HasCycle(head);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var head = new ListNode(1);
+            var node1 = new ListNode(2);
+            head.next = node1;
+            var expected = false;
+            var actual = LinkedListCycle.HasCycle(head);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
