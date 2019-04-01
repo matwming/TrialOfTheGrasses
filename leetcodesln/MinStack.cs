5f1859d668cd5be62d9a418251acf5c0351a5e57
user3301 <stan_gai@Hotmail.com>
2019-04-01 21:25:35 +1100

    min stack (using stack and list, Runtime: 288 ms, faster than 18.16% )

---
+++ leetcodesln/MinStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcodesln
+{
+    public class MinStack
+    {
+        private Stack<int> _stack;
+        private List<int> _minList;
+
+        // ** Initialize your data structure here. */
+        public MinStack()
+        {
+            _stack = new Stack<int>();
+            _minList = new List<int>();
+        }
+
+        public void Push(int x)
+        {
+            _stack.Push(x);
+
+            _minList.Add(x);
+
+            for (int i = _minList.Count - 1; i > 0; i--)
+            {
+                if (_minList[i] < _minList[i - 1])
+                {
+                    var temp = _minList[i - 1];
+                    _minList[i - 1] = _minList[i];
+                    _minList[i] = temp;
+                }
+            }
+        }
+
+        public void Pop()
+        {
+            if (!_stack.TryPop(out int res)) throw new Exception("the stack is empty");
+
+            _minList.Remove(res);
+        }
+
+        public int Top()
+        {
+            return _stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return _minList.FirstOrDefault();
+        }
+    }
+}
---
+++ leetcodesln_test/MinStackUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MinStackUnitTest
+    {
+        public MinStack MinStack { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MinStack = new MinStack();
+        }
+
+        [TestCleanup]
+        public void AfterEach()
+        {
+            MinStack = null;
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            MinStack.Push(-2);
+            MinStack.Push(0);
+            MinStack.Push(-3);
+            Assert.AreEqual(-3, MinStack.GetMin());
+            MinStack.Pop();
+            Assert.AreEqual(0, MinStack.Top());
+            Assert.AreEqual(-2, MinStack.GetMin());
+        }
+
+    }
+}
