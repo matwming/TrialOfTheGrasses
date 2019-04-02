bd36edc54592733f0bf0b05a1ca9fca5483e7635
user3301 <stan_gai@Hotmail.com>
2019-04-02 21:43:28 +1100

    min stack(using only one stack, Runtime: 136 ms, faster than 94.76% )

--- leetcodesln/MinStack.cs
+++ leetcodesln/MinStack.cs
@@ -1,53 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace leetcodesln
 {
     public class MinStack
     {
-        private Stack<int> _stack;
-        private List<int> _minList;
+        //private Stack<int> _stack;
+        //private List<int> _minList;
 
-        // ** Initialize your data structure here. */
+        //// ** Initialize your data structure here. */
+        //public MinStack()
+        //{
+        //    _stack = new Stack<int>();
+        //    _minList = new List<int>();
+        //}
+
+        //public void Push(int x)
+        //{
+        //    _stack.Push(x);
+
+        //    _minList.Add(x);
+
+        //    for (int i = _minList.Count - 1; i > 0; i--)
+        //    {
+        //        if (_minList[i] < _minList[i - 1])
+        //        {
+        //            var temp = _minList[i - 1];
+        //            _minList[i - 1] = _minList[i];
+        //            _minList[i] = temp;
+        //        }
+        //    }
+        //}
+
+        //public void Pop()
+        //{
+        //    if (!_stack.TryPop(out int res)) throw new Exception("the stack is empty");
+
+        //    _minList.Remove(res);
+        //}
+
+        //public int Top()
+        //{
+        //    return _stack.Peek();
+        //}
+
+        //public int GetMin()
+        //{
+        //    return _minList.FirstOrDefault();
+        //}
+
+
+
+        /** one stack solution */
+
+        private Stack<StackNode> stack;
+        private int? CurrentMinimumValue { get; set; }
+
         public MinStack()
         {
-            _stack = new Stack<int>();
-            _minList = new List<int>();
+            stack = new Stack<StackNode>();
         }
 
         public void Push(int x)
         {
-            _stack.Push(x);
-
-            _minList.Add(x);
-
-            for (int i = _minList.Count - 1; i > 0; i--)
+            if (x < CurrentMinimumValue || CurrentMinimumValue == null)
             {
-                if (_minList[i] < _minList[i - 1])
-                {
-                    var temp = _minList[i - 1];
-                    _minList[i - 1] = _minList[i];
-                    _minList[i] = temp;
-                }
+                CurrentMinimumValue = x;
+                stack.Push(new StackNode(x, CurrentMinimumValue.GetValueOrDefault()));
+            }
+            else
+            {
+                stack.Push(new StackNode(x, CurrentMinimumValue.GetValueOrDefault()));
             }
         }
 
         public void Pop()
         {
-            if (!_stack.TryPop(out int res)) throw new Exception("the stack is empty");
+            if (stack.Count == 0) return;
+            stack.Pop();
+            if (stack.Count == 0)
+            {
+                CurrentMinimumValue = null;
+                return;
+            }
+            CurrentMinimumValue = stack.Peek().Min;
 
-            _minList.Remove(res);
         }
 
         public int Top()
         {
-            return _stack.Peek();
+            return stack.Peek().Val;
         }
 
         public int GetMin()
         {
-            return _minList.FirstOrDefault();
+            return stack.Peek().Min;
+        }
+    }
+
+    class StackNode
+    {
+        public int Val { get; }
+        public int Min { get; }
+        public StackNode(int value, int min)
+        {
+            Val = value;
+            Min = min;
         }
     }
 }
--- leetcodesln_test/MinStackUnitTest.cs
+++ leetcodesln_test/MinStackUnitTest.cs
@@ -32,5 +32,52 @@ namespace leetcodesln_test
             Assert.AreEqual(-2, MinStack.GetMin());
         }
 
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            MinStack.Push(2147483646);
+            MinStack.Push(2147483646);
+            MinStack.Push(2147483647);
+            Assert.AreEqual(2147483647, MinStack.Top());
+            MinStack.Pop();
+            Assert.AreEqual(2147483646, MinStack.GetMin());
+            MinStack.Pop();
+            Assert.AreEqual(2147483646, MinStack.GetMin());
+            MinStack.Pop();
+            MinStack.Pop();
+            MinStack.Push(2147483647);
+            Assert.AreEqual(2147483647, MinStack.Top());
+            Assert.AreEqual(2147483647, MinStack.GetMin());
+            MinStack.Push(-2147483648);
+            Assert.AreEqual(-2147483648, MinStack.Top());
+            Assert.AreEqual(-2147483648, MinStack.GetMin());
+            MinStack.Pop();
+            Assert.AreEqual(2147483647, MinStack.GetMin());
+
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            MinStack.Push(-10);
+            MinStack.Push(14);
+            Assert.AreEqual(-10, MinStack.GetMin());
+            Assert.AreEqual(-10, MinStack.GetMin());
+
+            MinStack.Push(-20);
+            Assert.AreEqual(-20, MinStack.GetMin());
+            Assert.AreEqual(-20, MinStack.GetMin());
+            Assert.AreEqual(-20, MinStack.Top());
+            Assert.AreEqual(-20, MinStack.GetMin());
+            MinStack.Pop();
+            MinStack.Push(10);
+            MinStack.Push(-7);
+            Assert.AreEqual(-10, MinStack.GetMin());
+            MinStack.Push(-7);
+            MinStack.Pop();
+            Assert.AreEqual(-7, MinStack.Top());
+            Assert.AreEqual(-10, MinStack.GetMin());
+            MinStack.Pop();
+        }
     }
 }
