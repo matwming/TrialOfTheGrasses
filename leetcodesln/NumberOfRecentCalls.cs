dd059a11d5171c0a9423aef261db97cf0a1b7ad9
user3301 <stan_gai@Hotmail.com>
2019-04-04 21:06:04 +1100

    number of recent calls(using queue, Runtime: 364 ms, faster than 72.93% )

---
+++ leetcodesln/NumberOfRecentCalls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class NumberOfRecentCalls
+    {
+        private Queue<int> Calls { get; set; }
+
+        public NumberOfRecentCalls()
+        {
+            Calls = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            Calls.Enqueue(t);
+            while (Calls.Peek() < (t - 3000))
+            {
+                Calls.Dequeue();
+            }
+            return Calls.Count;
+        }
+    }
+}
---
+++ leetcodesln_test/NumberOfRecentCallsUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class NumberOfRecentCallsUnitTest
+    {
+        public NumberOfRecentCalls NumberOfRecentCalls { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            NumberOfRecentCalls = new NumberOfRecentCalls();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, NumberOfRecentCalls.Ping(1));
+            Assert.AreEqual(2, NumberOfRecentCalls.Ping(100));
+            Assert.AreEqual(3, NumberOfRecentCalls.Ping(3001));
+            Assert.AreEqual(3, NumberOfRecentCalls.Ping(3002));
+        }
+
+    }
+}
