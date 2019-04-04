254dd8aefeb55f6045a00d995d22825b617f6634
user3301 <stan_gai@Hotmail.com>
2019-04-04 23:58:49 +1100

    :sparkles::sparkles::sparkles::sparkles::sparkles: Remove linked list elements (:boom: unit test private method using reflection, Runtime: 100 ms, faster than 100.00%)

---
+++ leetcodesln/RemoveLinkedListElements.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcodesln
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+    public class RemoveLinkedListElements
+    {
+        private ListNode RemoveElements(ListNode head, int val)
+        {
+            var virtualStartNode = new ListNode(0)
+            {
+                next = head
+            };
+
+            var nxt = virtualStartNode.next;
+            var cur = virtualStartNode;
+            while (nxt != null)
+            {
+                if (nxt.val == val)
+                {
+                    cur.next = nxt.next == null ? null : nxt.next;
+                    nxt = nxt.next;
+                }
+                else
+                {
+                    cur = cur.next;
+                    nxt = nxt.next;
+                }
+            }
+            return virtualStartNode.next;
+        }
+    }
+}
---
+++ leetcodesln_test/RemoveLinkedListElementsUnitTest.cs
@@ -0,0 +1,72 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RemoveLinkedListElementsUnitTest
+    {
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            Type type = typeof(RemoveLinkedListElements);
+            var removeLinkedListElements = Activator.CreateInstance(type);
+
+            MethodInfo method = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(x => x.Name == "RemoveElements" && x.IsPrivate)
+                .First();
+
+            var head = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(6)
+                    {
+                        next = new ListNode(3)
+                        {
+                            next = new ListNode(4)
+                            {
+                                next = new ListNode(5)
+                                {
+                                    next = new ListNode(6)
+                                    {
+                                        next = null
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            var val = 6;
+            var expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            var actual = (ListNode)method.Invoke(removeLinkedListElements,new object[] { head, val});
+
+            while (expected.next!=null)
+            {
+                Assert.AreEqual(expected.val, actual.val);
+                expected = expected.next;
+                actual = actual.next;
+            }
+        }
+
+    }
+}
