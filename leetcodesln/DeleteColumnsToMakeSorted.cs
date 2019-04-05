cc4f4c1a7dc9d6c8c1ef0ce4b1573ee2b0f86979
user3301 <stan_gai@Hotmail.com>
2019-04-05 14:50:09 +1100

    delete columns to mark sorted(Runtime: 104 ms, faster than 97.53%)

---
+++ leetcodesln/DeleteColumnsToMakeSorted.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcodesln
+{
+    public class DeleteColumnsToMakeSorted
+    {
+        public int MinDeletionSize(string[] A)
+        {
+            var non_Satified = 0;
+
+            for (int i = 0; i < A[0].Length; i++)
+            {
+                var j = 0;
+                while (j < A.Length - 1)
+                {
+                    if (A[j][i] > A[j + 1][i])
+                    {
+                        ++non_Satified;
+                        break;
+                    }
+                    ++j;
+                }
+            }
+            return non_Satified;
+        }
+    }
+}
---
+++ leetcodesln_test/DeleteColumnsToMakeSortedUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DeleteColumnsToMakeSortedUnitTest
+    {
+        public DeleteColumnsToMakeSorted DeleteColumnsToMakeSorted { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DeleteColumnsToMakeSorted = new DeleteColumnsToMakeSorted();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new string[] { "cba", "daf", "ghi" };
+            var expected = 1;
+            var actual = DeleteColumnsToMakeSorted.MinDeletionSize(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new string[] { "a", "b" };
+            var expected = 0;
+            var actual = DeleteColumnsToMakeSorted.MinDeletionSize(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new string[] { "zyx", "wvu", "tsr"};
+            var expected = 3;
+            var actual = DeleteColumnsToMakeSorted.MinDeletionSize(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
