6ee8af7f61d9bae646085da62d7b8e2206020931
user3301 <stan_gai@Hotmail.com>
2019-04-07 12:49:46 +1000

    remove outermost parentheses (weekly contest 131 question 1)

---
+++ leetcodesln/RemoveOutermostParentheses.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace leetcodesln
+{
+    public class RemoveOutermostParentheses
+    {
+        public string RemoveOutermostParenthesesSln(string S)
+        {
+            var sb = new StringBuilder();
+
+            var currentOuterOpenParenthesesPosition = 0;
+            var charSum = 0;
+
+            foreach (var chr in S)
+            {
+                if (chr == '(')
+                {
+                    charSum += chr;
+                    sb.Append(chr);
+                }
+                else
+                {
+                    charSum -= chr - 1;
+                    sb.Append(chr);
+                }
+                if (charSum == 0)
+                {
+                    sb.Remove(currentOuterOpenParenthesesPosition, 1);
+                    sb.Remove(sb.Length - 1, 1);
+                    currentOuterOpenParenthesesPosition = sb.Length;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ leetcodesln_test/RemoveOutermostParenthesesUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RemoveOutermostParenthesesUnitTest
+    {
+        public RemoveOutermostParentheses RemoveOutermostParentheses { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            RemoveOutermostParentheses = new RemoveOutermostParentheses();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = "(()())(())";
+            var expected = "()()()";
+            var actual = RemoveOutermostParentheses.RemoveOutermostParenthesesSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = "(()())(())(()(()))";
+            var expected = "()()()()(())";
+            var actual = RemoveOutermostParentheses.RemoveOutermostParenthesesSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = "()()";
+            var expected = "";
+            var actual = RemoveOutermostParentheses.RemoveOutermostParenthesesSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
