c3fdc26e8556c7a980dab8eee26b2c73f5e6f5d4
user3301 <stan_gai@Hotmail.com>
2019-04-08 23:10:53 +1000

    sum of even numbers after queries (one pass, Runtime: 416 ms, faster than 99.74%)

--- leetcodesln/SumOfEvenNumbersAfterQueries.cs
+++ leetcodesln/SumOfEvenNumbersAfterQueries.cs
@@ -4,20 +4,45 @@ namespace leetcodesln
     {
         public int[] SumEvenAfterQueries(int[] A, int[][] queries)
         {
-            var result = new int[A.Length];
-            
-            for (int i = 0; i < queries.Length; i++)
+            var res = new int[A.Length];
+            var sum = 0;
+            var i = 0;
+            foreach (var num in A)
             {
-                var sumEven = 0;
-                A[queries[i][1]] += queries[i][0];
-                foreach (var num in A)
-                {
-                    if(num % 2 ==0) sumEven += num;
-                }
-                result[i] = sumEven;
+                if (num % 2 == 0) sum += num;
             }
 
-            return result;
+            foreach (var query in queries)
+            {
+                if ((A[query[1]] + query[0]) % 2 == 0)
+                {
+                    if (A[query[1]] % 2 == 0)
+                    {
+                        sum = sum + query[0];
+                        res[i++] = sum;
+                    }
+                    else
+                    {
+                        sum = sum + A[query[1]] + query[0];
+                        res[i++] = sum;
+                    }
+                }
+                else
+                {
+                    if (A[query[1]] % 2 == 0)
+                    {
+                        sum = sum - A[query[1]];
+                        res[i++] = sum;
+                    }
+                    else
+                    {
+                        res[i++] = sum;
+                    }
+                }
+                A[query[1]] += query[0];
+
+            }
+            return res;
         }
     }
 }
