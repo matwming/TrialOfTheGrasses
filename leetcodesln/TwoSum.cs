dd553623f85a237a9207055619a5832d1b223bb4
user3301 <stan_gai@Hotmail.com>
2019-04-08 11:11:02 +1000

    two sum(using hashmap one pass, Runtime: 248 ms, faster than 98.91%)

---
+++ leetcodesln/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class TwoSum
+    {
+        /// <summary>
+        /// This question we can use linear scan approach with two pointers, the time complexity will be O(N^2),
+        /// we can also use a hashmap and find if the current value's complement exists in the array, the time complexity will be O(N)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSumSln(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>(); // to store value-index pair
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                var complement = target - nums[i];
+
+                if (dict.ContainsKey(complement)) return new int[] { i, dict[complement] };
+                dict[nums[i]] = i;
+            }
+
+            throw new KeyNotFoundException();
+        }
+    }
+}
---
+++ leetcodesln_test/TwoSumUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class TwoSumUnitTest
+    {
+        public TwoSum TwoSum { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            TwoSum = new TwoSum();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var expected = new int[] { 1, 0 };
+            var actual = TwoSum.TwoSumSln(input, target);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] {3,3};
+            var target = 6;
+            var expected = new int[] { 1,0 };
+            var actual = TwoSum.TwoSumSln(input, target);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+    }
+}
