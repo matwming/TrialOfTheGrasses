e4c5546fc24f86da9ded8879e44d4ec8846e50eb
user3301 <stan_gai@Hotmail.com>
2019-04-09 12:53:44 +1000

    max consecutive ones (Runtime: 160 ms, faster than 45.42%)

---
+++ leetcodesln/MaxConsecutiveOnes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class MaxConsecutiveOnes
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int ans = 0;
+            int current = 0;
+
+            foreach (var num in nums)
+            {
+                current = num == 0 ? 0 : ++current;
+                ans = Math.Max(ans, current);
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/MaxConsecutiveOnesUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MaxConsecutiveOnesUnitTest
+    {
+        public MaxConsecutiveOnes MaxConsecutiveOnes { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MaxConsecutiveOnes = new MaxConsecutiveOnes();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 1, 0, 1, 1, 1 };
+            var expected = 3;
+            var actual = MaxConsecutiveOnes.FindMaxConsecutiveOnes(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
