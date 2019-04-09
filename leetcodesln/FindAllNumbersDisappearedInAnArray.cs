4de42bfb4eb6664f8f49c1e050dbcc6a84759c4b
user3301 <stan_gai@Hotmail.com>
2019-04-09 22:33:34 +1000

    :boom: find all numbers disappeared in an array(Runtime: 312 ms, faster than 94.03% )

---
+++ leetcodesln/FindAllNumbersDisappearedInAnArray.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class FindAllNumbersDisappearedInAnArray
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            var ans = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++) nums[(nums[i] - 1) % nums.Length] += nums.Length;
+            for (int i = 0; i < nums.Length; i++) if (nums[i] <= nums.Length) ans.Add(i + 1);
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/FindAllNumbersDisappearedInAnArrayUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetcodesln;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class FindAllNumbersDisappearedInAnArrayUnitTest
+    {
+        public FindAllNumbersDisappearedInAnArray FindAllNumbersDisappearedInAnArray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            FindAllNumbersDisappearedInAnArray = new FindAllNumbersDisappearedInAnArray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            var expected = new List<int> { 5, 6 };
+            var actual = FindAllNumbersDisappearedInAnArray.FindDisappearedNumbers(input);
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+    }
+}
