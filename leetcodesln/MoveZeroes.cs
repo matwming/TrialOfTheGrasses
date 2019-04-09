bdeaffd8c908f3248a203ce23a4444352585119e
user3301 <stan_gai@Hotmail.com>
2019-04-09 17:27:50 +1000

    move zeroes (in-place, Runtime: 256 ms, faster than 67.53%)

---
+++ leetcodesln/MoveZeroes.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcodesln
+{
+    public class MoveZeroes
+    {
+        public void MoveZeroesSln(int[] nums)
+        {
+            int counter = 0;
+            for (int i = 0; i < nums.Length - counter;)
+            {
+                if (nums[i] == 0)
+                {
+                    for (int j = i + 1; j < nums.Length - counter; j++)
+                    {
+                        nums[j - 1] = nums[j];
+                    }
+                    nums[nums.Length - counter - 1] = 0;
+                    counter++;
+
+                }
+                else ++i;
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/MoveZeroesUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MoveZeroesUnitTest
+    {
+        public MoveZeroes MoveZeroes { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MoveZeroes = new MoveZeroes();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 0, 1, 0, 3, 12 };
+            var expected = new int[] { 1, 3, 12, 0, 0 };
+            MoveZeroes.MoveZeroesSln(input);
+            Assert.IsTrue(Enumerable.SequenceEqual<int>(expected, input));
+
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1 };
+            var expected = new int[] { 1 };
+            MoveZeroes.MoveZeroesSln(input);
+            Assert.IsTrue(Enumerable.SequenceEqual<int>(expected, input));
+
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 0, 0, 1 };
+            var expected = new int[] { 1, 0, 0 };
+            MoveZeroes.MoveZeroesSln(input);
+            Assert.IsTrue(Enumerable.SequenceEqual<int>(expected, input));
+        }
+
+    }
+}
