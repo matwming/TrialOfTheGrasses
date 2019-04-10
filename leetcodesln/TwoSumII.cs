704976ef826ad502ce053edb97f0b6e23d0a2602
user3301 <stan_gai@Hotmail.com>
2019-04-10 16:00:41 +1000

    two sum 2 input array is sorted (using two pointers)

---
+++ leetcodesln/TwoSumII.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class TwoSumII
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var left = 0;
+            var right = numbers.Length - 1;
+
+            while (left < right)
+            {
+                if (numbers[left] + numbers[right] > target) --right;
+                else if (numbers[left] + numbers[right] < target) ++left;
+                else return new int[] { left + 1, right + 1 };
+            }
+
+            throw new Exception();
+        }
+    }
+}
---
+++ leetcodesln_test/TwoSumII.cs
@@ -0,0 +1,29 @@
+﻿using leetcodesln;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class TwoSumIIUnitTest
+    {
+        public TwoSumII TwoSumII { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            TwoSumII = new TwoSumII();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var expected = new int[] { 1, 2 };
+            var actual = TwoSumII.TwoSum(input, target);
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+    }
+}
