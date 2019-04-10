90dacbe1fe44aed66f0cbf021d46ed736ddb94f2
user3301 <stan_gai@Hotmail.com>
2019-04-10 15:40:40 +1000

    coantains duplicate (using hashtable)

---
+++ leetcodesln/ContainsDuplicate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class ContainsDuplicate
+    {
+        public bool ContainsDuplicateSln(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num)) return true;
+                else dict[num] = 1;
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/ContainsDuplicateUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ContainsDuplicateUnitTest
+    {
+        public ContainsDuplicate ContainsDuplicate { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ContainsDuplicate = new ContainsDuplicate();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 2, 3, 1 };
+            var actual = ContainsDuplicate.ContainsDuplicateSln(input);
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1, 2, 3, 4 };
+            var actual = ContainsDuplicate.ContainsDuplicateSln(input);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            var actual = ContainsDuplicate.ContainsDuplicateSln(input);
+            Assert.IsTrue(actual);
+        }
+
+    }
+}
