211ffbf7a4b8177c32c8d20602e78357e72ae372
user3301 <stan_gai@Hotmail.com>
2019-04-10 11:45:11 +1000

    best time to buy and sell stock 2 (greedy)

---
+++ leetcodesln/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcodesln
+{
+    public class BestTimeToBuyAndSellStockII
+    {
+
+        // greedy
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] - prices[i - 1] > 0) maxProfit += (prices[i] - prices[i - 1]);
+            }
+            return maxProfit;
+        }
+    }
+}
---
+++ leetcodesln_test/BestTimeToBuyAndSellStockIIUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class BestTimeToBuyAndSellStockIIUnitTest
+    {
+        public BestTimeToBuyAndSellStockII BestTimeToBuyAndSellStockII { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            BestTimeToBuyAndSellStockII = new BestTimeToBuyAndSellStockII();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 7, 1, 5, 3, 6, 4 };
+            var expected = 7;
+            var actual = BestTimeToBuyAndSellStockII.MaxProfit(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5 };
+            var expected = 4;
+            var actual = BestTimeToBuyAndSellStockII.MaxProfit(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 7, 6, 4, 3, 1 };
+            var expected = 0;
+            var actual = BestTimeToBuyAndSellStockII.MaxProfit(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
