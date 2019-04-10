bf829f78cafb44cfacb125f5610ba96a15bb5d6e
user3301 <stan_gai@Hotmail.com>
2019-04-10 21:08:55 +1000

    missing number (gauss formula, Runtime: 108 ms, faster than 97.81%)

--- leetcodesln/MissingNumber.cs
+++ leetcodesln/MissingNumber.cs
@@ -6,21 +6,33 @@ namespace leetcodesln
 {
     public class MissingNumber
     {
+        // public int FindMissingNumber(int[] nums)
+        // {
+        //     int[] arr = new int[nums.Length + 1];
+        //     for (int i = 0; i <nums.Length; i++)
+        //     {
+        //         arr[nums[i]] -= nums[i];
+        //     }
+        //     for (int i = 0; i < arr.Length; i++)
+        //     {
+        //         if (arr[i]==0 && i!=0)
+        //         {
+        //             return i;
+        //         }
+        //     }
+        //     return 0;
+        // }
+
+        //Gauss formula
         public int FindMissingNumber(int[] nums)
         {
-            int[] arr = new int[nums.Length + 1];
-            for (int i = 0; i <nums.Length; i++)
+            var sum = (0 + nums.Length + 1) * (nums.Length) / 2;
+
+            foreach (var num in nums)
             {
-                arr[nums[i]] -= nums[i];
+                sum -= num;
             }
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i]==0 && i!=0)
-                {
-                    return i;
-                }
-            }
-            return 0;
+            return sum;
         }
     }
 }
