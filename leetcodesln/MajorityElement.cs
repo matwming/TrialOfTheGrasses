c6652a0516e0e07e40777b2d53927c08052ee64d
user3301 <stan_gai@Hotmail.com>
2019-04-10 10:26:01 +1000

    :new: majority element (using Boyer-Moore Voting algorithm)

---
+++ leetcodesln/MajorityElement.cs
@@ -0,0 +1,47 @@
+﻿namespace leetcodesln
+{
+    public class MajorityElement
+    {
+        //using dictionary to count each element, when counter > half of the array size, return the number, O(n)
+        //public int MajorityElementSln(int[] nums)
+        //{
+        //    var half = nums.Length / 2;
+
+        //    var dict = new Dictionary<int, int>();
+
+        //    foreach (var num in nums)
+        //    {
+        //        if (dict.ContainsKey(num))
+        //        {
+        //            ++dict[num];
+        //        }
+        //        else dict[num] = 1;
+        //        if (dict[num] > half) return num;
+        //    }
+        //    throw new Exception();
+        //}
+
+
+        // boyer-moore majority voting algorithm
+        public int MajorityElementSln(int[] nums)
+        {
+            var majority = nums[0];
+            var count = 1;
+
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                if (count == 0)
+                {
+                    majority = nums[i];
+                    count = 1;
+                }
+                else if (majority != nums[i])
+                {
+                    --count;
+                }
+                else ++count;
+            }
+            return majority;
+        }
+    }
+}
---
+++ leetcodesln_test/MajorityElementUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class MajorityElementUnitTest
+    {
+        public MajorityElement MajorityElement { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MajorityElement = new MajorityElement();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 3, 2, 3 };
+            var expected = 3;
+            var actual = MajorityElement.MajorityElementSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
