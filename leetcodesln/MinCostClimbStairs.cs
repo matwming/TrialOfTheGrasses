f2cef0e003612563319d7e9480e91194ac71ee9e
user3301 <stan_gai@Hotmail.com>
2019-04-11 15:37:50 +1000

    min cost climbing stairs (greedy solution)

--- leetcodesln/MinCostClimbStairs.cs
+++ leetcodesln/MinCostClimbStairs.cs
@@ -4,21 +4,33 @@ namespace leetcodesln
 {
     public class MinCostClimbStairs
     {
+        //dp
+        //public int MinCostClimbingStairs(int[] cost)
+        //{
+        //    if (cost.Length<=1)
+        //    {
+        //        return 0;
+        //    }
+        //   int[] dp = new int[cost.Length+1];
+        //   dp[0] = cost[0];
+        //   dp[1] = cost[1];
+        //   for (int i = 2; i < dp.Length; i++)
+        //   {
+        //       int current = i==cost.Length?0:cost[i];
+        //       dp[i] = Math.Min(dp[i-1]+current,dp[i-2]+current);
+        //   }
+        //    return dp[cost.Length];
+        //}
+
+        //greedy
         public int MinCostClimbingStairs(int[] cost)
         {
-            if (cost.Length<=1)
+
+            for (int i = 2; i < cost.Length; i++)
             {
-                return 0;
+                cost[i] += Math.Min(cost[i - 1], cost[i - 2]);
             }
-           int[] dp = new int[cost.Length+1];
-           dp[0] = cost[0];
-           dp[1] = cost[1];
-           for (int i = 2; i < dp.Length; i++)
-           {
-               int current = i==cost.Length?0:cost[i];
-               dp[i] = Math.Min(dp[i-1]+current,dp[i-2]+current);
-           }
-            return dp[cost.Length];
+            return Math.Min(cost[cost.Length - 1], cost[cost.Length - 2]);
         }
     }
 }
--- leetcodesln_test/MinCostClimbingStairsUnitTest.cs
+++ leetcodesln_test/MinCostClimbingStairsUnitTest.cs
@@ -6,12 +6,13 @@ namespace leetcodesln_test
     [TestClass]
     public class MinCostClimbingStairsUnitTest
     {
-       [TestMethod]
-       public void MinCostClimbingStairsTestMethod()
-       {
-           MinCostClimbStairs minCostClimbingStairs = new MinCostClimbStairs();
-           Assert.AreEqual(15,minCostClimbingStairs.MinCostClimbingStairs(new int[]{10,15,20}));
-           Assert.AreEqual(6,minCostClimbingStairs.MinCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
-       } 
+        [TestMethod]
+        public void MinCostClimbingStairsTestMethod()
+        {
+            MinCostClimbStairs minCostClimbingStairs = new MinCostClimbStairs();
+            Assert.AreEqual(15, minCostClimbingStairs.MinCostClimbingStairs(new int[] { 10, 15, 20 }));
+            Assert.AreEqual(6, minCostClimbingStairs.MinCostClimbingStairs(new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }));
+            Assert.AreEqual(2, minCostClimbingStairs.MinCostClimbingStairs(new int[] { 0, 2, 2, 1 }));
+        }
     }
 }
