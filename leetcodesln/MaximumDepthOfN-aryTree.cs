30c5a24ab739dea56ffc2625c751d04fa8b20bc6
user3301 <stan_gai@Hotmail.com>
2019-04-13 21:36:07 +1000

    maximum depth of n-ary tree(BFS,Runtime: 180 ms, faster than 89.85%)

---
+++ leetcodesln/MaximumDepthOfN-aryTree.cs
@@ -0,0 +1,32 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class MaximumDepthOfN_aryTree
+    {
+        public int MaxDepth(Node root)
+        {
+            if (root == null) return 0;
+            var depth = 0;
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                ++depth;
+                var size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var current = queue.Dequeue();
+                    for (int j = 0; j < current.children.Count; j++)
+                    {
+                        queue.Enqueue(current.children[j]);
+                    }
+                }
+            }
+            return depth;
+
+        }
+    }
+}
---
+++ leetcodesln_test/MaximumDepthOfN-aryTreeUnitTest.cs
@@ -0,0 +1,75 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MaximumDepthOfN_aryTreeUnitTest
+    {
+        public MaximumDepthOfN_aryTree MaximumDepthOfN_aryTree { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MaximumDepthOfN_aryTree = new MaximumDepthOfN_aryTree();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node5 = new Node
+            {
+                val = 5,
+                children = new Node[] { }
+            };
+            var node6 = new Node
+            {
+                val = 6,
+                children = new Node[] { }
+            };
+            var node3 = new Node
+            {
+                val = 3,
+                children = new Node[]
+               {
+                    node5, node6
+               }
+            };
+
+            var node2 = new Node
+            {
+                val = 2,
+                children = new Node[] { }
+            };
+            var node4 = new Node
+            {
+                val = 4,
+                children = new Node[] { }
+            };
+
+            var root = new Node
+            {
+                val = 1,
+                children = new Node[]
+                {
+                    node3, node2, node4
+                }
+            };
+
+            var expected = 3;
+            var actual = MaximumDepthOfN_aryTree.MaxDepth(root);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            Node root = null;
+            var expected = 0;
+            var actual = MaximumDepthOfN_aryTree.MaxDepth(root);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
