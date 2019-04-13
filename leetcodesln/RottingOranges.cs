79b903c699c13467d54d59dc98dafd179f7d161e
user3301 <stan_gai@Hotmail.com>
2019-04-13 16:29:33 +1000

    :boom: rotting organes (using BFS, Runtime: 96 ms, faster than 100.00%)

---
+++ leetcodesln/RottingOranges.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class RottingOranges
+    {
+
+        // bfs
+        public int OrangesRotting(int[][] grid)
+        {
+            var numberOfFreshOranges = 0;
+            var queue = new Queue<int[]>();
+            var mins = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 1) ++numberOfFreshOranges;
+                    if (grid[i][j] == 2)
+                    {
+                        queue.Enqueue(new int[] { i, j });
+                    }
+                }
+            }
+
+            if (numberOfFreshOranges == 0) return 0;
+
+            int[][] adjacents = new int[4][];
+            adjacents[0] = new int[] { 1, 0 };
+            adjacents[1] = new int[] { -1, 0 };
+            adjacents[2] = new int[] { 0, 1 };
+            adjacents[3] = new int[] { 0, -1 };
+
+            while (queue.Count > 0)
+            {
+                ++mins;
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    int[] point = queue.Dequeue();
+                    foreach (var dir in adjacents)
+                    {
+                        int x = point[0] + dir[0];
+                        int y = point[1] + dir[1];
+
+                        if (x < 0 || y < 0 || x >= grid.Length || y >= grid[0].Length || grid[x][y]==0 || grid[x][y]==2) continue;
+                        grid[x][y] = 2;
+                        queue.Enqueue(new int[] { x, y });
+                        --numberOfFreshOranges;
+                    }
+                }
+            }
+
+            return numberOfFreshOranges == 0 ? mins - 1 : -1;
+
+
+        }
+    }
+}
---
+++ leetcodesln_test/RottingOrangesUnitTest.cs
@@ -0,0 +1,86 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RottingOrangesUnitTest
+    {
+        public RottingOranges RottingOranges { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            RottingOranges = new RottingOranges();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[3][];
+            input[0] = new int[] { 2, 1, 1 };
+            input[1] = new int[] { 1, 1, 0 };
+            input[2] = new int[] { 0, 1, 1 };
+            var expected = 4;
+            var actual = RottingOranges.OrangesRotting(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[3][];
+            input[0] = new int[] { 2, 1, 1 };
+            input[1] = new int[] { 0, 1, 1 };
+            input[2] = new int[] { 1, 0, 1 };
+            var expected = -1;
+            var actual = RottingOranges.OrangesRotting(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[1][];
+            input[0] = new int[] { 0, 1 };
+            var expected = -1;
+            var actual = RottingOranges.OrangesRotting(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var input = new int[1][];
+            input[0] = new int[] { 0 };
+            var expected = 0;
+            var actual = RottingOranges.OrangesRotting(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            var input = new int[1][];
+            input[0] = new int[] { 0,2,2 };
+            var expected = 0;
+            var actual = RottingOranges.OrangesRotting(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod6()
+        {
+            var input = new int[4][];
+            input[0] = new int[] { 1 };
+            input[1] = new int[] { 2 };
+            input[2] = new int[] { 1 };
+            input[3] = new int[] { 2 };
+
+            var expected = 1;
+            var actual = RottingOranges.OrangesRotting(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
