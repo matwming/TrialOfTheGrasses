969aa31e4744681313579bf059f115e2c6f8f8f8
user3301 <stan_gai@Hotmail.com>
2019-04-14 22:01:10 +1000

    n-ary tree level order traveral (BFS, Runtime: 336 ms, faster than 71.30%)

---
+++ leetcodesln/N-aryTreeLevelOrderTraversal.cs
@@ -0,0 +1,34 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class N_aryTreeLevelOrderTraversal
+    {
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            if (root == null) return new List<IList<int>>();
+            var ans = new List<IList<int>>();
+            var q = new Queue<Node>();
+            q.Enqueue(root);
+            while (q.Count != 0)
+            {
+                int size = q.Count;
+                IList<int> list = new List<int>();
+                for (int i = 0; i < size; i++)
+                {
+                    var node = q.Dequeue();
+                    list.Add(node.val);
+                    if (node.children == null) continue;
+                    foreach (var child in node.children)
+                    {
+                        q.Enqueue(child);
+                    }
+                }
+                ans.Add(list);
+
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/N-aryTreeLevelOrderTraversalUnitTest.cs
@@ -0,0 +1,76 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class N_aryTreeLevelOrderTraversalUnitTest
+    {
+        public N_aryTreeLevelOrderTraversal N_aryTreeLevelOrderTraversal { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            N_aryTreeLevelOrderTraversal = new N_aryTreeLevelOrderTraversal();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node5 = new Node
+            {
+                val = 5
+            };
+            var node6 = new Node
+            {
+                val = 6
+            };
+            var node3 = new Node
+            {
+                val = 3,
+                children = new List<Node> { node5, node6 }
+            };
+            var node2 = new Node
+            {
+                val = 2
+            };
+            var node4 = new Node
+            {
+                val = 4
+            };
+            var root = new Node
+            {
+                val = 1,
+                children = new List<Node> { node3, node2, node4 }
+            };
+
+            var expected = new List<IList<int>>();
+            expected.Add(new int[] { 1 });
+            expected.Add(new int[] { 3, 2, 4 });
+            expected.Add(new int[] { 5, 6 });
+            var actual = N_aryTreeLevelOrderTraversal.LevelOrder(root);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected[i], actual[i]));
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]  // null condition
+        public void TestMethod2()
+        {
+            Node root = null;
+            var expected = new List<IList<int>>();
+            var actual = N_aryTreeLevelOrderTraversal.LevelOrder(root);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected[i], actual[i]));
+            }
+
+        }
+
+    }
+}
