6802abf9fb36a5d91f2625adf92e91a10523cc8e
user3301 <stan_gai@Hotmail.com>
2019-04-15 20:57:07 +1000

    cousins in binary tree(bfs, Runtime: 92 ms, faster than 100.00%)

---
+++ leetcodesln/CousinsInBinaryTree.cs
@@ -0,0 +1,46 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class CousinsInBinaryTree
+    {
+        public bool IsCousins(TreeNode root, int x, int y)
+        {
+            if (root == null || root.val == x || root.val == y) return false;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count != 0)
+            {
+                bool foundx = false, foundy = false;
+                int size = q.Count;
+                TreeNode fatherx = null;
+                TreeNode fathery = null;
+                for (int i = 0; i < size; i++)
+                {
+                    var current = q.Dequeue();
+                    if (current.left != null)
+                    {
+                        foundx = foundx || current.left.val == x;
+                        q.Enqueue(current.left);
+                        foundy = foundy || current.left.val == y;
+                        fatherx = foundx && foundx ? fatherx == null ? current : fatherx : null;
+                        fathery = foundy ? fathery == null ? current : fathery : null;
+                    }
+                    if (current.right != null)
+                    {
+                        foundx = foundx || current.right.val == x;
+                        q.Enqueue(current.right);
+                        foundy = foundy || current.right.val == y;
+                        fatherx = foundx && foundx ? fatherx == null ? current : fatherx : null;
+                        fathery = foundy ? fathery == null ? current : fathery : null;
+                    }
+                }
+                if (foundx && foundy && fatherx != fathery && fatherx != null && fathery != null) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/CousinsInBinaryTreeUnitTest.cs
@@ -0,0 +1,107 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class CousinsInBinaryTreeUnitTest
+    {
+        public CousinsInBinaryTree CousinsInBinaryTree { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            CousinsInBinaryTree = new CousinsInBinaryTree();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node4 = new TreeNode(4);
+            var node3 = new TreeNode(3);
+            var node2 = new TreeNode(2)
+            {
+                left = node4
+            };
+            var root = new TreeNode(1)
+            {
+                left = node2,
+                right = node3
+            };
+            var expected = false;
+            var actual = CousinsInBinaryTree.IsCousins(root, 4, 3);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var node4 = new TreeNode(4);
+            var node5 = new TreeNode(5);
+            var node2 = new TreeNode(2)
+            {
+                right = node4
+            };
+            var node3 = new TreeNode(3)
+            {
+                right = node5
+            };
+            var root = new TreeNode(1)
+            {
+                left = node2,
+                right = node3
+            };
+            var expected = true;
+            var actual = CousinsInBinaryTree.IsCousins(root, 5, 4);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var node4 = new TreeNode(4);
+            var node3 = new TreeNode(3);
+            var node2 = new TreeNode(2)
+            {
+                right = node4
+            };
+            var root = new TreeNode(1)
+            {
+                left = node2,
+                right = node3
+            };
+            var expected = false;
+            var actual = CousinsInBinaryTree.IsCousins(root, 2, 3);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var node4 = new TreeNode(4);
+            var node6 = new TreeNode(6);
+            var node3 = new TreeNode(3)
+            {
+                left = node4
+            };
+            var node5 = new TreeNode(5)
+            {
+                right = node6
+            };
+            var node2 = new TreeNode(2)
+            {
+                left = node3
+            };
+            var root = new TreeNode(1)
+            {
+                left = node2,
+                right = node5
+            };
+            var expected = true;
+            var actual = CousinsInBinaryTree.IsCousins(root, 3,6);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
