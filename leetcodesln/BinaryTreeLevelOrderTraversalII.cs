adc4e0e3fbd567de3c2099c2100ee465eedc06e0
user3301 <stan_gai@Hotmail.com>
2019-04-15 21:59:52 +1000

    binary tree level order traversal ii (bfs, Runtime: 256 ms, faster than 63.67%)

---
+++ leetcodesln/BinaryTreeLevelOrderTraversalII.cs
@@ -0,0 +1,32 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class BinaryTreeLevelOrderTraversalII
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            var ans = new List<IList<int>>();
+            if (root == null) return ans;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count != 0)
+            {
+                int size = q.Count;
+                IList<int> list = new List<int>();
+                for (int i = 0; i < size; i++)
+                {
+                    var current = q.Dequeue();
+                    list.Add(current.val);
+                    if (current.left != null) q.Enqueue(current.left);
+                    if (current.right != null) q.Enqueue(current.right);
+                }
+                ans.Insert(0, list);
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/BinaryTreeLevelOrderTraversalIIUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class BinaryTreeLevelOrderTraversalIIUnitTest
+    {
+        public BinaryTreeLevelOrderTraversalII BinaryTreeLevelOrderTraversalII { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            BinaryTreeLevelOrderTraversalII = new BinaryTreeLevelOrderTraversalII();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node15 = new TreeNode(15);
+            var node7 = new TreeNode(7);
+            var node20 = new TreeNode(20)
+            {
+                left = node15,
+                right = node7
+            };
+            var node9 = new TreeNode(9);
+            var root = new TreeNode(3)
+            {
+                left = node9,
+                right = node20
+            };
+            var expected = new int[][] { new int[] { 15, 7 }, new int[] { 9, 20 }, new int[] { 3 } };
+            var actual = BinaryTreeLevelOrderTraversalII.LevelOrderBottom(root);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected[i], actual[i]));
+            }
+        }
+
+    }
+}
