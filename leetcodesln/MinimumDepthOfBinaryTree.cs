de05cde4613ddac65d06271f356412fe6557dc35
user3301 <stan_gai@Hotmail.com>
2019-04-16 15:25:29 +1000

    minimum depth of binary tree(bfs, Runtime: 96 ms, faster than 99.06%)

---
+++ leetcodesln/MinimumDepthOfBinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class MinimumDepthOfBinaryTree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            var minDepth = 1;
+            while (q.Count != 0)
+            {
+                int size = q.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var current = q.Dequeue();
+                    if (current.left == null && current.right == null) return minDepth;
+                    if (current.left != null) q.Enqueue(current.left);
+                    if (current.right != null) q.Enqueue(current.right);
+                }
+                ++minDepth;
+            }
+            return minDepth;
+        }
+    }
+}
---
+++ leetcodesln_test/MinimumDepthOfBinaryTreeUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class MinimumDpethOfBinaryTreeUnitTest
+    {
+        public MinimumDepthOfBinaryTree MinimumDepthOfBinaryTree { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MinimumDepthOfBinaryTree = new MinimumDepthOfBinaryTree();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node15 = new TreeNode(15);
+            var node7 = new TreeNode(7);
+            var node20 = new TreeNode(20)
+            {
+                left = node15,
+                right = node7
+            };
+            var node9 = new TreeNode(9);
+            var root = new TreeNode(3)
+            {
+                left = node9,
+                right = node20
+            };
+            var expected = 2;
+            var actual = MinimumDepthOfBinaryTree.MinDepth(root);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
