a9fc8d7c706ad59b73dddb0777304b9f28618e0e
user3301 <stan_gai@Hotmail.com>
2019-04-16 17:17:55 +1000

    find bottom left tree value(bfs, Runtime: 96 ms, faster than 100.00%)

---
+++ leetcodesln/FindBottomLeftTreeValue.cs
@@ -0,0 +1,29 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class FindBottomLeftTreeValue
+    {
+        public int FindBottomLeftValue(TreeNode root)
+        {
+            var q = new Queue<TreeNode>();
+            var ans = root.val;
+            q.Enqueue(root);
+
+            while (q.Count != 0)
+            {
+                int size = q.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var current = q.Dequeue();
+                    if (current.right != null) ans = current.right.val;
+                    if (current.left != null) ans = current.left.val;
+                    if (current.right != null) q.Enqueue(current.right);
+                    if (current.left != null) q.Enqueue(current.left);
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/FindBottomLeftTreeValueUnitTest.cs
@@ -0,0 +1,79 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindBottomLeftTreeValueUnitTest
+    {
+        public FindBottomLeftTreeValue FindBottomLeftTreeValue { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            FindBottomLeftTreeValue = new FindBottomLeftTreeValue();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node1 = new TreeNode(1);
+            var node3 = new TreeNode(3);
+            var root = new TreeNode(2)
+            {
+                left = node1,
+                right = node3
+            };
+            var expected = 1;
+            var actual = FindBottomLeftTreeValue.FindBottomLeftValue(root);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var node3 = new TreeNode(-1);
+            var root = new TreeNode(0)
+            {
+                right = node3
+        };
+            var expected = -1;
+            var actual = FindBottomLeftTreeValue.FindBottomLeftValue(root);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var node7 = new TreeNode(7);
+            var node4 = new TreeNode(4);
+            var node6 = new TreeNode(6);
+            var node5 = new TreeNode(5)
+            {
+                left = node7
+            };
+            var node3 = new TreeNode(3)
+            {
+                left = node5,
+                right = node6
+            };
+            var node2 = new TreeNode(2)
+            {
+                right = node4
+            };
+            var root = new TreeNode(1)
+            {
+                left = node2,
+                right = node3
+            };
+            var expected = 7;
+            var actual = FindBottomLeftTreeValue.FindBottomLeftValue(root);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
