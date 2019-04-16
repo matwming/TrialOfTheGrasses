997051dca1d836ef449e712ddd0d23dcf06ae42a
user3301 <stan_gai@Hotmail.com>
2019-04-16 20:26:03 +1000

    find largest value in each tree row(bfs, Runtime: 252 ms, faster than 98.15%)

---
+++ leetcodesln/FindLargestValueInEachTreeRow.cs
@@ -0,0 +1,33 @@
+﻿using leetcodesln.common;
+using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class FindLargestValueInEachTreeRow
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            if (root == null) return new int[] { };
+
+            var ans = new List<int>();
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count != 0)
+            {
+                var max = int.MinValue;
+                int size = q.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var current = q.Dequeue();
+                    max = Math.Max(max, current.val);
+                    if (current.left != null) q.Enqueue(current.left);
+                    if (current.right != null) q.Enqueue(current.right);
+                }
+                ans.Add(max);
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/FindLargestValueInEachTreeRowUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindLargestValueInEachTreeRowUnitTest
+    {
+        public FindLargestValueInEachTreeRow FindLargestValueInEachTreeRow { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            FindLargestValueInEachTreeRow = new FindLargestValueInEachTreeRow();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node5 = new TreeNode(5);
+            var node3b = new TreeNode(3);
+            var node9 = new TreeNode(9);
+            var node3a = new TreeNode(3)
+            {
+                left = node5,
+                right = node3b
+            };
+            var node2 = new TreeNode(2)
+            {
+                right = node9
+            };
+            var root = new TreeNode(1)
+            {
+                left = node3a,
+                right = node2
+            };
+            var expected = new int[] { 1, 3, 9 };
+            var actual = FindLargestValueInEachTreeRow.LargestValues(root);
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            TreeNode root = null;
+            var expected = new int[] { };
+            var actual = FindLargestValueInEachTreeRow.LargestValues(root);
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+    }
+}
