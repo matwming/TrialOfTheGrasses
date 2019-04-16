12c390cec9b289ebd2c25334aaf39817cf9560c2
user3301 <stan_gai@Hotmail.com>
2019-04-16 13:59:39 +1000

    symmetric tree (bfs, Runtime: 96 ms, faster than 64.02%)

---
+++ leetcodesln/SymmetricTree.cs
@@ -0,0 +1,50 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class SymmetricTree
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count != 0)
+            {
+                int size = q.Count;
+                var list = new List<int?>();
+                for (int i = 0; i < size; i++)
+                {
+                    var current = q.Dequeue();
+                    if (current.left != null)
+                    {
+                        list.Add(current.left.val);
+                        q.Enqueue(current.left);
+                    }
+                    else
+                    {
+                        list.Add(null);
+                    }
+                    if (current.right != null)
+                    {
+                        list.Add(current.right.val);
+                        q.Enqueue(current.right);
+                    }
+                    else
+                    {
+                        list.Add(null);
+                    }
+                }
+                int left = 0, right = list.Count - 1;
+                while (left < right)
+                {
+                    if (list[left++] != list[right--]) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/SymmetricTreeUnitTest.cs
@@ -0,0 +1,76 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SymmetricTreeUnitTest
+    {
+        public SymmetricTree SymmetricTree { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            SymmetricTree = new SymmetricTree();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node3l = new TreeNode(3);
+            var node3r = new TreeNode(3);
+            var node2l = new TreeNode(2)
+            {
+                right = node3l
+            };
+            var node2r = new TreeNode(2)
+            {
+                right = node3r
+            };
+            var root = new TreeNode(1)
+            {
+                left = node2l,
+                right = node2r
+            };
+            var expected = false;
+            var actual = SymmetricTree.IsSymmetric(root);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            TreeNode root = null;
+            Assert.IsTrue(SymmetricTree.IsSymmetric(root));
+        }
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var node3l = new TreeNode(3);
+            var node4l = new TreeNode(4);
+            var node4r = new TreeNode(4);
+            var node3r = new TreeNode(3);
+            var node2l = new TreeNode(2)
+            {
+                left = node3l,
+                right = node4l
+            };
+            var node2r = new TreeNode(2)
+            {
+                left = node4r,
+                right = node3r
+            };
+            var root = new TreeNode(1)
+            {
+                left = node2l,
+                right = node2r
+            };
+            var expected = true;
+            var actual = SymmetricTree.IsSymmetric(root);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
