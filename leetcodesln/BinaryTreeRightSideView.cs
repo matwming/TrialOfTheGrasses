b5027a039f66ef23859d3cf77be83e9a58e30177
user3301 <stan_gai@Hotmail.com>
2019-04-17 15:00:23 +1000

    binary tree right side view (bfs, Runtime: 248 ms, faster than 91.63%)

---
+++ leetcodesln/BinaryTreeRightSideView.cs
@@ -0,0 +1,31 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class BinaryTreeRightSideView
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            var ans = new List<int>();
+
+            if (root == null) return ans;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count != 0)
+            {
+                int size = q.Count;
+                ans.Add(q.Peek().val);
+                for (int i = 0; i < size; i++)
+                {
+                    var current = q.Dequeue();
+                    if (current.right != null) q.Enqueue(current.right);
+                    if (current.left != null) q.Enqueue(current.left);
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/BinaryTreeRightSideViewUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class BinaryTreeRightSideViewUnitTest
+    {
+        public BinaryTreeRightSideView BinaryTreeRightSideView { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            BinaryTreeRightSideView = new BinaryTreeRightSideView();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var node5 = new TreeNode(5);
+            var node4 = new TreeNode(4);
+            var node2 = new TreeNode(2)
+            {
+                right = node5
+            };
+            var node3 = new TreeNode(3)
+            {
+                right = node4
+            };
+            var root = new TreeNode(1)
+            {
+                left = node2,
+                right = node3
+            };
+            var expected = new int[] { 1, 3, 4 };
+            var actual = BinaryTreeRightSideView.RightSideView(root);
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+    }
+}
