5ccfecd2fd91665181074b81e4acc5edc931d0a3
user3301 <stan_gai@Hotmail.com>
2019-04-18 22:59:28 +1000

    :new:increasing order search tree(dfs-in order, Runtime: 124 ms, faster than 94.66%)

---
+++ leetcodesln/IncreasingOrderSearchTree.cs
@@ -0,0 +1,38 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class IncreasingOrderSearchTree
+    {
+        public TreeNode IncreasingBST(TreeNode root)
+        {
+            if (root == null) return null;
+
+            var s = new Stack<TreeNode>();
+            var list = new List<int>();
+            var current = root;
+            while (current != null || s.Count != 0)
+            {
+                while (current != null)
+                {
+                    s.Push(current);
+
+                    current = current.left;
+                }
+                current = s.Peek();
+                s.Pop();
+                list.Add(current.val);
+                current = current.right;
+            }
+            var ans = new TreeNode(list[0]);
+            var walk = ans;
+            for (int i = 1; i < list.Count; i++)
+            {
+                walk.right = new TreeNode(list[i]);
+                walk = walk.right;
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/IncreasingOrderSearchTreeUnitTest.cs
@@ -0,0 +1,87 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class IncreasingOrderSearchTreeUnitTest
+    {
+        public IncreasingOrderSearchTree IncreasingOrderSearchTree { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            IncreasingOrderSearchTree = new IncreasingOrderSearchTree();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            TreeNode root = null;
+            var actual = IncreasingOrderSearchTree.IncreasingBST(root);
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var node1 = new TreeNode(1);
+            var node7 = new TreeNode(7);
+            var node9 = new TreeNode(9);
+            var node8 = new TreeNode(8)
+            {
+                left = node7,
+                right = node9
+            };
+            var node2 = new TreeNode(2)
+            {
+                left = node1
+            };
+            var node4 = new TreeNode(4);
+            var node6 = new TreeNode(6)
+            {
+                right = node8
+            };
+            var node3 = new TreeNode(3)
+            {
+                left = node2,
+                right = node4
+            };
+            var root = new TreeNode(5)
+            {
+                left = node3,
+                right = node6
+            };
+            var expected = new TreeNode(1)
+            {
+                right = new TreeNode(2)
+                {
+                    right = new TreeNode(3)
+                    {
+                        right = new TreeNode(4)
+                        {
+                            right = new TreeNode(5)
+                            {
+                                right = new TreeNode(6)
+                                {
+                                    right = new TreeNode(7)
+                                    {
+                                        right = new TreeNode(8)
+                                        {
+                                            right = new TreeNode(9)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            var actual = IncreasingOrderSearchTree.IncreasingBST(root);
+            Assert.IsTrue(Helper<int>.HaveSameInorderSequenceForTwoTreeNodes(expected, actual));
+        }
+
+    }
+}
