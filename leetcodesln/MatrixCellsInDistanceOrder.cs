c4c25385b5bcfd2f7852fb1b507275fd5df8fa2e
user3301 <stan_gai@Hotmail.com>
2019-04-21 16:30:18 +1000

    weekly contest 133, matrix cells in distance order

---
+++ leetcodesln/MatrixCellsInDistanceOrder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcodesln
+{
+    public class MatrixCellsInDistanceOrder
+    {
+        public int[][] AllCellsDistOrder(int R, int C, int r0, int c0)
+        {
+            var dict = new Dictionary<(int x, int y), int>();
+            var ans = new int[R * C][];
+            for (int i = 0; i < R; i++)
+            {
+                for (int j = 0; j < C; j++)
+                {
+                    dict.Add((i, j), Math.Abs(r0 - i) + Math.Abs(c0 - j));
+                }
+            }
+            var myList = dict.ToList();
+            myList.Sort((p1, p2) => p1.Value.CompareTo(p2.Value));
+            for (int i = 0; i < myList.Count; i++)
+            {
+                ans[i] = new int[] { myList[i].Key.x, myList[i].Key.y };
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/MatrixCellsInDistanceOrderUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using leetcodesln;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MatrixCellsInDistanceOrderUnitTest
+    {
+        public MatrixCellsInDistanceOrder MatrixCellsInDistanceOrder { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MatrixCellsInDistanceOrder = new MatrixCellsInDistanceOrder();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var R = 1;
+            var C = 2;
+            var r0 = 0;
+            var c0 = 0;
+            var acutal = MatrixCellsInDistanceOrder.AllCellsDistOrder(R, C, r0, c0);
+            var expected = new int[][] { new int[] { 0, 0 }, new int[] { 1,0} };
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected[i], acutal[i]));
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var R = 2;
+            var C = 2;
+            var r0 = 0;
+            var c0 = 1;
+            var acutal = MatrixCellsInDistanceOrder.AllCellsDistOrder(R, C, r0, c0);
+            var expected = new int[][] { new int[] { 0,1 }, new int[] { 0, 0 }, new int[] { 1,1}, new int[] { 1,0} };
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected[i], acutal[i]));
+            }
+        }
+
+    }
+}
