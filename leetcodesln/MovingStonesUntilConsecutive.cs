94e14a7df17adfed2aba264557454606829ca858
user3301 <stan_gai@Hotmail.com>
2019-04-29 09:51:42 +1000

    moving stones until consecutive(weekly contest 134)

---
+++ leetcodesln/MovingStonesUntilConsecutive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class MovingStonesUntilConsecutive
+    {
+        public int[] NumMovesStones(int a, int b, int c)
+        {
+            int[] ans = new int[2];
+            int[] arr = new int[] { a, b, c };
+            Array.Sort(arr);
+
+            ans[0] = MinSteps(arr[0], arr[1], arr[2]);
+            ans[1] = arr[1] - arr[0] - 1 + (arr[2] - arr[1] - 1);
+            return ans;
+        }
+
+
+        // good one
+        private int MinSteps(int a, int b, int c)
+        {
+            int left = b - a - 1;
+            int right = c - b - 1;
+            //                   xx___x              x____xx                    x_x_x
+            return Math.Min((left > 0 ? 1 : 0) + (right > 0 ? 1 : 0), (left == 1 || right == 1 ? 1 : 2));
+        }
+
+        // not so good one
+        //private int MinSteps(int a, int b, int c)
+        //{
+        //    if (a + 1 == b && b + 1 == c) return 0;
+        //    if (a + 1 != b && b + 1 == c) return 1;
+        //    if (a + 2 == b || b + 2 == c) return 1;
+        //    if (a + 1 == b && b + 1 != c) return 1;
+        //    else return 2;
+        //}
+    }
+}
---
+++ leetcodesln_test/MovingStonesUntilConsecutiveUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetcodesln;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MovingStonesUntilConsecutiveUnitTest
+    {
+        public MovingStonesUntilConsecutive MovingStonesUntilConsecutive { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MovingStonesUntilConsecutive = new MovingStonesUntilConsecutive();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            int a = 4, b = 3, c = 2;
+            var expected = new int[] { 0, 0 };
+            var actual = MovingStonesUntilConsecutive.NumMovesStones(a, b, c);
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            int a = 3, b = 5, c = 1;
+            var expected = new int[] { 1, 2 };
+            var actual = MovingStonesUntilConsecutive.NumMovesStones(a, b, c);
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+    }
+}
