f476816e9e9317bbf0148f87f9cd546168cd9d88
user3301 <stan_gai@Hotmail.com>
2019-05-01 21:17:42 +1000

    coloring a border (weekly contest 134, bfs with another table to record visited nodes)

---
+++ leetcodesln/ColoringABorder.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class ColoringABorder
+    {
+
+        public int[][] ColorBorder(int[][] grid, int r0, int c0, int color)
+        {
+            bool[][] check = new bool[grid.Length][];
+            for (int i = 0; i < grid.Length; i++)
+            {
+                check[i] = new bool[grid[i].Length];
+            }
+
+            var queue = new Queue<int[]>();
+
+            int[][] dirs = new int[4][];
+            dirs[0] = new int[] { 1, 0 };
+            dirs[1] = new int[] { -1, 0 };
+            dirs[2] = new int[] { 0, 1 };
+            dirs[3] = new int[] { 0, -1 };
+
+            int val = grid[r0][c0];
+            queue.Enqueue(new int[] { r0, c0 });
+
+            while (queue.Count!=0)
+            {
+                int size = queue.Count;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var point = queue.Dequeue();
+
+                    if(NeedColor(grid,point[0], point[1], val))
+                    {
+                        check[point[0]][point[1]] = true;
+                        grid[point[0]][point[1]] = -grid[point[0]][point[1]];
+                    }
+
+                    foreach (var dir in dirs)
+                    {
+                        int x = point[0] + dir[0];
+                        int y = point[1] + dir[1];
+
+                        if (x < 0 || y < 0 || x >= grid.Length || y >= grid[0].Length || check[x][y]) continue;
+                        if (grid[x][y] == val)
+                        {
+                            queue.Enqueue(new int[] { x, y });
+                            check[x][y] = true;
+                        }
+                    }
+
+
+                }
+
+            }
+
+            for (int a = 0; a < grid.Length; a++)
+            {
+                for (int b = 0; b < grid[a].Length; b++)
+                {
+                    if (grid[a][b]< 0) grid[a][b] = color;
+                }
+            }
+            return grid;
+        }
+
+        private bool NeedColor(int[][] grid, int x, int y, int val)
+        {
+            int[][] dirs = new int[4][];
+            dirs[0] = new int[] { 1, 0 };
+            dirs[1] = new int[] { -1, 0 };
+            dirs[2] = new int[] { 0, 1 };
+            dirs[3] = new int[] { 0, -1 };
+
+            if (x == 0 || y == 0 || x == grid.Length - 1 || y == grid[0].Length - 1) return true;
+            foreach (var dir in dirs)
+            {
+                int xx = x + dir[0];
+                int yy = y + dir[1];
+                if (xx < 0 || yy < 0 || xx >= grid.Length || yy >= grid[0].Length) continue;
+                if (Math.Abs(grid[xx][yy]) != val) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/ColoringABorderUnitTest.cs
@@ -0,0 +1,143 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ColoringABorderUnitTest
+    {
+        public ColoringABorder ColoringABorder { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ColoringABorder = new ColoringABorder();
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var grid = new int[2][];
+            grid[0] = new int[] { 1, 2, 2 };
+            grid[1] = new int[] { 2, 3, 2 };
+            int r0 = 0;
+            int c0 = 1;
+            int color = 3;
+            var expected = new int[2][];
+            expected[0] = new int[] { 1, 3, 3 };
+            expected[1] = new int[] { 2, 3, 3 };
+            var actual = ColoringABorder.ColorBorder(grid, r0, c0, color);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var grid = new int[3][];
+            grid[0] = new int[] { 1, 1, 1 };
+            grid[1] = new int[] { 1, 1, 1 };
+            grid[2] = new int[] { 1, 1, 1 };
+
+            int r0 = 1;
+            int c0 = 1;
+            int color = 2;
+            var expected = new int[3][];
+            expected[0] = new int[] { 2, 2, 2 };
+            expected[1] = new int[] { 2, 1, 2 };
+            expected[2] = new int[] { 2, 2, 2 };
+            var actual = ColoringABorder.ColorBorder(grid, r0, c0, color);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var grid = new int[2][];
+            grid[0] = new int[] { 1, 1 };
+            grid[1] = new int[] { 1, 2 };
+
+            int r0 = 0;
+            int c0 = 0;
+            int color = 3;
+            var expected = new int[2][];
+            expected[0] = new int[] { 3, 3 };
+            expected[1] = new int[] { 3, 2 };
+            var actual = ColoringABorder.ColorBorder(grid, r0, c0, color);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod4()
+        {
+            var grid = new int[3][];
+            grid[0] = new int[] { 1, 1, 1, 2, 2 };
+            grid[1] = new int[] { 2, 1, 2, 2, 2 };
+            grid[2] = new int[] { 1, 1, 2, 2, 1 };
+
+            int r0 = 1;
+            int c0 = 0;
+            int color = 1;
+            var expected = new int[3][];
+            expected[0] = new int[] { 1, 1, 1, 2, 2 };
+            expected[1] = new int[] { 1, 1, 2, 2, 2 };
+            expected[2] = new int[] { 1, 1, 2, 2, 1 };
+
+            var actual = ColoringABorder.ColorBorder(grid, r0, c0, color);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+
+        [TestMethod, Timeout(8_000)]
+        public void TestMethod5()
+        {
+            var grid = new int[5][];
+            grid[0] = new int[] { 2, 1, 3, 2, 1, 1, 2 };
+            grid[1] = new int[] { 1, 2, 3, 1, 2, 1, 2 };
+            grid[2] = new int[] { 1, 2, 1, 2, 2, 2, 2 };
+            grid[3] = new int[] { 2, 1, 2, 2, 2, 2, 2 };
+            grid[4] = new int[] { 2, 3, 3, 3, 2, 1, 2 };
+
+            int r0 = 4;
+            int c0 = 4;
+            int color = 3;
+            var expected = new int[5][];
+            expected[0] = new int[] { 2, 1, 3, 2, 1, 1, 3 };
+            expected[1] = new int[] { 1, 2, 3, 1, 3, 1, 3 };
+            expected[2] = new int[] { 1, 2, 1, 3, 2, 3, 3 };
+            expected[3] = new int[] { 2, 1, 3, 3, 2, 3, 3 };
+            expected[4] = new int[] { 2, 3, 3, 3, 3, 1, 3 };
+
+            var actual = ColoringABorder.ColorBorder(grid, r0, c0, color);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+    }
+}
