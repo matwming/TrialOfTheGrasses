6d28180ce35e982352c43ac2a9f1d2848e396899
user3301 <stan_gai@Hotmail.com>
2019-05-05 12:24:59 +1000

    wip(time limit exceeded)

---
+++ leetcodesln/EscapeALargeMaze.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class EscapeALargeMaze
+    {
+        public bool IsEscapePossible(int[][] blocked, int[] source, int[] target)
+        {
+            if (blocked.Length == 0) return true;
+            var blockers = new HashSet<string>();
+            foreach (var b in blocked)
+            {
+                blockers.Add(b[0] + "," + b[1]);
+            }
+            var seen = new HashSet<string>();
+            return IsEscapePossibleBFS(blockers, source, target, seen) && IsEscapePossibleBFS(blockers, target, source, seen);
+        }
+
+        private bool IsEscapePossibleBFS(HashSet<string> blockers, int[] source, int[] target, HashSet<string> seen)
+        {
+            if (target[0] < 0 || target[0] >= 1e6 || target[1] < 0 || target[1] >= 1e6) return false;
+
+            var dirs = new int[4][];
+            dirs[0] = new int[] { 1, 0 };
+            dirs[1] = new int[] { 0, 1 };
+            dirs[2] = new int[] { -1, 0 };
+            dirs[3] = new int[] { 0, -1 };
+
+
+            var q = new Queue<int[]>();
+
+
+            q.Enqueue(source);
+
+            while (q.Count != 0)
+            {
+                int size = q.Count;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var point = q.Dequeue();
+                    if (point[0] == target[0] && point[1] == target[1]) return true;
+
+                    foreach (var dir in dirs)
+                    {
+                        int x = point[0] + dir[0];
+                        int y = point[1] + dir[1];
+                        var str = x + "," + y;
+                        if (x >= 0 && x < 1e6 && y >= 0 && y < 1e6 && !blockers.Contains(str) && !seen.Contains(str))
+                        {
+                            if (target[0] == x && target[1] == y) return true;
+                            q.Enqueue(new int[] { x, y });
+                            seen.Add(str);
+                            if (seen.Count == 2e4) return true;
+                        }
+
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/EscapeALargeMazeUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class EscapeALargeMazeUnitTest
+    {
+        public EscapeALargeMaze EscapeALargeMaze { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            EscapeALargeMaze = new EscapeALargeMaze();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var blocked = new int[2][];
+            blocked[0] = new int[] { 0, 1 };
+            blocked[1] = new int[] { 1, 0 };
+            var source = new int[] { 0, 0 };
+            var target = new int[] { 0, 2 };
+            var expected = false;
+            var actual = EscapeALargeMaze.IsEscapePossible(blocked, source, target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var blocked = new int[][] {  };
+            var source = new int[] { 0,0 };
+            var target = new int[] { 999999,999999 };
+            var expected = true;
+            var actual = EscapeALargeMaze.IsEscapePossible(blocked, source, target);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(5_000)]
+        public void TestMethod3()
+        {
+            var blocked = new int[][] { new int[] { 961938,300406}, new int[] { 710196, 624190}, new int[] { 858790, 609485 }, new int[] { 268029, 225806}, new int[] { 200010, 18864}, new int[] { 132599, 612099}, new int[] { 329444, 633495}, new int[] { 196657, 757958}, new int[] { 628509, 883388} };
+            var source = new int[] { 655988, 180910 };
+            var target = new int[] { 267728, 840949 };
+            var expected = true;
+            var actual = EscapeALargeMaze.IsEscapePossible(blocked, source, target);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
