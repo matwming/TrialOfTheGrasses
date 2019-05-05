d4f2e5b99f671b5499569f4a6162154bcbf4eccc
user3301 <stan_gai@Hotmail.com>
2019-05-06 09:37:15 +1000

    valid boomerang(weekly contest 135, Runtime: 92 ms, faster than 100.00%)

---
+++ leetcodesln/ValidBoomerang.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class ValidBoomerang
+    {
+        public bool IsBoomerang(int[][] points)
+        {
+            return !IsOnLineAndValid(points) && !IsOnDiagonalAndValid(points);
+        }
+
+        private bool IsOnDiagonalAndValid(int[][] points)
+        {
+            if (points[0][0] == points[1][0] && points[0][1] == points[1][1]) return true;  // point1 == point2 not valid 
+            if (points[1][0] == points[2][0] && points[1][1] == points[2][1]) return true; // p2 == p3 not boomerang
+            if (points[0][0] == points[2][0] && points[0][1] == points[2][1] && (points[0][0] - points[1][0] != points[0][1] - points[1][1])) return true; // point1==point3 but point2 not valid
+            if ((points[0][0]-points[1][0]== points[0][1]-points[1][1]) && (points[0][0] - points[2][0] == points[0][1] - points[2][1])) return true;
+            return false;
+        }
+
+        private bool IsOnLineAndValid(int[][] points)
+        {
+            if (points[0][0] == points[1][0] && points[0][1] == points[1][1]) return true;  // point1 == point2 not valid 
+            if (points[1][0] == points[2][0] && points[1][1] == points[2][1]) return true; // p2 == p3 not boomerang
+
+            if (points[0][0] == points[2][0] && points[0][1] == points[2][1] && (points[0][0] - points[1][0] != points[0][1] - points[1][1])) return true; // point1==point3 but point2 not valid
+            if ((points[0][0] == points[1][0] && points[0][0] == points[2][0]) || (points[0][1] == points[1][1] && points[0][1] == points[2][1])) return true;
+            return false;
+        }
+    }
+}
---
+++ leetcodesln_test/ValidBoomerangUnitTest.cs
@@ -0,0 +1,138 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ValidBoomerangUnitTest
+    {
+        public ValidBoomerang ValidBoomerang { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ValidBoomerang = new ValidBoomerang();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 1, 1 };
+            input[1] = new int[] { 2, 2 };
+            input[2] = new int[] { 3, 3 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsFalse(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 1, 1 };
+            input[1] = new int[] { 2, 3 };
+            input[2] = new int[] { 3, 2 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 0 };
+            input[1] = new int[] { 0, 2 };
+            input[2] = new int[] { 2, 1 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 0 };
+            input[1] = new int[] { 1, 0 };
+            input[2] = new int[] { 2, 2 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 0 };
+            input[1] = new int[] { 2, 0 };
+            input[2] = new int[] { 0, 2 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod6()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 1 };
+            input[1] = new int[] { 1, 0 };
+            input[2] = new int[] { 0, 1 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsFalse(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod7()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 1 };
+            input[1] = new int[] { 1, 1 };
+            input[2] = new int[] { 2, 1 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsFalse(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod8()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 0 };
+            input[1] = new int[] { 2, 1 };
+            input[2] = new int[] { 2, 1 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsFalse(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod9()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 0 };
+            input[1] = new int[] { 2, 0 };
+            input[2] = new int[] { 0, 2 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod10()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 2 };
+            input[1] = new int[] { 0, 2 };
+            input[2] = new int[] { 2, 0 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsFalse(expected);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod11()
+        {
+            int[][] input = new int[3][];
+            input[0] = new int[] { 0, 1 };
+            input[1] = new int[] { 0, 2 };
+            input[2] = new int[] { 1, 2 };
+            var expected = ValidBoomerang.IsBoomerang(input);
+            Assert.IsTrue(expected);
+        }
+    }
+}
