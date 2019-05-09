531fcd0f9be2c532111a64f1167eef04174ab14c
user3301 <stan_gai@Hotmail.com>
2019-05-09 13:25:14 +1000

    :star:divisor game (game theory, dp)

---
+++ leetcodesln/DivisorGame.cs
@@ -0,0 +1,55 @@
+﻿namespace leetcodesln
+{
+    public class DivisorGame
+    {
+        public bool?[] Dp { get; set; } = new bool?[1001];
+
+
+        public bool DivisorGameSln(int N)
+        {
+            return AliceWin(N);
+        }
+
+        private bool AliceWin(int n)
+        {
+            if (n == 1) return false;
+            if (Dp[n] != null) return Dp[n].Value;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    if (!BobWin(n - i))
+                    {
+                        Dp[i] = true;
+                        return true;
+                    }
+                    else Dp[i] = false;
+                }
+            }
+            Dp[n] = false;
+            return Dp[n].Value;
+        }
+
+        private bool BobWin(int n)
+        {
+            if (n == 1) return false;
+            if (Dp[n] != null) return Dp[n].Value;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    if (!AliceWin(n - i))
+                    {
+                        Dp[i] = true;
+                        return true;
+                    }
+                    else Dp[i] = false;
+                }
+            }
+            Dp[n] = false;
+            return Dp[n].Value;
+        }
+    }
+}
---
+++ leetcodesln_test/DivisorGameUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DivisorGameUnitTest
+    {
+        public DivisorGame DivisorGame { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DivisorGame = new DivisorGame();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = 2;
+            Assert.IsTrue(DivisorGame.DivisorGameSln(input));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = 3;
+            Assert.IsFalse(DivisorGame.DivisorGameSln(input));
+        }
+
+    }
+}
