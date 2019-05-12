a4bea4ded32e1ab6ad613bc52fe90029852d832e
user3301 <stan_gai@Hotmail.com>
2019-05-12 16:42:12 +1000

    weekly contest 136(robot bounded in circle using  oop)

---
+++ leetcodesln/RobotBoundedInCircle.cs
@@ -0,0 +1,105 @@
+﻿namespace leetcodesln
+{
+    public class RobotBoundedInCircle
+    {
+        public bool IsRobotBounded(string instructions)
+        {
+            var r = new RobotForThis();
+
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (var chr in instructions)
+                {
+                    r.Move(chr);
+                }
+            }
+
+
+            if (r.Initial[0] == 0 && r.Initial[1] == 0) return true;
+            else return false;
+        }
+
+
+    }
+
+    class RobotForThis
+    {
+        public int[] Initial { get; set; } = new int[] { 0, 0 };
+
+        public string Dirction { get; set; } = "N";
+
+        public void Move(char instruction)
+        {
+            switch (instruction)
+            {
+                case 'G':
+                    Go(Dirction);
+                    break;
+                case 'L':
+                    ChangeDirctionL();
+                    break;
+                case 'R':
+                    ChangeDirctionR();
+                    break;
+            }
+        }
+
+        private void ChangeDirctionR()
+        {
+            switch (Dirction)
+            {
+                case "N":
+                    Dirction = "E";
+                    break;
+                case "W":
+                    Dirction = "N";
+                    break;
+                case "S":
+                    Dirction = "W";
+                    break;
+                case "E":
+                    Dirction = "S";
+                    break;
+            }
+        }
+
+        private void ChangeDirctionL()
+        {
+            switch (Dirction)
+            {
+                case "N":
+                    Dirction = "W";
+                    break;
+                case "W":
+                    Dirction = "S";
+                    break;
+                case "S":
+                    Dirction = "E";
+                    break;
+                case "E":
+                    Dirction = "N";
+                    break;
+            }
+        }
+
+        private void Go(string dirction)
+        {
+            switch (dirction)
+            {
+                case "N":
+                    Initial[1]++;
+                    break;
+                case "W":
+                    Initial[0]--;
+                    break;
+                case "S":
+                    Initial[1]--;
+                    break;
+                case "E":
+                    Initial[0]++;
+                    break;
+            }
+        }
+    }
+
+}
---
+++ leetcodesln_test/RobotBoundedInCircleUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RobotBoundedInCircleUnitTest
+    {
+        public RobotBoundedInCircle RobotBoundedInCircle { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            RobotBoundedInCircle = new RobotBoundedInCircle();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = "GGLLGG";
+            var expected = true;
+            var actual = RobotBoundedInCircle.IsRobotBounded(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = "GG";
+            var expected = false;
+            var actual = RobotBoundedInCircle.IsRobotBounded(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = "GL";
+            var expected = true;
+            var actual = RobotBoundedInCircle.IsRobotBounded(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
