db622874056232f04cda9bcbc672f997d20bcd04
user3301 <stan_gai@Hotmail.com>
2019-05-13 17:58:42 +1000

    weekly contest 136 (q2, greedy)

---
+++ leetcodesln/FlowerPlantingWithNoAdjacent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class FlowerPlantingWithNoAdjacent
+    {
+        public int[] GardenNoAdj(int N, int[][] paths)
+        {
+            int[] ans = new int[N];
+
+            List<int>[] g = new List<int>[N];
+            for (int i = 0; i < N; i++)
+            {
+                g[i] = new List<int>();
+            }
+
+            foreach (var path in paths)
+            {
+                g[path[0] - 1].Add(path[1] - 1);
+                g[path[1] - 1].Add(path[0] - 1);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                bool[] used = new bool[5];
+                foreach (var j in g[i]) used[ans[j]] = true;
+                for (int j = 1; j <= 4; j++)
+                {
+                    if (!used[j]) ans[i] = j;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/FlowerPlantingWithNoAdjacentUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using leetcodesln;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FlowerPlantingWithNoAdjacentUnitTest
+    {
+        public FlowerPlantingWithNoAdjacent FlowerPlantingWithNoAdjacent { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            FlowerPlantingWithNoAdjacent = new FlowerPlantingWithNoAdjacent();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            int N = 3;
+            int[][] paths = new int[3][];
+            paths[0] = new int[] { 1, 2 };
+            paths[1] = new int[] { 2, 3 };
+            paths[2] = new int[] { 3, 1 };
+            var expected = new int[] { 1, 2, 3 };
+            var actual = FlowerPlantingWithNoAdjacent.GardenNoAdj(N, paths);
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+    }
+}
