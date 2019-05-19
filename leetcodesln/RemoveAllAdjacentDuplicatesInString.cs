91d92fa51bacbed140a9c4c0d6ad3484b17e5202
user3301 <stan_gai@Hotmail.com>
2019-05-19 13:55:21 +1000

    weekly contest 137(remove all adjacent duplicates in string using brute force)

---
+++ leetcodesln/RemoveAllAdjacentDuplicatesInString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class RemoveAllAdjacentDuplicatesInString
+    {
+        public string RemoveDuplicates(string S)
+        {
+            var before = S;
+            var after = RemoveProcess(S);
+            while(before != after)
+            {
+                before = after;
+                after = RemoveProcess(after);
+                
+            }
+            return after;
+        }
+
+        private string RemoveProcess(string s)
+        {
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if(s[i] == s[i+1])
+                {
+                    return s.Remove(i, 2);
+                }
+            }
+            return s;
+        }
+    }
+}
---
+++ leetcodesln_test/RemoveAllAdjacentDuplicatesInStringUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RemoveAllAdjacentDuplicatesInStringUnitTest
+    {
+        public RemoveAllAdjacentDuplicatesInString RemoveAllAdjacentDuplicatesInString { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            RemoveAllAdjacentDuplicatesInString = new RemoveAllAdjacentDuplicatesInString();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = "abbaca";
+            var expected = "ca";
+            var actual = RemoveAllAdjacentDuplicatesInString.RemoveDuplicates(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = "aaaa";
+            var expected = "";
+            var actual = RemoveAllAdjacentDuplicatesInString.RemoveDuplicates(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
