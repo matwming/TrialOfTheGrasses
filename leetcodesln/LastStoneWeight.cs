fb9fdcbb02d4aa71326ead0b419b8e6120c84024
user3301 <stan_gai@Hotmail.com>
2019-05-20 11:38:28 +1000

    :star: last stone weight(using sortedDictionary as priority queue alternative,Runtime: 108 ms, faster than 100.00%)

---
+++ leetcodesln/LastStoneWeight.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class LastStoneWeight
+    {
+
+        // keep stones sorted when new stones added using sortedDictionary
+        public int LastStoneWeightSln(int[] stones)
+        {
+            var sortedDictionary = new SortedDictionary<int, int>();
+
+            foreach (var stone in stones)
+            {
+                if (sortedDictionary.ContainsKey(stone)) sortedDictionary[stone]++;
+                else sortedDictionary.Add(stone, 1);
+            }
+
+            while(sortedDictionary.Count >= 2)
+            {
+                var large = sortedDictionary.Last();
+                sortedDictionary.Remove(large.Key);
+
+                // if current max stones are even number, they will destroy each other, otherwise we need take out 
+                if(large.Value % 2 == 1)
+                {
+                    var next = sortedDictionary.Last();
+                    if (next.Value == 1) sortedDictionary.Remove(next.Key);
+                    else sortedDictionary[next.Key]--;
+
+                    if (sortedDictionary.ContainsKey(large.Key - next.Key)) sortedDictionary[large.Key - next.Key]++;
+                    else sortedDictionary.Add(large.Key - next.Key, 1);
+                }
+            }
+            var remain = sortedDictionary.First();
+            return remain.Value%2==0?0:remain.Key;
+        }
+    }
+}
---
+++ leetcodesln_test/LastStoneWeightUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LastStoneWeightUnitTest
+    {
+        public LastStoneWeight LastStoneWeight { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            LastStoneWeight = new LastStoneWeight();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 2, 2 };
+            var expected = 0;
+            var actual = LastStoneWeight.LastStoneWeightSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
