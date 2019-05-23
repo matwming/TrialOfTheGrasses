18f7e7ebe4eab487253804587adade5f60ed7be6
user3301 <stan_gai@Hotmail.com>
2019-05-24 09:34:39 +1000

    reveal cards in increasing order(using queue, Runtime: 264 ms, faster than 44.66%)

---
+++ leetcodesln/RevealCardsInIncreasingOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class RevealCardsInIncreasingOrder
+    {
+        public int[] DeckRevealedIncreasing(int[] deck)
+        {
+            Array.Sort(deck);
+            int[] ans = new int[deck.Length];
+            var q = new Queue<int>();
+            for (int i = 0; i < deck.Length; i++)
+            {
+                q.Enqueue(i);
+            }
+
+            for (int i = 0; i < ans.Length; i++)
+            {
+                ans[q.Peek()] = deck[i];
+                q.Dequeue();
+                if(q.Count!=0)
+                {
+                    var next = q.Dequeue();
+                    q.Enqueue(next);
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/RevealCardsInIncreasingOrderUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RevealCardsInIncreasingOrderUnitTest
+    {
+        public RevealCardsInIncreasingOrder RevealCardsInIncreasingOrder { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            RevealCardsInIncreasingOrder = new RevealCardsInIncreasingOrder();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 17, 13, 11, 2, 3, 5, 7 };
+            var expected = new int[] { 2, 13, 3, 11, 5, 17, 7 };
+            var actual = RevealCardsInIncreasingOrder.DeckRevealedIncreasing(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+    }
+}
