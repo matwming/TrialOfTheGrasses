00eedf4df8306c7adec11266d85d7695767e2208
user3301 <stan_gai@Hotmail.com>
2019-05-24 17:42:49 +1000

    find all duplicates in an array(in-place o(n), Runtime: 336 ms, faster than 40.50%)

---
+++ leetcodesln/FindAllDuplicatesInAnArray.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class FindAllDuplicatesInAnArray
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            var ans = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index;
+                if (nums[i] > nums.Length)
+                {
+                    index = nums[i] % nums.Length == 0 ? nums.Length - 1 : nums[i] % nums.Length - 1;
+                    nums[index] += nums.Length;
+
+                }
+                else
+                {
+                    index = nums[i] - 1;
+                    nums[index] += nums.Length;
+                }
+                if (nums[index] > nums.Length * 2) ans.Add(index + 1);
+            }
+            return ans;
+        }
+    }
+}
