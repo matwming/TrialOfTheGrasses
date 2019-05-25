7fee1507b4b87e4dfb8d9b630ee89bb1b3cd8df5
user3301 <stan_gai@Hotmail.com>
2019-05-25 21:20:51 +1000

    coin change(dp, Runtime: 112 ms, faster than 93.90% )

---
+++ leetcodesln/CoinChange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class CoinChange
+    {
+        public int Change(int amount, int[] coins)
+        {
+            int max = 0;
+            for (int i = 0; i < coins.Length; i++)
+            {
+                max = Math.Max(max, amount / coins[i]+1);
+            }
+            int[] dp = new int[amount + 1];
+            Array.Fill(dp, max+10);
+
+            dp[0] = 0;
+            for (int i = 1; i < amount + 1; i++)
+            {
+                int min = dp[i];
+                for (int j = 0; j < coins.Length; j++)
+                {
+                    if (i - coins[j] >= 0)
+                    {
+                        min = Math.Min(dp[i - coins[j]] + 1, min);
+                    }
+                }
+                dp[i] = min;
+            }
+            return dp[amount] == (max+10) ? -1 : dp[amount];
+        }
+    }
+}
---
+++ leetcodesln_test/CoinChangeUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class CoinChangeUnitTest
+    {
+        public CoinChange CoinChange { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            CoinChange = new CoinChange();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var amount = 11;
+            var coins = new int[] { 1, 2, 5 };
+            var expected = 3;
+            var actual = CoinChange.Change(amount, coins);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var amount = 3;
+            var coins = new int[] { 2};
+            var expected = -1;
+            var actual = CoinChange.Change(amount, coins);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
