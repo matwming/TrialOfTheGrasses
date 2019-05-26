27253796e589e906b1216beb2c7e57287caf4b3a
user3301 <stan_gai@Hotmail.com>
2019-05-26 22:58:11 +1000

    height checker(sort and compare,Runtime: 84 ms, faster than 100.00%)

---
+++ leetcodesln/HeightChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class HeightChecker
+    {
+        public int HeightCheckerSln(int[] heights)
+        {
+            int ans = 0;
+            var copy = new int[heights.Length];
+            for (int i = 0; i < copy.Length; i++)
+            {
+                copy[i] = heights[i];
+            }
+            Array.Sort(copy);
+            for (int i = 0; i < heights.Length; i++) ans += copy[i] - heights[i] == 0 ? 0 : 1;
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/HeightCheckerUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class HeightCheckerUnitTest
+    {
+        public HeightChecker HeightChecker { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            HeightChecker = new HeightChecker();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 1, 4, 2, 1, 3 };
+            var expected = 3;
+            var actual = HeightChecker.HeightCheckerSln(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
