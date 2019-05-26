ff7ff72a2888349a744ec4402fc0b177a5820416
user3301 <stan_gai@Hotmail.com>
2019-05-27 09:47:21 +1000

    grumpy bookstore owner(sliding window, Runtime: 896 ms, faster than 100.00% )

---
+++ leetcodesln/GrumpyBookstoreOwner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class GrumpyBookstoreOwner
+    {
+        public int MaxSatisfied(int[] customers, int[] grumpy, int X)
+        {
+            
+            int original = 0;
+            for (int i = 0; i < customers.Length; i++)
+            {
+                original += grumpy[i] == 1 ? 0 : customers[i];
+            }
+            int maxSatisfied = original;
+            for (int i = 0; i <= customers.Length -X; i++)
+            {
+                var current = original;
+                for (int j = 0; j < X; j++)
+                {
+                    if (grumpy[i + j] != 0) current += customers[i + j]; 
+                }
+                maxSatisfied = maxSatisfied > current ? maxSatisfied : current;
+            }
+            return maxSatisfied;
+        }
+    }
+}
---
+++ leetcodesln_test/GrumpyBookstoreOwnerUnitTest.cs
@@ -0,0 +1,54 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class GrumpyBookstoreOwnerUnitTest
+    {
+        public GrumpyBookstoreOwner GrumpyBookstoreOwner { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            GrumpyBookstoreOwner = new GrumpyBookstoreOwner();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var customers = new int[] { 1, 0, 1, 2, 1, 1, 7, 5 };
+            var grumpy = new int[] { 0, 1, 0, 1, 0, 1, 0, 1 };
+            var X = 3;
+            var expected = 16;
+            var actual = GrumpyBookstoreOwner.MaxSatisfied(customers, grumpy, X);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var customers = new int[] { 1 };
+            var grumpy = new int[] { 0 };
+            var X = 1;
+            var expected = 1;
+            var actual = GrumpyBookstoreOwner.MaxSatisfied(customers, grumpy, X);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var customers = new int[] { 3 };
+            var grumpy = new int[] { 1 };
+            var X = 1;
+            var expected = 3;
+            var actual = GrumpyBookstoreOwner.MaxSatisfied(customers, grumpy, X);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
