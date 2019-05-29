f0b463b6aed65b2290a7996d4906cd20e9302452
user3301 <stan_gai@Hotmail.com>
2019-05-29 10:20:52 +1000

    wip

---
+++ leetcodesln/DistantBarcodes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class DistantBarcodes
+    {
+        public int[] RearrangeBarcodes(int[] barcodes)
+        {
+            for (int i = 0; i < barcodes.Length; i++)
+            {
+                if (i - 1 >= 0 && barcodes[i] != barcodes[i - 1] || i - 1 < 0) continue;
+                else if(i == barcodes.Length -1)
+                {
+                    for (int j = i - 1; j >= 0; j--)
+                    {
+                        if (barcodes[i] == barcodes[j]) i = j;
+                        else if(barcodes[j]!=barcodes[i])
+                        {
+                            var temp = barcodes[j];
+                            barcodes[j] = barcodes[i];
+                            barcodes[i] = temp;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int j = i+1; j < barcodes.Length; j++)
+                    {
+                        if(barcodes[j]!= barcodes[i])
+                        {
+                            var temp = barcodes[j];
+                            barcodes[j] = barcodes[i];
+                            barcodes[i] = temp;
+                            break;
+                        }
+                    }
+                }
+            }
+            return barcodes;
+        }
+    }
+}
---
+++ leetcodesln_test/DistantBarcodesUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DistantBarcodesUnitTest
+    {
+        public DistantBarcodes DistantBarcodes { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DistantBarcodes = new DistantBarcodes();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 2, 1, 1 };
+            var expected = new int[] { 1, 2, 1 };
+            var actual = DistantBarcodes.RearrangeBarcodes(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 4,3,8,4,4,4,8,3,3,3 };
+            var actual = DistantBarcodes.RearrangeBarcodes(input);
+            for (int i = 1; i < actual.Length; i++)
+            {
+                Assert.IsTrue(actual[i - 1] != actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 1,1,1,1,2,2,3,3 };
+            var actual = DistantBarcodes.RearrangeBarcodes(input);
+            for (int i = 1; i < actual.Length; i++)
+            {
+                Assert.IsTrue(actual[i - 1] != actual[i]);
+            }
+        }
+
+    }
+}
