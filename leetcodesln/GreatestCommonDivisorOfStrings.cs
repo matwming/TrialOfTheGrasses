940881e2acf1507caf79449940a0a2653f5868db
user3301 <stan_gai@Hotmail.com>
2019-06-03 12:25:29 +1000

    :star: greatest common divisor of strings (recursive, Runtime: 88 ms, faster than 100.00%)

---
+++ leetcodesln/GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcodesln
+{
+    public class GreatestCommonDivisorOfStrings
+    {
+        //recursive
+        public string GcdOfStrings(string str1, string str2)
+        {
+            if (str1.Length < str2.Length) return GcdOfStrings(str2, str1);
+
+            else if (str1.Substring(0, str2.Length) != str2) return string.Empty;
+            else if (string.IsNullOrEmpty(str2)) return str1;
+            else return GcdOfStrings(str1.Substring(str2.Length), str2);
+        }
+    }
+}
---
+++ leetcodesln_test/GreatestCommonDivisorOfStringsUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class GreatestCommonDivisorOfStringsUnitTest
+    {
+        public GreatestCommonDivisorOfStrings GreatestCommonDivisorOfStrings { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            GreatestCommonDivisorOfStrings = new GreatestCommonDivisorOfStrings();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var str1 = "ABCABC";
+            var str2 = "ABC";
+            var expected = "ABC";
+            var actual = GreatestCommonDivisorOfStrings.GcdOfStrings(str1, str2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var str1 = "ABABAB";
+            var str2 = "ABAB";
+            var expected = "AB";
+            var actual = GreatestCommonDivisorOfStrings.GcdOfStrings(str1, str2);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
