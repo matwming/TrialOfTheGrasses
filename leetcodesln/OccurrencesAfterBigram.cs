40d4e4d690246d5143b636837b8f94f26e3481ed
user3301 <stan_gai@Hotmail.com>
2019-06-10 23:04:00 +1000

    wip

---
+++ leetcodesln/OccurrencesAfterBigram.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcodesln
+{
+    public class OccurrencesAfterBigram
+    {
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            var ans = new List<string>();
+            List<string> words = text.Split(' ').ToList();
+
+            //TODO
+            return ans.ToArray();
+        }
+    }
+}
