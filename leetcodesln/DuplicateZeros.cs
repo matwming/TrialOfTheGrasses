411a37f38071faed7399fbfbfb3b77f42f699184
user3301 <stan_gai@Hotmail.com>
2019-06-17 11:11:09 +1000

    duplicate zeros (Runtime: 276 ms, faster than 100.00%)

---
+++ leetcodesln/DuplicateZeros.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class DuplicateZeros
+    {
+        public void DuplicateZerosSln(int[] arr)
+        {
+            var q = new Queue<int>(arr);
+            int i = 0;
+            while (q.Count != 0 && i < arr.Length)
+            {
+                var current = q.Peek();
+                if (current == 0)
+                {
+                    arr[i++] = 0;
+                    q.Dequeue();
+                    if (i >= arr.Length) return;
+                    arr[i++] = 0;
+                }
+                else
+                {
+                    arr[i++] = current;
+                    q.Dequeue();
+                }
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/DuplicateZerosUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DuplicateZerosUnitTest
+    {
+        public DuplicateZeros DuplicateZeros { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DuplicateZeros = new DuplicateZeros();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+            var expected = new int[] { 1, 0, 0, 2, 3, 0, 0, 4 };
+            DuplicateZeros.DuplicateZerosSln(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], input[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 0, 0, 0 };
+            var expected = new int[] { 0, 0, 0 };
+            DuplicateZeros.DuplicateZerosSln(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], input[i]);
+            }
+        }
+    }
+}
