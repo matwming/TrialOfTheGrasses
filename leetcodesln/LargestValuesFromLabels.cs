e5ac627c503bfce01bda06d434ce3deb2b868884
user3301 <stan_gai@Hotmail.com>
2019-06-18 22:27:28 +1000

    wip

---
+++ leetcodesln/LargestValuesFromLabels.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class LargestValuesFromLabels
+    {
+        public int LargestValsFromLabels(int[] values, int[] labels, int num_wanted, int use_limit)
+        {
+            var usedLabels = new Dictionary<int, int>();
+
+            var bucket = new SortedDictionary<int, int>();
+
+            foreach (var value in values)
+            {
+                if (bucket.ContainsKey(value)) bucket[value]++;
+            }
+            return -1; // TODO wip
+        }
+    }
+}
