792209225b6405270f887a693f27e87684b7eadf
user3301 <stan_gai@Hotmail.com>
2019-06-19 11:40:52 +1000

    largest values from lables(sort in sync, Runtime: 1016 ms, faster than 7.32%)

--- leetcodesln/LargestValuesFromLabels.cs
+++ leetcodesln/LargestValuesFromLabels.cs
@@ -6,15 +6,56 @@ namespace leetcodesln
     {
         public int LargestValsFromLabels(int[] values, int[] labels, int num_wanted, int use_limit)
         {
-            var usedLabels = new Dictionary<int, int>();
+            var ans = 0;
+            var labelsOccurrenceTable = new Dictionary<int, int>();  // label, occurrence
+            SelectSortValuesAndLabels(ref values, ref labels);
 
-            var bucket = new SortedDictionary<int, int>();
-
-            foreach (var value in values)
+            int idx = 0;
+            while (num_wanted != 0)
             {
-                if (bucket.ContainsKey(value)) bucket[value]++;
+                if (labelsOccurrenceTable.ContainsKey(labels[idx]) && labelsOccurrenceTable[labels[idx]] < use_limit)
+                {
+                    ans += values[idx];
+                    labelsOccurrenceTable[labels[idx]]++;
+                    num_wanted--;
+                    idx++;
+                }
+                else if (!labelsOccurrenceTable.ContainsKey(labels[idx]))
+                {
+                    ans += values[idx];
+                    labelsOccurrenceTable.Add(labels[idx], 1);
+                    num_wanted--;
+                    idx++;
+                }
+                else idx++;
+
+                if (idx >= values.Length) break;
             }
-            return -1; // TODO wip
+
+
+            return ans;
+        }
+
+        private void SelectSortValuesAndLabels(ref int[] values, ref int[] labels)
+        {
+            int n = values.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int max_idx = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (values[j] > values[max_idx]) max_idx = j;
+                }
+
+                int tempValue = values[max_idx];
+                int tempLabel = labels[max_idx];
+                values[max_idx] = values[i];
+                labels[max_idx] = labels[i];
+                values[i] = tempValue;
+                labels[i] = tempLabel;
+            }
         }
     }
 }
---
+++ leetcodesln_test/LargestValuesFromLabelsUnitTest.cs
@@ -0,0 +1,66 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LargestValuesFromLabelsUnitTest
+    {
+        public LargestValuesFromLabels LargestValuesFromLabels { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            LargestValuesFromLabels = new LargestValuesFromLabels();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var values = new int[] { 5, 4, 3, 2, 1 };
+            var labels = new int[] { 1, 1, 2, 2, 3 };
+            var num_wanted = 3;
+            var use_limit = 1;
+            var expected = 9;
+            var actual = LargestValuesFromLabels.LargestValsFromLabels(values, labels, num_wanted: num_wanted, use_limit: use_limit);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var values = new int[] { 5, 4, 3, 2, 1 };
+            var labels = new int[] { 1, 3, 3, 3, 2 };
+            var num_wanted = 3;
+            var use_limit = 2;
+            var expected = 12;
+            var actual = LargestValuesFromLabels.LargestValsFromLabels(values, labels, num_wanted: num_wanted, use_limit: use_limit);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var values = new int[] { 9, 8, 8, 7, 6 };
+            var labels = new int[] { 0, 0, 0, 1, 1 };
+            var num_wanted = 3;
+            var use_limit = 1;
+            var expected = 16;
+            var actual = LargestValuesFromLabels.LargestValsFromLabels(values, labels, num_wanted: num_wanted, use_limit: use_limit);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var values = new int[] { 9, 8, 8, 7, 6 };
+            var labels = new int[] { 0, 0, 0, 1, 1 };
+            var num_wanted = 3;
+            var use_limit = 2;
+            var expected = 24;
+            var actual = LargestValuesFromLabels.LargestValsFromLabels(values, labels, num_wanted: num_wanted, use_limit: use_limit);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
