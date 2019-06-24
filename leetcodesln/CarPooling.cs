5103c619aa539ede7917b53c9b62d5c8874e3b0d
user3301 <stan_gai@Hotmail.com>
2019-06-25 09:25:16 +1000

    car pooling (Runtime: 96 ms, faster than 100.00%)

---
+++ leetcodesln/CarPooling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class CarPooling
+    {
+        public bool CarPoolingSln(int[][] trips, int capacity)
+        {
+            int[] hopOnAndOff = new int[1000];
+
+            for (int i = 0; i < trips.Length; i++)
+            {
+                var passangers = trips[i][0];
+                hopOnAndOff[trips[i][1]] += passangers;
+                hopOnAndOff[trips[i][2]] -= passangers;
+            }
+
+            var currentCapacity = 0;
+            for (int i = 0; i < hopOnAndOff.Length; i++)
+            {
+                currentCapacity += hopOnAndOff[i];
+                if (currentCapacity > capacity) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/CarPoolingUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class CarPoolingUnitTest
+    {
+        public CarPooling CarPooling { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            CarPooling = new CarPooling();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var trips = new int[][] { new int[] { 2, 1, 5 }, new int[] { 3, 3, 7 } };
+            var capacity = 4;
+            var expected = false;
+            var actual = CarPooling.CarPoolingSln(trips, capacity);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var trips = new int[][] { new int[] { 2, 1, 5 }, new int[] { 3, 3, 7 } };
+            var capacity = 5;
+            var expected = true;
+            var actual = CarPooling.CarPoolingSln(trips, capacity);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var trips = new int[][] { new int[] { 2, 1, 5 }, new int[] { 3, 5, 7 } };
+            var capacity = 3;
+            var expected = true;
+            var actual = CarPooling.CarPoolingSln(trips, capacity);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var trips = new int[][] { new int[] { 3,2,7 }, new int[] { 3,7,9 }, new int[] { 8,3,9} };
+            var capacity = 11;
+            var expected = true;
+            var actual = CarPooling.CarPoolingSln(trips, capacity);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
