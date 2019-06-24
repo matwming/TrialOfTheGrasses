1aee9e2d61d4a549f60bf01588e6b931682a4337
user3301 <stan_gai@Hotmail.com>
2019-06-24 10:35:00 +1000

    :x: insufficient memory solution

---
+++ leetcodesln/StatisticsFromALargeSample.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class StatisticsFromALargeSample
+    {
+        public double[] SampleStats(int[] count)
+        {
+            double? min = default;
+            double? max = default;
+            double? mode = default;
+            int currentMostAppearTimes = 0;
+            int totalSum = 0;
+
+            int totalCount = 0;
+            int mIdxL = 0;
+            int mIdxR = 0;
+
+            var list = new List<int>();
+
+            for (int i = 0; i < 255; i++)
+            {
+                if (count[i] != 0)
+                {
+                    for (int j = 0; j < count[i]; j++)
+                    {
+                        list.Add(i);
+                    }
+                    min = min == null ? i : (min < i ? min : i);
+                    max = max == null ? i : (max > i ? max : i);
+                    totalCount += count[i];
+                    totalSum += i * count[i];
+                    if (currentMostAppearTimes < count[i])
+                    {
+                        mode = i;
+                        currentMostAppearTimes = count[i];
+                    }
+                    if (totalCount % 2 == 0)
+                    {
+                        mIdxL = totalCount / 2 - 1;
+                        mIdxR = totalCount / 2;
+                    }
+                    else
+                    {
+                        mIdxL = mIdxR = totalCount / 2;
+                    }
+
+                }
+            }
+
+            double? mean = Math.Round((double)totalSum / totalCount, 5);
+            double? median = Math.Round((double)(list[mIdxL] + list[mIdxR]) / 2, 5);
+
+            return new double[] { min.Value, max.Value, mean.Value, median.Value, mode.Value };
+        }
+    }
+}
---
+++ leetcodesln_test/StatisticsFromALargeSampleUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class StatisticsFromALargeSampleUnitTest
+    {
+        public StatisticsFromALargeSample StatisticsFromALargeSample { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            StatisticsFromALargeSample = new StatisticsFromALargeSample();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var count = new int[] { 0, 1, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            var expected = new double[] { 1.00000, 3.00000, 2.37500, 2.50000, 3.00000 };
+            var actual = StatisticsFromALargeSample.SampleStats(count);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var count = new int[] { 0, 4, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            var expected = new double[] { 1.00000, 4.00000, 2.18182, 2.00000, 1.00000 };
+            var actual = StatisticsFromALargeSample.SampleStats(count);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+    }
+}
