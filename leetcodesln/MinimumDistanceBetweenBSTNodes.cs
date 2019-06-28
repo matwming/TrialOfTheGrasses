52268835c9349ca67964f5bee903675de733bfff
user3301 <stan_gai@Hotmail.com>
2019-06-28 22:08:29 +1000

    minimum distance between bst nodes(in order Runtime: 96 ms, faster than 47.83%)

---
+++ leetcodesln/MinimumDistanceBetweenBSTNodes.cs
@@ -0,0 +1,27 @@
+﻿using leetcodesln.common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class MinimumDistanceBetweenBSTNodes
+    {
+        private int? min = default;
+
+        private TreeNode pre = null;
+
+        public int MinDiffInBST(TreeNode root)
+        {
+            if (root.left != null) MinDiffInBST(root.left);
+            if (pre != null)
+            {
+                min = min == null ? root.val - pre.val : Math.Min(min.Value, root.val - pre.val);
+                
+            }
+            pre = root;
+            if (root.right != null) MinDiffInBST(root.right);
+            return min.GetValueOrDefault();
+        }
+    }
+}
---
+++ leetcodesln_test/MinimumDistanceBetweenBSTNodesUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MinimumDistanceBetweenBSTNodesUnitTest
+    {
+        public MinimumDistanceBetweenBSTNodes MinimumDistanceBetweenBSTNodes { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MinimumDistanceBetweenBSTNodes = new MinimumDistanceBetweenBSTNodes();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var root = new TreeNode(4)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(1),
+                    right = new TreeNode(3)
+                },
+                right = new TreeNode(6)
+            };
+
+            var expected = 1;
+            var actual = MinimumDistanceBetweenBSTNodes.MinDiffInBST(root);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
