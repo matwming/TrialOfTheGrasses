9602e9ce71999493ae8d539d8470d671dc19d29c
user3301 <stan_gai@Hotmail.com>
2019-06-29 20:19:26 +1000

    wip

---
+++ leetcodesln/ThreeSum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class ThreeSum
+    {
+        public IList<IList<int>> ThreeSumSln(int[] nums)
+        {
+            var ans = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var current = nums[i];
+                var complement = -current;
+
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (j == i) continue;
+
+                    var cur = nums[j];
+                    var comp = complement - cur;
+                    for (int s = 0; s < nums.Length; s++)
+                    {
+                        if (nums[s] == comp && s != j && s != i)
+                        {
+                            ans.Add(new List<int> { nums[i], nums[j], nums[s] });
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/ThreeSumUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class ThreeSumUnitTest
+    {
+        public ThreeSum ThreeSum { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ThreeSum = new ThreeSum();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var nums = new int[] { -1, 0, 1, 2, -1, -4 };
+            var expected = new List<IList<int>>() { new List<int>() { -1, 0, 1 }, new List<int>() { -1, -1, 2 } };
+            var actual = ThreeSum.ThreeSumSln(nums);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                for (int j = 0; j < expected[i].Count; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+
+    }
+}
