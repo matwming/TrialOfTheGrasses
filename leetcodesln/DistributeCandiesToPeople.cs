cd88ea4ec430e168644c5585ae6a745439fe3872
user3301 <stan_gai@Hotmail.com>
2019-07-01 09:42:50 +1000

    distribute candies to people (Brute force, Runtime: 224 ms, faster than 100.00%)

---
+++ leetcodesln/DistributeCandiesToPeople.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcodesln
+{
+    public class DistributeCandiesToPeople
+    {
+        public int[] DistributeCandies(int candies, int num_people)
+        {
+            var ans = new int[num_people];
+
+            int i = 1;
+            while (candies > 0)
+            {
+                if (candies - i >= 0)
+                {
+                    ans[i % num_people == 0? num_people-1 : i%num_people -1] += i;
+                    candies -= i;
+                    i++;
+                }
+                else
+                {
+                    ans[i % num_people == 0 ? num_people-1 : i % num_people - 1] += candies;
+                    candies = 0;
+                }
+
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/DistributeCandiesToPeopleUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DistributeCandiesToPeopleUnitTest
+    {
+        public DistributeCandiesToPeople DistributeCandiesToPeople { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DistributeCandiesToPeople = new DistributeCandiesToPeople();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var candies = 7;
+            var num_people = 4;
+            var expected = new[] { 1, 2, 3, 1 };
+            var actual = DistributeCandiesToPeople.DistributeCandies(candies, num_people);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var candies = 10;
+            var num_people = 3;
+            var expected = new[] { 5,2,3 };
+            var actual = DistributeCandiesToPeople.DistributeCandies(candies, num_people);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
