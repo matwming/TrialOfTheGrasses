74776d5c872a146a6d51b29951e163df10f168b4
user3301 <stan_gai@Hotmail.com>
2019-07-05 11:00:24 +1000

    :star::star:path in zigzag babelled binary tree(Runtime: 208 ms, faster than 100.00% )

---
+++ leetcodesln/PathInZigzagLabelledBinaryTree.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class PathInZigzagLabelledBinaryTree
+    {
+        public IList<int> PathInZigZagTree(int label)
+        {
+            var row = 0;
+            var ans = new List<int>() { label };
+
+            while (label >= 1 << row + 1) row++;
+
+            while (row > 0)
+            {
+                var preivous = (1 << row) + ((1 << row - 1) - 1) - label / 2;
+                ans.Insert(0, preivous);
+                row--;
+                label = preivous;
+
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/PathInZigzagLabelledBinaryTreeUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PathInZigzagLabelledBinaryTreeUnitTest
+    {
+        public PathInZigzagLabelledBinaryTree PathInZigzagLabelledBinaryTree { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            PathInZigzagLabelledBinaryTree = new PathInZigzagLabelledBinaryTree();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var label = 14;
+            var expected = new List<int>() { 1, 3, 4, 14 };
+            var actual = PathInZigzagLabelledBinaryTree.PathInZigZagTree(label);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+    }
+}
