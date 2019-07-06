89dff4750df2518321971552c08560e434de8cac
user3301 <stan_gai@Hotmail.com>
2019-07-06 23:52:48 +1000

    wip

---
+++ leetcodesln/FillingBookcaseShelves.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class FillingBookcaseShelves
+    {
+        public int MinHeightShelves(int[][] books, int shelf_width)
+        {
+            var minHeight = 0;
+            // store current optimised height
+            var dp = new int[books.Length + 1];
+            dp[0] = 0;
+
+            for (int i = 1; i <= books.Length; ++i)
+            {
+                int width = books[i - 1][0];
+                int height = books[i - 1][1];
+
+                for (int j = i - 1; j >= 0 && width + books[j][0] <= shelf_width; --j)
+                {
+                    width += books[j][0];
+                    var max_height = Math.Max(height, books[j][1]);
+                    dp[i] = Math.Min(dp[i], dp[j] + max_height);
+
+                }
+
+            }
+            //wip
+            return minHeight;
+        }
+    }
+}
