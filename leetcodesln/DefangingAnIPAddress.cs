d68da9d55175f83e199388b23c6fc81e8fba35f6
user3301 <stan_gai@Hotmail.com>
2019-07-08 22:32:52 +1000

    :zzz:defanging an ip address(using queue, Runtime: 104 ms, faster than 9.72%)

---
+++ leetcodesln/DefangingAnIPAddress.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class DefangingAnIPAddress
+    {
+        public string DefangIPaddr(string address)
+        {
+            var q = new Queue<char>(address);
+
+            var ans = new StringBuilder();
+
+            while (q.Count != 0)
+            {
+                if (q.Peek() != '.')
+                {
+                    ans.Append(q.Dequeue());
+                }
+                else
+                {
+                    ans.Append("[");
+                    ans.Append(q.Dequeue());
+                    ans.Append("]");
+                }
+            }
+            return ans.ToString();
+        }
+    }
+}
---
+++ leetcodesln_test/DefangingAnIPAddressUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DefangingAnIPAddressUnitTest
+    {
+        public DefangingAnIPAddress DefangingAnIPAddress { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DefangingAnIPAddress = new DefangingAnIPAddress();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var address = "1.1.1.1";
+            var expected = "1[.]1[.]1[.]1";
+            var actual = DefangingAnIPAddress.DefangIPaddr(address);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var address = "255.100.50.0";
+            var expected = "255[.]100[.]50[.]0";
+            var actual = DefangingAnIPAddress.DefangIPaddr(address);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
