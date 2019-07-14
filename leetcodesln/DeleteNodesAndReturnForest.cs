38194034a6f76ea40e025059e8f18c628e39413b
user3301 <stan_gai@Hotmail.com>
2019-07-14 22:52:45 +1000

    wip

---
+++ leetcodesln/DeleteNodesAndReturnForest.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using leetcodesln.common;
+
+namespace leetcodesln
+{
+    public class DeleteNodesAndReturnForest
+    {
+        public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete)
+        {
+            var ans = new List<TreeNode>();
+            var q = new Queue<TreeNode>();
+
+            return ans;
+        }
+    }
+}
