9b08f27e9d3155613576f71f374a512d5ffe34f0
user3301 <stan_gai@Hotmail.com>
2019-07-17 09:35:08 +1000

    wip

---
+++ leetcodesln/LowestCommonAncestorOfDeepestLeaves.cs
@@ -0,0 +1,59 @@
+﻿using leetcodesln.common;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class LowestCommonAncestorOfDeepestLeaves
+    {
+        public TreeNode LcaDeepestLeaves(TreeNode root)
+        {
+            TreeNode ans = null;
+            var q = new Queue<NodeWDepth>();
+            var maxDepth = 0;
+
+            if (root == null) { return null; }
+
+            q.Enqueue(new NodeWDepth(root, 1, null));
+
+            while (q.Count != 0)
+            {
+                var current = q.Dequeue();
+                if (current.node.left == null && current.node.right == null)
+                {
+                    if (current.depth > maxDepth)
+                    {
+                        ans = current.father ?? current.node;
+                    }
+                }
+                else
+                {
+                    if (current.node.left != null)
+                    {
+                        q.Enqueue(new NodeWDepth(current.node.left, current.depth + 1, current.node));
+                    }
+                    if (current.node.right != null)
+                    {
+                        q.Enqueue(new NodeWDepth(current.node.right, current.depth + 1, current.node));
+                    }
+                }
+                maxDepth++;
+
+            }
+
+            return ans;
+        }
+    }
+
+    struct NodeWDepth
+    {
+        public TreeNode node;
+        public int depth;
+        public TreeNode father;
+        public NodeWDepth(TreeNode node, int depth, TreeNode father)
+        {
+            this.node = node;
+            this.depth = depth;
+            this.father = father;
+        }
+    }
+}
---
+++ leetcodesln_test/LowestCommonAncestorOfDeepestLeavesUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetcodesln;
+using leetcodesln.common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LowestCommonAncestorOfDeepestLeavesUnitTest
+    {
+        public LowestCommonAncestorOfDeepestLeaves LowestCommonAncestorOfDeepestLeaves { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            LowestCommonAncestorOfDeepestLeaves = new LowestCommonAncestorOfDeepestLeaves();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var root = new TreeNode(2)
+            {
+                left = new TreeNode(1),
+                right = new TreeNode(3)
+            };
+
+            var expected = LowestCommonAncestorOfDeepestLeaves.LcaDeepestLeaves(root);
+        }
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var root = new TreeNode(2)
+            {
+                left = new TreeNode(1),
+                right = new TreeNode(3)
+            };
+
+            var expected = LowestCommonAncestorOfDeepestLeaves.LcaDeepestLeaves(root);
+        }
+    }
+}
