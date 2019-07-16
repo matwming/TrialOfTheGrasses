0466edcc64400acf31b3822b23b87a91e0e4588b
user3301 <stan_gai@Hotmail.com>
2019-07-16 10:08:57 +1000

    :star:delete nodes and return forest(recursion, Runtime: 288 ms, faster than 37.66%)

--- leetcodesln/DeleteNodesAndReturnForest.cs
+++ leetcodesln/DeleteNodesAndReturnForest.cs
@@ -1,17 +1,35 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using leetcodesln.common;
 
 namespace leetcodesln
 {
+    /// <summary>
+    ///  FIXME: no test cases
+    /// </summary>
     public class DeleteNodesAndReturnForest
     {
         public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete)
         {
             var ans = new List<TreeNode>();
-            var q = new Queue<TreeNode>();
 
+            Helper(root, true, to_delete, ref ans);
             return ans;
+        }
+
+        private TreeNode Helper(TreeNode node, bool isRoot, int[] to_delete, ref List<TreeNode> ans)
+        {
+            if (node == null) return null;
+            bool needDelete = to_delete.Contains(node.val);
+
+            if (isRoot && !needDelete)
+            {
+                ans.Add(node);
+            }
+            node.left = Helper(node.left, needDelete, to_delete, ref ans);
+            node.right = Helper(node.right, needDelete, to_delete, ref ans);
+            return needDelete ? null : node;
         }
     }
 }
