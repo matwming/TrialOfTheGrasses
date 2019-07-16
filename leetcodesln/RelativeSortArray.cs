6833f0bc16de4598f879befe1ada2abe2237c71f
user3301 <stan_gai@Hotmail.com>
2019-07-16 10:58:52 +1000

    relative sort array(sorted dictionary, Runtime: 268 ms, faster than 58.62%)

---
+++ leetcodesln/RelativeSortArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class RelativeSortArray
+    {
+        public int[] RelativeSortArraySln(int[] arr1, int[] arr2)
+        {
+            var ans = new int[arr1.Length];
+
+            var sortedDict = new SortedDictionary<int, int>();
+
+            foreach (var num in arr1)
+            {
+                if (sortedDict.ContainsKey(num))
+                {
+                    sortedDict[num]++;
+                }
+                else { sortedDict.Add(num, 1); }
+            }
+            var idx = 0;
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                var count = sortedDict[arr2[i]];
+                for (int j = 0; j < count; j++)
+                {
+                    ans[idx++] = arr2[i];
+                }
+                sortedDict.Remove(arr2[i]);
+            }
+            while(sortedDict.Count!=0)
+            { 
+                var kv = sortedDict.FirstOrDefault();
+                var count = kv.Value;
+                for (int j = 0; j < count; j++)
+                {
+                    ans[idx++] = kv.Key;
+                }
+                sortedDict.Remove(kv.Key);
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/RelativeSortArrayUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class RelativeSortArrayUnitTest
+    {
+        public RelativeSortArray RelativeSortArray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            RelativeSortArray = new RelativeSortArray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var arr1 = new[] { 2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19 };
+            var arr2 = new[] { 2, 1, 4, 3, 9, 6 };
+            var expected = new[] { 2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19 };
+            var actual = RelativeSortArray.RelativeSortArraySln(arr1, arr2);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(actual[i], expected[i]);
+            }
+        }
+
+    }
+}
