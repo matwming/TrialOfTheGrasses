31bcefee46eb451b8155835e9637cd261d2f77d5
user3301 <stan_gai@Hotmail.com>
2019-07-21 13:17:57 +1000

    weekly contest 146 (Number of equivalent domino pairs)

---
+++ leetcodesln/NumberOfEquivalentDominoPairs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcodesln
+{
+    public class NumberOfEquivalentDominoPairs
+    {
+        public int NumEquivDominoPairs(int[][] dominoes)
+        {
+            int ans = 0;
+            var dict = new Dictionary<int, List<int[]>>();
+
+            for (int i = 0; i < dominoes.Length; i++)
+            {
+                var domino = dominoes[i];
+                var sum = domino[0] + domino[1];
+                if (dict.ContainsKey(sum))
+                {
+                    foreach (var item in dict[sum].ToList())
+                    {
+                        if (item[0] == domino[0] || item[1] == domino[0])
+                        {
+                            ans++;
+                        }
+                    }
+                    dict[sum].Add(new[] { domino[0], domino[1] });
+
+                }
+                else
+                {
+                    dict.Add(sum, new List<int[]> { new[] { domino[0], domino[1] } });
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/NumberOfEquivalentDominoPairsUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class NumberOfEquivalentDominoPairsUnitTest
+    {
+        public NumberOfEquivalentDominoPairs NumberOfEquivalentDominoPairs { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            NumberOfEquivalentDominoPairs = new NumberOfEquivalentDominoPairs();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var dominoes = new int[4][];
+            dominoes[0] = new[] { 1, 2 };
+            dominoes[1] = new[] { 2, 1 };
+            dominoes[2] = new[] { 3, 4 };
+            dominoes[3] = new[] { 5, 6 };
+            var expected = 1;
+            var actual = NumberOfEquivalentDominoPairs.NumEquivDominoPairs(dominoes);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var dominoes = new int[4][];
+            dominoes[0] = new[] { 1, 2 };
+            dominoes[1] = new[] { 2, 1 };
+            dominoes[2] = new[] { 1, 2 };
+            dominoes[3] = new[] { 1, 2 };
+            var expected = 6;
+            var actual = NumberOfEquivalentDominoPairs.NumEquivDominoPairs(dominoes);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
