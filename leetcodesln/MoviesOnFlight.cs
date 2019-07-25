ae6e1941a8a244f01284b8751590e15ed1f35eb0
user3301 <stan_gai@Hotmail.com>
2019-07-25 10:48:07 +1000

    movies on flight (amazon online assessment question)

---
+++ leetcodesln/MoviesOnFlight.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcodesln
+{
+    /// <summary>
+    /// Amazon online assessment question 2019
+    /// </summary>
+    public class MoviesOnFlight
+    {
+        public int[] LongestMovies(int[] movies, int d)
+        {
+            d = d - 30;
+            var longest = 0;
+            var ans = new int[] { 0, 0 };
+            for (int i = 0; i < movies.Length; i++)
+            {
+                for (int j = i + 1; j < movies.Length; j++)
+                {
+                    var cur = movies[i] + movies[j];
+                    if (cur <= d)
+                    {
+                        if (longest >= cur)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            longest = cur;
+                            ans = new[] { movies[i], movies[j] };
+
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/MoviesOnFlightUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetcodesln;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MoviesOnFlightUnitTest
+    {
+        public MoviesOnFlight MoviesOnFlight { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MoviesOnFlight = new MoviesOnFlight();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var movies_duration = new[] { 90, 85, 75, 60, 120, 150, 125 };
+            var d = 250;
+
+            var actual = MoviesOnFlight.LongestMovies(movies_duration, d);
+            var expected = new[] { 90, 125 };
+            Assert.IsTrue(Helper<int>.HaveSameElementsForTwoCollections(actual, expected));
+        }
+
+    }
+}
