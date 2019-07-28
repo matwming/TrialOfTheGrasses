84943db368a292dd222b5c2f23f55e48b6106c48
user3301 <stan_gai@Hotmail.com>
2019-07-28 14:15:44 +1000

    n-th tribonacci number (dp, recursion with tle)

---
+++ leetcodesln/N_thTribonacciNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcodesln
+{
+    public class N_thTribonacciNumber
+    {
+        public int Tribonacci(int n)
+        {
+            var dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+            }
+            return dp[n];
+        }
+    }
+}
---
+++ leetcodesln_test/N_thTribonacciNumberUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class N_thTribonacciNumberUnitTest
+    {
+        public N_thTribonacciNumber N_thTribonacciNumber { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            N_thTribonacciNumber = new N_thTribonacciNumber();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = 4;
+            var expected = 4;
+            var actual = N_thTribonacciNumber.Tribonacci(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = 25;
+            var expected = 1389537;
+            var actual = N_thTribonacciNumber.Tribonacci(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
