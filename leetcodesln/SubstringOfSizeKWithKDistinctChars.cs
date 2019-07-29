4178a2f639cdebed20479532aa84089d48d73d71
user3301 <stan_gai@Hotmail.com>
2019-07-29 15:27:08 +1000

    substring of size k with k distinct chars (sliding window)

---
+++ leetcodesln/SubstringOfSizeKWithKDistinctChars.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class SubstringOfSizeKWithKDistinctChars
+    {
+        public ICollection<string> GetSubstrs(string str, int k)
+        {
+            var ans = new HashSet<string>();
+            var cur = new HashSet<char>();
+            int curIdx = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                while (cur.Contains(str[i]))
+                {
+                    cur.Remove(str[curIdx++]);
+                }
+
+                cur.Add(str[i]);
+                if (cur.Count == k)
+                {
+                    ans.Add(str.Substring(curIdx, k));
+                    cur.Remove(str[curIdx]);
+                    curIdx++;
+                }
+
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/SubstringOfSizeKWithKDistinctCharsUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class SubstringOfSizeKWithKDistinctCharsUnitTest
+    {
+        public SubstringOfSizeKWithKDistinctChars SubstringOfSizeKWithKDistinctChars { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            SubstringOfSizeKWithKDistinctChars = new SubstringOfSizeKWithKDistinctChars();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var str = "awaglknagawunagwkwagl";
+            var k = 4;
+            var actual = SubstringOfSizeKWithKDistinctChars.GetSubstrs(str, k);
+            var expected = new HashSet<string> { "wagl", "aglk", "glkn", "lkna", "knag", "gawu", "awun", "wuna", "unag", "nagw", "agwk", "kwag" };
+            Assert.AreEqual(actual.Count, expected.Count);
+            foreach (var stri in expected)
+            {
+                Assert.IsTrue(actual.Contains(stri));
+            }
+        }
+
+    }
+}
