821555973ccd2386faf01015f1c6400fe92258b7
user3301 <stan_gai@Hotmail.com>
2019-08-11 13:32:47 +1000

    day of the year

---
+++ leetcodesln/DayOfTheYear.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class DayOfTheYear
+    {
+        public int DayOfYear(string date)
+        {
+            int[] days = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int ans = 0;
+            string[] words = date.Split('-');
+
+            int year = Int32.Parse(words[0]);
+            int month = Int32.Parse(words[1]);
+            int day = Int32.Parse(words[2]);
+
+            bool isLeap = false;
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                isLeap = true;
+            }
+
+            if (month <= 1) return day;
+            if (month == 2)
+            {
+                ans += 31;
+                ans += day;
+                return ans;
+            }
+
+            for (int i = 0; i < month - 1; i++)
+            {
+                ans += days[i];
+            }
+            ans += day;
+            return isLeap ? ans + 1 : ans;
+        }
+
+    }
+}
---
+++ leetcodesln_test/DayOfTheYearUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DayOfTheYearUnitTest
+    {
+        public DayOfTheYear DayOfTheYear { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DayOfTheYear = new DayOfTheYear();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            string date = "2019-02-10";
+            var expected = 41;
+            var actual = DayOfTheYear.DayOfYear(date);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            string date = "2019-03-01";
+            var expected = 60;
+            var actual = DayOfTheYear.DayOfYear(date);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            string date = "2016-02-29";
+            var expected = 60;
+            var actual = DayOfTheYear.DayOfYear(date);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            string date = "1969-09-28";
+            var expected = 271;
+            var actual = DayOfTheYear.DayOfYear(date);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
