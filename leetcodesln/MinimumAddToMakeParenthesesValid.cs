5084f3b1cca790e13662a589077941df6e2088c7
user3301 <stan_gai@Hotmail.com>
2019-08-13 17:47:25 +1000

    leetcode 921(using stack, Runtime: 80 ms, faster than 48.42%)

---
+++ leetcodesln/MinimumAddToMakeParenthesesValid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class MinimumAddToMakeParenthesesValid
+    {
+        public int MinAddToMakeValid(string S)
+        {
+            var ans = 0;
+            var leftSet = new HashSet<char> { '(', '[', '{' };
+            var stack = new Stack<char>();
+            foreach (var chr in S)
+            {
+                if (leftSet.Contains(chr))
+                {
+                    switch (chr)
+                    {
+                        case '(':
+                            stack.Push(')');
+                            break;
+                        case '[':
+                            stack.Push(']');
+                            break;
+                        case '{':
+                            stack.Push('}');
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    if (stack.Count != 0 && chr == stack.Peek())
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        ++ans;
+                    }
+                }
+            }
+            return stack.Count == 0 ? ans : ans + stack.Count;
+        }
+    }
+}
---
+++ leetcodesln_test/MinimumAddToMakeParenthesesValidUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MinimumAddToMakeParenthesesValidUnitTest
+    {
+        public MinimumAddToMakeParenthesesValid MinimumAddToMakeParenthesesValid { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MinimumAddToMakeParenthesesValid = new MinimumAddToMakeParenthesesValid();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = "())";
+            var expected = 1;
+            var actual = MinimumAddToMakeParenthesesValid.MinAddToMakeValid(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = "(((";
+            var expected = 3;
+            var actual = MinimumAddToMakeParenthesesValid.MinAddToMakeValid(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = "()";
+            var expected = 0;
+            var actual = MinimumAddToMakeParenthesesValid.MinAddToMakeValid(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var input = "()))((";
+            var expected = 4;
+            var actual = MinimumAddToMakeParenthesesValid.MinAddToMakeValid(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
