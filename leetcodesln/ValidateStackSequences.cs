b99d7ae810bce3452bcc661c3996ca91c86866c9
user3301 <stan_gai@Hotmail.com>
2019-08-13 20:30:34 +1000

    leetcode 946 (Runtime: 108 ms, faster than 33.78%)

---
+++ leetcodesln/ValidateStackSequences.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class ValidateStackSequences
+    {
+        public bool ValidateStackSequencesSln(int[] pushed, int[] popped)
+        {
+            var stack = new Stack<int>();
+            var i = 0;
+            var j = 0;
+
+            while (j < popped.Length)
+            {
+                while (!stack.Contains(popped[j]) && i < pushed.Length)
+                {
+                    stack.Push(pushed[i++]);
+                }
+                if (stack.Peek() == popped[j])
+                {
+                    j++;
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return j == popped.Length;
+        }
+    }
+}
---
+++ leetcodesln_test/ValidateStackSequencesUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class ValidateStackSequencesUnitTest
+    {
+        public ValidateStackSequences ValidateStackSequences { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            ValidateStackSequences = new ValidateStackSequences();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var pushed = new[] { 1, 2, 3, 4, 5 };
+            var popped = new[] { 4, 5, 3, 2, 1 };
+            var expected = true;
+            var actual = ValidateStackSequences.ValidateStackSequencesSln(pushed, popped);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var pushed = new[] { 1, 2, 3, 4, 5 };
+            var popped = new[] { 4, 3, 5, 1, 2 };
+            var expected = false;
+            var actual = ValidateStackSequences.ValidateStackSequencesSln(pushed, popped);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
