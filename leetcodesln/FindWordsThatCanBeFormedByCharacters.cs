8f19269dae40ac1cbb7dac16b266781bbf91f339
user3301 <stan_gai@Hotmail.com>
2019-08-18 13:56:26 +1000

    contest 150- Find words that can be formed by characters (SOLVED, using dictionary)

---
+++ leetcodesln/FindWordsThatCanBeFormedByCharacters.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class FindWordsThatCanBeFormedByCharacters
+    {
+        public int CountCharacters(string[] words, string chars)
+        {
+            int ans = 0;
+            var dict = new Dictionary<char, int>();
+
+            foreach (var chr in chars)
+            {
+                if (dict.ContainsKey(chr)) { dict[chr]++; }
+                else { dict.Add(chr, 1); }
+            }
+
+            foreach (var word in words)
+            {
+                bool canForm = true;
+                var d = new Dictionary<char, int>();
+                foreach (var chr in word)
+                {
+                    if (d.ContainsKey(chr)) { d[chr]++; }
+                    else { d.Add(chr, 1); }
+                }
+
+                foreach (var dd in d)
+                {
+                    if (!dict.ContainsKey(dd.Key) || dict[dd.Key] < dd.Value)
+                    {
+                        canForm = false;
+                        break;
+                    }
+                }
+                if (canForm) ans += word.Length;
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/FindWordsThatCanBeFormedByCharactersUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class FindWordsThatCanBeFormedByCharactersUnitTest
+    {
+        public FindWordsThatCanBeFormedByCharacters FindWordsThatCanBeFormedByCharacters { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            FindWordsThatCanBeFormedByCharacters = new FindWordsThatCanBeFormedByCharacters();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var words = new[] { "cat", "bt", "hat", "tree" };
+            var chars = "atach";
+            var expected = 6;
+            var actual = FindWordsThatCanBeFormedByCharacters.CountCharacters(words, chars);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var words = new[] { "hello", "world", "leetcode" };
+            var chars = "welldonehoneyr";
+            var expected = 10;
+            var actual = FindWordsThatCanBeFormedByCharacters.CountCharacters(words, chars);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
