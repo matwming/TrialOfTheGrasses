388c68acbd56958421ca6999c025de4bbf3abb56
user3301 <stan_gai@Hotmail.com>
2019-08-27 15:46:37 +1000

    :white_check_mark:Runtime: 300 ms, faster than 52.50%

---
+++ leetcodesln/CompareStringsByFrequencyOfTheSmallestCharacter.cs
@@ -0,0 +1,63 @@
+﻿namespace leetcodesln
+{
+    public class CompareStringsByFrequencyOfTheSmallestCharacter
+    {
+        public int[] NumSmallerByFrequency(string[] queries, string[] words)
+        {
+            var ans = new int[queries.Length];
+            var frequencyArr = new int[words.Length];
+            var queriesFrequency = new int[queries.Length];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                GetMaxFrequency(ref frequencyArr, words[i], i);
+            }
+            for (int i = 0; i < queries.Length; i++)
+            {
+                GetMaxFrequency(ref queriesFrequency, queries[i], i);
+            }
+
+            for (int i = 0; i < queriesFrequency.Length; i++)
+            {
+                GetFrequency(queriesFrequency[i], frequencyArr, ans, i);
+            }
+            return ans;
+        }
+
+        private void GetFrequency(int qf, int[] frequencyArr, int[] ans, int pos)
+        {
+            var counter = 0;
+            for (int i = 0; i < frequencyArr.Length; i++)
+            {
+                if (qf < frequencyArr[i])
+                {
+                    counter++;
+                }
+            }
+            ans[pos] = counter;
+        }
+
+        private void GetMaxFrequency(ref int[] f, string word, int pos)
+        {
+            var wordFrequency = new int[26];
+            var curMax = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                wordFrequency[word[i] - 'a']++;
+                if (curMax < wordFrequency[word[i] - 'a'])
+                {
+                    curMax = wordFrequency[word[i] - 'a'];
+                }
+            }
+
+            for (int i = 0; i < wordFrequency.Length; i++)
+            {
+                if (wordFrequency[i] != 0)
+                {
+                    f[pos] = wordFrequency[i];
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/CompareStringsByFrequencyOfTheSmallestCharacterUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class CompareStringsByFrequencyOfTheSmallestCharacterUnitTest
+    {
+        public CompareStringsByFrequencyOfTheSmallestCharacter CompareStringsByFrequencyOfTheSmallestCharacter { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            CompareStringsByFrequencyOfTheSmallestCharacter = new CompareStringsByFrequencyOfTheSmallestCharacter();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var queries = new[] { "cbd" };
+            var words = new[] { "zaaaz" };
+            var expected = new[] { 1 };
+            var actual = CompareStringsByFrequencyOfTheSmallestCharacter.NumSmallerByFrequency(queries, words);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var queries = new[] { "bbb", "cc" };
+            var words = new[] { "a", "aa", "aaa", "aaaa" };
+            var expected = new[] { 1 };
+            var actual = CompareStringsByFrequencyOfTheSmallestCharacter.NumSmallerByFrequency(queries, words);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
