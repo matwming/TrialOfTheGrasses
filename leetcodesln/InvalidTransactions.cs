8d839fabcd17bcc266ddda64934d6e85e9f39418
user3301 <stan_gai@Hotmail.com>
2019-08-29 14:58:57 +1000

    :white_check_mark: Runtime: 540 ms, faster than 18.18% (using brute force)

---
+++ leetcodesln/InvalidTransactions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class InvalidTransactions
+    {
+        public IList<string> InvalidTranscationsSln(string[] transactions)
+        {
+            var ans = new List<string>();
+
+            for (int i = 0; i < transactions.Length; i++)
+            {
+                bool isValid = Validate(transactions, i);
+                if (!isValid)
+                {
+                    ans.Add(transactions[i]);
+                }
+            }
+            return ans;
+        }
+
+        private bool Validate(string[] transactions, int pos)
+        {
+            string[] elements = transactions[pos].Split(",");
+            var name = elements[0];
+            var time = int.Parse(elements[1]);
+            var amount = int.Parse(elements[2]);
+            var city = elements[3];
+
+            if (amount > 1000) return false;
+            for (int i = 0; i < transactions.Length; i++)
+            {
+                if (i == pos) continue;
+
+                string[] ele = transactions[i].Split(",");
+                var name1 = ele[0];
+                var time1 = int.Parse(ele[1]);
+                var amount1 = int.Parse(ele[2]);
+                var city1 = ele[3];
+
+                if (name == name1 && city != city1 && Math.Abs(time1 - time) <= 60) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/InvalidTransactionsUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using leetcodesln;
+using leetcodesln_test.utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class InvalidTransactionsUnitTest
+    {
+        public InvalidTransactions InvalidTransactions { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            InvalidTransactions = new InvalidTransactions();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var transactions = new[] { "alice,20,800,mtv", "alice,50,100,beijing" };
+            var expected = new[] { "alice,20,800,mtv", "alice,50,100,beijing" };
+            var actual = InvalidTransactions.InvalidTranscationsSln(transactions);
+            Assert.IsTrue(Helper<string>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var transactions = new[] { "alice,20,800,mtv", "alice,50,1200,mtv" };
+            var expected = new[] { "alice,50,1200,mtv" };
+            var actual = InvalidTransactions.InvalidTranscationsSln(transactions);
+            Assert.IsTrue(Helper<string>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var transactions = new[] { "alice,20,800,mtv", "bob,50,1200,mtv" };
+            var expected = new[] { "bob,50,1200,mtv" };
+            var actual = InvalidTransactions.InvalidTranscationsSln(transactions);
+            Assert.IsTrue(Helper<string>.HaveSameElementsForTwoCollections(expected, actual));
+        }
+
+    }
+}
