d09a4e5882af2af392de31a753bc8267a1847092
user3301 <stan_gai@Hotmail.com>
2019-09-01 13:26:56 +1000

    :white_check_mark:weekly contest 152 question 2 (using sliding window)

---
+++ leetcodesln/DietPlanPerformance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class DietPlanPerformance
+    {
+        public int DietPlanPerform(int[] calories, int k, int lower, int upper)
+        {
+            int totalPoints = 0;
+
+            if (k > calories.Length)
+            {
+                throw new Exception();
+            }
+
+            int currentWindow = 0;
+            for (int i = 0; i < k; i++)
+            {
+                currentWindow += calories[i];
+            }
+
+            if (currentWindow < lower)
+            {
+                totalPoints--;
+            }
+            if (currentWindow > upper)
+            {
+                totalPoints++;
+            }
+
+            for (int i = k, j = 0; i < calories.Length; i++)
+            {
+                currentWindow -= calories[j++];
+                currentWindow += calories[i];
+                if (currentWindow < lower)
+                {
+                    totalPoints--;
+                }
+                if (currentWindow > upper)
+                {
+                    totalPoints++;
+                }
+            }
+            return totalPoints;
+        }
+    }
+}
---
+++ leetcodesln_test/DietPlanPerformanceUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class DietPlanPerformanceUnitTest
+    {
+        public DietPlanPerformance DietPlanPerformance { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DietPlanPerformance = new DietPlanPerformance();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var calories = new[] { 1, 2, 3, 4, 5 };
+            var k = 1;
+            var lower = 3;
+            var upper = 3;
+            var expected = 0;
+            var actual = DietPlanPerformance.DietPlanPerform(calories, k, lower, upper);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var calories = new[] { 3, 2 };
+            var k = 2;
+            var lower = 0;
+            var upper = 1;
+            var expected = 1;
+            var actual = DietPlanPerformance.DietPlanPerform(calories, k, lower, upper);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var calories = new[] { 6, 5, 0, 0 };
+            var k = 2;
+            var lower = 1;
+            var upper = 5;
+            var expected = 0;
+            var actual = DietPlanPerformance.DietPlanPerform(calories, k, lower, upper);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
