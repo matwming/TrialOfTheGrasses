4e79072eedab95f94bf2928c0433c3ad1afb469d
user3301 <stan_gai@Hotmail.com>
2019-09-02 10:24:32 +1000

    :sweat_smile: prime arrangements (Runtime: 40 ms, faster than 100.00%)

---
+++ leetcodesln/PrimeArrangements.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcodesln
+{
+    public class PrimeArrangements
+    {
+        const int MOD = (int)(1e9 + 7);
+
+        public int NumPrimeArrangements(int n)
+        {
+            long ans = 1;
+
+            int primes = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primes++;
+                }
+            }
+
+            int notPrimes = n - primes;
+
+            for (int i = primes; i > 0; i--)
+            {
+                ans = ans * i % MOD;
+            }
+            for (int i = notPrimes; i > 0; i--)
+            {
+                ans = ans * i % MOD;
+            }
+            return (int)ans;
+        }
+
+        bool IsPrime(int num)
+        {
+            if (num < 2) return false;
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcodesln_test/PrimeArrangementsUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class PrimeArrangementsUnitTest
+    {
+        public PrimeArrangements PrimeArrangements { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            PrimeArrangements = new PrimeArrangements();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var n = 5;
+            var expected = 12;
+            var actual = PrimeArrangements.NumPrimeArrangements(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var n = 100;
+            int expected = checked(682289015);
+            var actual = PrimeArrangements.NumPrimeArrangements(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var n = 2;
+            int expected = 1;
+            var actual = PrimeArrangements.NumPrimeArrangements(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
