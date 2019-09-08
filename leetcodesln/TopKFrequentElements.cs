d5e4aa6cb66b865b4550b3a4295aa2b9200edd85
user3301 <stan_gai@Hotmail.com>
2019-09-08 21:21:19 +1000

    :eyeglasses:Top k frequent elements (bucket array)

---
+++ leetcodesln/TopKFrequentElements.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class TopKFrequentElements
+    {
+        public IList<int> TopKFrequent(int[] nums, int k)
+        {
+            var ans = new List<int>();
+            var frequencyTable = new Dictionary<int, int>();
+            int maxCount = 0;
+
+            //build frequency table [key:number, value: frequency]
+            foreach (var num in nums)
+            {
+                var f = frequencyTable.ContainsKey(num) ? frequencyTable[num] + 1 : 1;
+                frequencyTable[num] = f;
+                maxCount = maxCount > f ? maxCount : f;
+            }
+
+            // build frequency bucket
+            var bucket = new IList<int>[maxCount + 1];
+            foreach (var d in frequencyTable)
+            {
+                int count = d.Value;
+                if (bucket[count] == null) bucket[count] = new List<int>();
+                bucket[count].Add(d.Key);
+            }
+
+            for (int i = bucket.Length - 1; i >= 0 && k > 0; i--)
+            {
+                if (bucket[i] != null)
+                {
+                    foreach (var num in bucket[i])
+                    {
+                        ans.Add(num);
+                        k -= bucket[i].Count;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/TopKFrequentElementsUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class TopKFrequentElementsUnitTest
+    {
+        public TopKFrequentElements TopKFrequentElements { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            TopKFrequentElements = new TopKFrequentElements();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var nums = new[] { 1, 1, 1, 2, 2, 3 };
+            var k = 2;
+            var expected = new[] { 1, 2 };
+            var actual = TopKFrequentElements.TopKFrequent(nums, k);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+    }
+}
