ac8b0218397e7432c88cefd9ecfdcc1eb3b5d5c9
user3301 <stan_gai@Hotmail.com>
2019-09-09 08:44:49 +1000

    :white_check_mark: Sort characters by frequency (bucket sort, Runtime: 100 ms, faster than 89.04% )

---
+++ leetcodesln/SortCharactersByFrequency.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class SortCharactersByFrequency
+    {
+        public string FrequencySort(string s)
+        {
+            var ans = new StringBuilder();
+            var maxCount = 0;
+            var dict = new Dictionary<char, int>();
+            foreach (var chr in s)
+            {
+                var count = dict.ContainsKey(chr) ? dict[chr] + 1 : 1;
+                dict[chr] = count;
+                maxCount = maxCount > count ? maxCount : count;
+            }
+
+            // k-char v - count
+            var bucket = new List<char>[maxCount + 1];
+
+            foreach (var d in dict)
+            {
+                if (bucket[d.Value] == null)
+                {
+                    bucket[d.Value] = new List<char>();
+                    for (int i = 0; i < d.Value; i++)
+                    {
+                        bucket[d.Value].Add(d.Key);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < d.Value; i++)
+                    {
+                        bucket[d.Value].Add(d.Key);
+                    }
+                }
+            }
+
+            for (int i = bucket.Length - 1; i >= 0; i--)
+            {
+                if (bucket[i] != null)
+                {
+                    foreach (var item in bucket[i])
+                    {
+                        ans.Append(item);
+                    }
+                }
+            }
+            return ans.ToString();
+        }
+    }
+}
---
+++ leetcodesln_test/SortCharactersByFrequencyUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SortCharactersByFrequencyUnitTest
+    {
+        public SortCharactersByFrequency SortCharactersByFrequency { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            SortCharactersByFrequency = new SortCharactersByFrequency();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = "tree";
+            var expected = "eetr";
+            var actual = SortCharactersByFrequency.FrequencySort(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
