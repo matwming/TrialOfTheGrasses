563f8bad805e732add9ee54ebae8634df44c2a42
user3301 <stan_gai@Hotmail.com>
2019-09-08 15:25:08 +1000

    :white_check_mark: Longest word in dictionary through deleting (two pointers, Runtime: 140 ms, faster than 84.62%)

---
+++ leetcodesln/LongestWordInDictionaryThroughDeleting.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class LongestWordInDictionaryThroughDeleting
+    {
+        public string FindLongestWord(string s, IList<string> d)
+        {
+            string ans = string.Empty;
+            foreach (var word in d)
+            {
+                if (word.Length <= s.Length)
+                {
+                    if (CanFormByDelete(s, word))
+                    {
+                        if (ans.Length < word.Length)
+                        {
+                            ans = word;
+                        }
+                        else if (ans.Length == word.Length)
+                        {
+                            ans = ans[0] > word[0] ? word : ans;
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+
+        private bool CanFormByDelete(string s, string word)
+        {
+            var ls = 0;
+            var ws = 0;
+            while (ls < s.Length && ws < word.Length)
+            {
+                if (word[ws] == s[ls])
+                {
+                    ls++;
+                    ws++;
+                }
+                else
+                {
+                    ls++;
+                }
+            }
+            return ws == word.Length;
+        }
+    }
+}
---
+++ leetcodesln_test/LongestWordInDictionaryThroughDeletingUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class LongestWordInDictionaryThroughDeletingUnitTest
+    {
+        public LongestWordInDictionaryThroughDeleting LongestWordInDictionaryThroughDeleting { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            LongestWordInDictionaryThroughDeleting = new LongestWordInDictionaryThroughDeleting();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var s = "abpcplea";
+            var d = new List<string> { "ale", "apple", "monkey", "plea" };
+            var expected = "apple";
+            var actual = LongestWordInDictionaryThroughDeleting.FindLongestWord(s, d);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var s = "abpcplea";
+            var d = new List<string> { "a", "b", "c" };
+            var expected = "a";
+            var actual = LongestWordInDictionaryThroughDeleting.FindLongestWord(s, d);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
