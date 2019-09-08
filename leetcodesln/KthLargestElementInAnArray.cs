5581d8526065fd88f5b8516c6b3bbcd896517f3e
user3301 <stan_gai@Hotmail.com>
2019-09-08 20:38:17 +1000

    :white_check_mark: kth largest element in an array (sorting, Runtime: 104 ms, faster than 87.88%)

---
+++ leetcodesln/KthLargestElementInAnArray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class KthLargestElementInAnArray
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            return nums[nums.Length - k];
+        }
+    }
+}
---
+++ leetcodesln_test/KthLargestElementInAnArrayUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class KthLargestElementInAnArrayUnitTest
+    {
+        public KthLargestElementInAnArray KthLargestElementInAnArray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            KthLargestElementInAnArray = new KthLargestElementInAnArray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new[] { 3, 2, 1, 5, 6, 4 };
+            var k = 2;
+            var expected = 5;
+            var actual = KthLargestElementInAnArray.FindKthLargest(input, k);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
+            var k = 4;
+            var expected = 4;
+            var actual = KthLargestElementInAnArray.FindKthLargest(input, k);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
