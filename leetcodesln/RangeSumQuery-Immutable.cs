f0d4dc98c782604f117196fda64693c74d5ea8cf
user3301 <stan_gai@Hotmail.com>
2019-09-17 22:11:51 +1000

    :star: sum array

--- leetcodesln/RangeSumQuery-Immutable.cs
+++ leetcodesln/RangeSumQuery-Immutable.cs
@@ -4,38 +4,23 @@ namespace leetcodesln
 {
     public class RangeSumQuery_Immutable
     {
-        private int[] _nums { get; set; }
+        private int[] _nums;
 
         public RangeSumQuery_Immutable(int[] nums)
         {
-            _nums = nums;
+            _nums = new int[nums.Length + 1];
+            _nums[0] = 0;
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                _nums[i] = _nums[i - 1] + nums[i - 1];
+            }
         }
 
         public int SumRange(int i, int j)
         {
-            if (i == j) return _nums[i];
-
-            var dp = new int[_nums.Length + 1];
-            Array.Fill(dp, int.MinValue);
-            dp[0] = 0;
-
-            return GetZeroToIndex(ref dp, j + 1) - GetZeroToIndex(ref dp, i);
+            return _nums[j + 1] - _nums[i];
         }
 
-        private int GetZeroToIndex(ref int[] dp, int i)
-        {
 
-            if (dp[i] != int.MinValue) return dp[i];
-            else
-            {
-                var sum = 0;
-                for (int v = 0; v < i; v++)
-                {
-                    sum += _nums[v];
-                }
-                dp[i] = sum;
-                return dp[i];
-            }
-        }
     }
 }
