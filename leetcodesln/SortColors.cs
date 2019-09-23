cd5025265966e78a98f4e6f7f712b1add37f57c0
Zhelun Gai <zhelun.gai@tmacomms.com>
2019-09-23 17:26:28 +1000

    :star: sort colors(dutch national flag problem)

---
+++ leetcodesln/SortColors.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcodesln
+{
+    public class SortColors
+    {
+        public void SortColorsSln(int[] nums)
+        {
+            int low = 0, high = nums.Length - 1;
+            for (int i = 0; i <= high; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    var temp = nums[low];
+                    nums[low++] = nums[i];
+                    nums[i] = temp;
+                }
+                else if (nums[i] == 2)
+                {
+                    var temp = nums[high];
+                    nums[high--] = nums[i];
+                    nums[i--] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ leetcodesln_test/SortColorsUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SortColorsUnitTest
+    {
+        public SortColors SortColors { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            SortColors = new SortColors();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new[] { 2, 0, 2, 1, 1, 0 };
+            var expected = new[] { 0, 0, 1, 1, 2, 2 };
+            SortColors.SortColorsSln(input);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(input[i], expected[i]);
+            }
+        }
+
+    }
+}
