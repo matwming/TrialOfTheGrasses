38d5f2a45f95e9203c30509e4e1e730190c71fd0
Zhelun Gai <zhelun.gai@tmacomms.com>
2019-09-25 12:38:48 +1000

    minimum size subarray sum (two pointers)

---
+++ leetcodesln/MimimumSizeSubarraySum.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcodesln
+{
+    public class MinimumSizeSubarraySum
+    {
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int l = 0, r = 0, min = nums.Length, cur = 0;
+
+            while (r < nums.Length)
+            {
+                cur += nums[r];
+                if (cur >= s)
+                {
+                    min = min < r + 1 - l ? min : r + 1 - l;
+                }
+
+                while (cur - s >= nums[l])
+                {
+                    cur -= nums[l++];
+                    min = min < r + 1 - l ? min : r + 1 - l;
+                }
+
+
+                r++;
+            }
+            return (l == 0 && cur < s) ? 0 : min;
+        }
+    }
+}
---
+++ leetcodesln_test/MinimumSizeSubarraySumUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MImimumSizeSubarraySumUnitTest
+    {
+        public MinimumSizeSubarraySum MinimumSizeSubarraySum { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MinimumSizeSubarraySum = new MinimumSizeSubarraySum();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var s = 11;
+            var nums = new[] { 1, 2, 3, 4, 5 };
+            var expected = 3;
+            var actual = MinimumSizeSubarraySum.MinSubArrayLen(s, nums);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var s = 5;
+            var nums = new[] { 2, 3, 1, 1, 1, 1, 1 };
+            var expected = 2;
+            var actual = MinimumSizeSubarraySum.MinSubArrayLen(s, nums);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var s = 4;
+            var nums = new[] { 1, 4, 4 };
+            var expected = 1;
+            var actual = MinimumSizeSubarraySum.MinSubArrayLen(s, nums);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var s = 100;
+            var nums = new[] { 1, 2, 3 };
+            var expected = 0;
+            var actual = MinimumSizeSubarraySum.MinSubArrayLen(s, nums);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
