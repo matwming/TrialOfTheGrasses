411569fab65f2b261bd1ac0cefb0739ad7947892
user3301 <stan_gai@Hotmail.com>
2019-10-02 08:52:24 +1000

    valid palindrome 2 optimized soln

--- leetcodesln/ValidPalindromeII.cs
+++ leetcodesln/ValidPalindromeII.cs
@@ -4,59 +4,25 @@
     {
         public bool ValidPalindrome(string s)
         {
-            if (s.Length <= 1) return true;
-
-            return IsPalindromeLeft(s, 0, s.Length-1) || IsPalindromeRight(s, 0, s.Length-1);
-        }
-
-        private bool IsPalindromeLeft(string s, int left, int right)
-        {
-            if (s.Length <= 1) return true;
-
-            var chance = 1;
-
-            while (left <= right)
+            int l = 0, r = s.Length - 1;
+            while (l < r)
             {
-                if (IsSameChar(s[left], s[right]) && chance >= 0)
-                {
-                    left++;
-                    right--;
-                }
-                else if (IsSameChar(s[left + 1], s[right]) && chance > 0)
-                {
-                    left++;
-                    chance--;
-                    continue;
-                }
-                else return false;
+                if (s[l] != s[r]) { return Aux(s, l + 1, r) || Aux(s, l, r - 1); }
+                l++;
+                r--;
             }
             return true;
         }
 
-        private bool IsPalindromeRight(string s, int left, int right)
+        bool Aux(string s, int l, int r)
         {
-            if (s.Length <= 1) return true;
-
-            var chance = 1;
-
-            while (left <= right)
+            while (l < r)
             {
-                if (IsSameChar(s[left], s[right]) && chance >= 0)
-                {
-                    left++;
-                    right--;
-                }
-                else if (IsSameChar(s[left], s[right-1]) && chance > 0)
-                {
-                    right--;
-                    chance--;
-                    continue;
-                }
-                else return false;
+                if (s[l] != s[r]) return false;
+                l++;
+                r--;
             }
             return true;
         }
-
-        private bool IsSameChar(char a, char b) => a == b;
     }
 }
