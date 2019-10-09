10ecbdf167ebe39d73fb729d0e36280545751242
Zhelun Gai <zhelun.gai@tmacomms.com>
2019-10-09 21:37:03 +1100

    [ci skip] wip

---
+++ leetcodesln/SubarrayProductLessThanK.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcodesln
+{
+    public class SubarrayProductLessThanK
+    {
+        public int NumSubarrayProductLessThanK(int[] nums, int k)
+        {
+            int l = 0, r = 0, prod = 1, ans = 0;
+            while (r < nums.Length)
+            {
+                if (nums[r] < k) ans++;
+                prod *= nums[r];
+                if (prod < k)
+                {
+                    if (l != r) ans++;
+                    r++;
+                }
+                else
+                {
+                    if (l <= r) prod /= nums[l++];
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodesln_test/SubarrayProductLessThanKUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class SubarrayProductLessThanKUnitTest
+    {
+        public SubarrayProductLessThanK SubarrayProductLessThanK { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            SubarrayProductLessThanK = new SubarrayProductLessThanK();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var nums = new[] { 10, 5, 2, 6 };
+            var k = 100;
+            var expected = 8;
+            var actual = SubarrayProductLessThanK.NumSubarrayProductLessThanK(nums, k);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
