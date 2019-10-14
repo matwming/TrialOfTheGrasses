012010ab3a51eaca5436fd6ae3af10782d984ad2
Zhelun Gai <zhelun.gai@tmacomms.com>
2019-10-14 16:57:22 +1100

    interval list intersections

---
+++ leetcodesln/IntervalListIntersections.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class IntervalListIntersections
+    {
+        public int[][] IntervalIntersection(int[][] A, int[][] B)
+        {
+            var ans = new List<int[]>();
+            int a = 0, b = 0;
+            while (a < A.Length && b < B.Length)
+            {
+                var low = Math.Max(A[a][0], B[b][0]);
+                var high = Math.Min(A[a][1], B[b][1]);
+                if (low <= high) ans.Add(new int[] { low, high });
+                if (A[a][1] < B[b][1]) a++;
+                else b++;
+            }
+            var result = new int[ans.Count][];
+            for (int i = 0; i < ans.Count; ++i)
+            {
+                result[i] = ans[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ leetcodesln_test/IntervalListIntersectionsUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class IntervalListIntersectionsUnitTest
+    {
+        public IntervalListIntersections IntervalListIntersections { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            IntervalListIntersections = new IntervalListIntersections();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var A = new int[][]
+            {
+                new int[]{ 0,2},
+                new int[]{ 5,10},
+                new int[]{ 13,23},
+                new int[]{ 24,25}
+            };
+            var B = new int[][]
+            {
+                new int[]{ 1,5},
+                new int[]{ 8,12},
+                new int[]{ 15,24},
+                new int[]{ 25,26}
+            };
+            var expected = new int[][]
+            {
+                new int[]{ 1,2},
+                new int[]{ 5,5},
+                new int[]{ 8,10},
+                new int[]{ 15,23},
+                new int[]{ 24,24},
+                new int[]{ 25,25}
+            };
+            var actual = IntervalListIntersections.IntervalIntersection(A, B);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i][0], actual[i][0]);
+                Assert.AreEqual(expected[i][1], actual[i][1]);
+            }
+        }
+
+    }
+}
