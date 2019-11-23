50a31dd12871a9ecab293ab20f2aaa48a85b2e43
Zhelun Gai <zhelun.gai@tmacomms.com>
2019-11-24 09:39:07 +1100

    shift 2d grid

---
+++ CSharp/leetcodesln/Shift2DGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace leetcodesln
+{
+    public class Shift2DGrid
+    {
+        public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+        {
+            var n = grid.Length;
+            var m = grid[0].Length;
+
+            var ans = new List<IList<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                ans.Add(new List<int>());
+                for (int j = 0; j < m; j++)
+                {
+                    ans[i].Add(0);
+                }
+            }
+
+
+            for (int i = 0; i < m * n; i++)
+            {
+                int j = (i + k) % (m * n);
+                ans[j / m][j % m] = grid[i / m][i % m];
+            }
+            return ans;
+        }
+    }
+}
---
+++ CSharp/leetcodesln_test/Shift2DGridUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class Shift2DGridUnitTest
+    {
+        public Shift2DGrid Shift2DGrid { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            Shift2DGrid = new Shift2DGrid();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var grid = new int[3][];
+            var k = 1;
+            grid[0] = new int[] { 1, 2, 3 };
+            grid[1] = new int[] { 4, 5, 6 };
+            grid[2] = new int[] { 7, 8, 9 };
+            var actual = Shift2DGrid.ShiftGrid(grid, k);
+        }
+
+    }
+}
