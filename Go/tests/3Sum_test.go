35a902a64bf68f3af6ba8c22ac88dae723d4d196
Zhelun Gai <zhelun.gai@tmacomms.com>
2019-12-02 09:48:10 +1100

    leetcode 15 go solution

---
+++ Go/tests/3Sum_test.go
@@ -0,0 +1,22 @@
+package tests
+
+import (
+	"gotest.tools/assert"
+	"m/v2/questions"
+	"testing"
+)
+
+func Test3Sum(t *testing.T) {
+	nums:=[]int{-1, 0, 1,2,-1, -4}
+	expected := [][]int{
+		{-1,2,-1},
+		{0,1,-1},
+	}
+	actual := questions.ThreeSum(nums)
+
+	for i1,val := range expected {
+		for i2, v := range val {
+			assert.Equal(t, actual[i1][i2], v)
+		}
+	}
+}
