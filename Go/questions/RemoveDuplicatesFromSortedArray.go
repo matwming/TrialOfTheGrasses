db1b2c5f010159925808a7b309324d99a37e9fb8
Zhelun Gai <zhelun.gai@tmacomms.com>
2019-12-06 11:30:51 +1100

    leetcode 26 go sollution

---
+++ Go/questions/RemoveDuplicatesFromSortedArray.go
@@ -0,0 +1,23 @@
+package questions
+
+func RemoveDuplicates(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+
+	cur, sorted := 1,0
+
+	for cur < n {
+		if nums[cur] == nums[sorted] {
+			cur++
+		} else {
+			nums[sorted+1] = nums[cur]
+			sorted++
+			cur++
+		}
+	}
+	return sorted+1
+}
---
+++ Go/tests/RemoveDuplicatesFromSortedArray_test.go
@@ -0,0 +1,18 @@
+package tests
+
+import (
+	"gotest.tools/assert"
+	"m/v2/questions"
+	"testing"
+)
+
+func TestRemoveDuplicatesFromSortedArray1(t *testing.T) {
+	nums := []int{1,1,2}
+	expected := 2
+	expectedArr:= []int{1,2}
+	questions.RemoveDuplicates(nums)
+
+	for i := 0; i < expected; i++ {
+		assert.Equal(t, expectedArr[i], nums[i])
+	}
+}
