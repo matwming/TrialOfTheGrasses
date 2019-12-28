3ec88e06419f6a52e0ffa256e6b06e1c95a05c58
gaiz <stan_gai@Hotmail.com>
2019-12-29 10:44:58 +1100

    leetcode 33 go solution

---
+++ Go/questions/SearchInRotatedSortedArray.go
@@ -0,0 +1,31 @@
+package questions
+
+func SearchInRotatedSortedArray(nums []int, target int) int {
+	l, r := 0, len(nums) -1
+
+	for l< r {
+		mid:= (l+r)>>1
+		if nums[mid] < nums[r] {
+			r = mid
+		} else {
+			l = mid +1
+		}
+	}
+
+	shuffle := l
+
+	l, r = 0, len(nums)-1
+
+	for l <= r {
+		mid := (l+r)>>1
+		realmid := (mid + shuffle) % len(nums)
+		if nums[realmid] == target {
+			return realmid
+		} else if nums[realmid] > target {
+			r = mid -1
+		} else {
+			l = mid +1
+		}
+	}
+	return -1
+}
---
+++ Go/tests/SearchInRotatedSortedArray_test.go
@@ -0,0 +1,15 @@
+package tests
+
+import (
+	"github.com/leetcode/questions"
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestSearchInRotatedSortedArray(t *testing.T) {
+	nums := []int{4,5,6,7,0,1,2}
+	target:= 0
+	expected := 4
+	actual := questions.SearchInRotatedSortedArray(nums,target)
+	assert.Equal(t, expected,actual)
+}
