c20fdf1ac99cd38d099a5b72e1d1cd270f4cc746
gaiz <stan_gai@Hotmail.com>
2020-01-05 14:59:34 +1100

    leetcode weekly contest 170 2nd Medium question - prefixSum

---
+++ CSharp/leetcodesln/XORQueriesOfASubarray.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcodesln
+{
+    public class XORQueriesOfASubarray
+    {
+        public int[] XorQueries(int[] arr, int[][] queries)
+        {
+            var prefixXOR = new int[arr.Length + 1];
+
+            for (int i = 1; i < prefixXOR.Length; i++)
+            {
+                prefixXOR[i] = prefixXOR[i - 1] ^ arr[i - 1];
+            }
+
+            var ans = new int[queries.Length];
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                var l = queries[i][0];
+                var r = queries[i][1];
+
+                ans[i] = prefixXOR[r + 1] ^ prefixXOR[l];
+            }
+            return ans;
+
+        }
+    }
+}
---
+++ CSharp/leetcodesln_test/XORQueriesOfASubarrayUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class XORQueriesOfASubarrayUnitTest
+    {
+        public XORQueriesOfASubarray XORQueriesOfASubarray { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            XORQueriesOfASubarray = new XORQueriesOfASubarray();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var arr = new[] { 1, 3, 4, 8 };
+            var queries = new int[4][];
+            queries[0] = new[] { 0, 1 };
+            queries[1] = new[] { 1, 2 };
+            queries[2] = new[] { 0, 3 };
+            queries[3] = new[] { 3, 3 };
+            var expected = new[] { 2, 7, 14, 8 };
+            var actual = XORQueriesOfASubarray.XorQueries(arr, queries);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+    }
+}
