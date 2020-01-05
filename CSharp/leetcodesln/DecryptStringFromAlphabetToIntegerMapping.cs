d220481a5620050fc543740831336e96aed61b10
gaiz <stan_gai@Hotmail.com>
2020-01-05 14:58:57 +1100

    Leetcode weekly contest 170 1st Easy question

---
+++ CSharp/leetcodesln/DecryptStringFromAlphabetToIntegerMapping.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodesln
+{
+    public class DecryptStringFromAlphabetToIntegerMapping
+    {
+        public string FreqAlphabets(string s)
+        {
+            var map = new Dictionary<string, char>
+            {
+                {"1" , 'a'},
+                {"2" , 'b'},
+                {"3" , 'c'},
+                {"4" , 'd'},
+                {"5" , 'e'},
+                {"6" , 'f'},
+                {"7" , 'g'},
+                {"8" , 'h'},
+                {"9" , 'i'},
+                {"10#" , 'j'},
+                {"11#" , 'k'},
+                {"12#" , 'l'},
+                {"13#" , 'm'},
+                {"14#" , 'n'},
+                {"15#" , 'o'},
+                {"16#" , 'p'},
+                {"17#" , 'q'},
+                {"18#" , 'r'},
+                {"19#" , 's'},
+                {"20#" , 't'},
+                {"21#" , 'u'},
+                {"22#" , 'v'},
+                {"23#" , 'w'},
+                {"24#" , 'x'},
+                {"25#" , 'y'},
+                {"26#" , 'z'},
+
+            };
+
+            var sb = new StringBuilder();
+            var len = s.Length - 1;
+            while (len >= 0)
+            {
+                if (s[len] == '#')
+                {
+                    len -= 2;
+                    sb.Append(map[s.Substring(len, 3)]);
+                    len--;
+                }
+                else
+                {
+                    var idx = s.Substring(len--, 1);
+                    sb.Append(map[idx]);
+
+                }
+            }
+            var chararr = sb.ToString().ToCharArray();
+            Array.Reverse(chararr);
+            return new string(chararr);
+        }
+    }
+}
---
+++ CSharp/leetcodesln_test/DecryptStringFromAlphabetToIntegerMappingUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class DecryptStringFromAlphabetToIntegerMappingUnitTest
+    {
+        public DecryptStringFromAlphabetToIntegerMapping DecryptStringFromAlphabetToIntegerMapping { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            DecryptStringFromAlphabetToIntegerMapping = new DecryptStringFromAlphabetToIntegerMapping();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var s = "10#11#12";
+            var expected = "jkab";
+            var actual = DecryptStringFromAlphabetToIntegerMapping.FreqAlphabets(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var s = "1326#";
+            var expected = "acz";
+            var actual = DecryptStringFromAlphabetToIntegerMapping.FreqAlphabets(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#";
+            var expected = "abcdefghijklmnopqrstuvwxyz";
+            var actual = DecryptStringFromAlphabetToIntegerMapping.FreqAlphabets(s);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
