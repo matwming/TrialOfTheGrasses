fed0dcce15eb7b8e8b18529aa4926e2c60a88e8c
gaiz <stan_gai@Hotmail.com>
2020-01-12 21:05:10 +1100

    contest 171 question 1 brute force

---
+++ CSharp/leetcodesln/ConvertIntegerToTheSumOfTwoNo-ZeroIntegers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace leetcodesln
+{
+    public class ConvertIntegerToTheSumOfTwoNo_ZeroIntegers
+    {
+        public int[] GetNoZeroIntegers(int n)
+        {
+            var ans = new int[2];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!i.ToString().Contains("0"))
+                {
+                    var comp = n - i;
+                    if (!comp.ToString().Contains("0"))
+                    {
+                        ans[0] = i;
+                        ans[1] = comp;
+                        return ans;
+                    }
+                }
+            }
+            throw new Exception();
+        }
+    }
+}
