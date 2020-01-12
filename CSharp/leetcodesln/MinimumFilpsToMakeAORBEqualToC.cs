d6b847ff1d000f787f75c8c938c1ae11a3f3caf4
gaiz <stan_gai@Hotmail.com>
2020-01-12 14:18:41 +1100

    contest 171 question 2

---
+++ CSharp/leetcodesln/MinimumFilpsToMakeAORBEqualToC.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcodesln
+{
+    public class MinimumFilpsToMakeAORBEqualToC
+    {
+        public int MinFlips(int a, int b, int c)
+        {
+            var ans = 0;
+
+            while (c != 0 || a != 0 || b != 0)
+            {
+                var clast = GetLastBit(c);
+                var alast = GetLastBit(a);
+                var blast = GetLastBit(b);
+                if (clast == 0)
+                {
+                    if ((alast | blast) != clast)
+                    {
+                        if (alast != 0) ans++;
+                        if (blast != 0) ans++;
+                    }
+                }
+                else
+                {
+                    if ((alast | blast) != clast) ans += 1;
+                }
+                c >>= 1;
+                b >>= 1;
+                a >>= 1;
+            }
+            return ans;
+        }
+
+        private int GetLastBit(int k)
+        {
+            return k % 2;
+        }
+    }
+}
---
+++ CSharp/leetcodesln_test/MinimumFilpsToMakeAORBEqualToCUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using leetcodesln;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcodesln_test
+{
+    [TestClass]
+    public class MinimumFilpsToMakeAORBEqualToCUnitTest
+    {
+        public MinimumFilpsToMakeAORBEqualToC MinimumFilpsToMakeAORBEqualToC { get; set; }
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            MinimumFilpsToMakeAORBEqualToC = new MinimumFilpsToMakeAORBEqualToC();
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            int a = 8, b = 3, c = 5;
+            var expected = 3;
+            var actual = MinimumFilpsToMakeAORBEqualToC.MinFlips(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            int a = 2, b = 6, c = 5;
+            var expected = 3;
+            var actual = MinimumFilpsToMakeAORBEqualToC.MinFlips(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            int a = 4, b = 2, c = 7;
+            var expected = 1;
+            var actual = MinimumFilpsToMakeAORBEqualToC.MinFlips(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            int a = 1, b = 2, c = 3;
+            var expected = 0;
+            var actual = MinimumFilpsToMakeAORBEqualToC.MinFlips(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
